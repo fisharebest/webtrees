d7606a2ca77385140ee298aea3d25e9cc1a82d6a
fisharebest <fisharebest@gmail.com>
2010-04-25 14:08:49 +0000

    Don't overwrite existing XXX_conf.php and XXX_priv.php files when adding new gedcomds

--- editgedcoms.php
+++ editgedcoms.php
@@ -70,11 +70,6 @@ case 'add_ged':
 	// check it doesn't already exist before we create it
 	if (!$gedcom_id && file_exists($INDEX_DIRECTORY.$ged_name)) {
 		$gedcom_id=get_id_from_gedcom($ged_name, true);
-		copy('config_gedcom.php', $INDEX_DIRECTORY.$ged_name.'_conf.php');
-		copy('privacy.php',       $INDEX_DIRECTORY.$ged_name.'_priv.php');
-		set_gedcom_setting($gedcom_id, 'config',  $INDEX_DIRECTORY.$ged_name.'_conf.php');
-		set_gedcom_setting($gedcom_id, 'privacy', $INDEX_DIRECTORY.$ged_name.'_priv.php');
-		set_gedcom_setting($gedcom_id, 'title',   i18n::translate('Genealogy from [%s]', $ged_name));
 		import_gedcom_file($gedcom_id, $INDEX_DIRECTORY.$ged_name);
 	}
 	break;
@@ -84,12 +79,6 @@ case 'new_ged':
 	// check it doesn't already exist before we create it
 	if (!$gedcom_id) {
 		$gedcom_id=get_id_from_gedcom($ged_name, true);
-		copy('config_gedcom.php', $INDEX_DIRECTORY.$ged_name.'_conf.php');
-		copy('privacy.php',       $INDEX_DIRECTORY.$ged_name.'_priv.php');
-		set_gedcom_setting($gedcom_id, 'config',  $INDEX_DIRECTORY.$ged_name.'_conf.php');
-		set_gedcom_setting($gedcom_id, 'privacy', $INDEX_DIRECTORY.$ged_name.'_priv.php');
-		set_gedcom_setting($gedcom_id, 'title',   i18n::translate('Genealogy from [%s]', $ged_name));
-
 		// I18N: This should be a common/default/placeholder name of a person.  Put slashes around the surname.
 		$john_doe=i18n::translate('John /DOE/');
 		$note=i18n::translate('Edit this individual and replace their details with your own');
@@ -108,11 +97,6 @@ case 'upload_ged':
 			// check it doesn't already exist before we create it
 			if (!$gedcom_id) {
 				$gedcom_id=get_id_from_gedcom($ged_name, true);
-				copy('config_gedcom.php', $INDEX_DIRECTORY.$ged_name.'_conf.php');
-				copy('privacy.php',       $INDEX_DIRECTORY.$ged_name.'_priv.php');
-				set_gedcom_setting($gedcom_id, 'config',  $INDEX_DIRECTORY.$ged_name.'_conf.php');
-				set_gedcom_setting($gedcom_id, 'privacy', $INDEX_DIRECTORY.$ged_name.'_priv.php');
-				set_gedcom_setting($gedcom_id, 'title',   i18n::translate('Genealogy from [%s]', $ged_name));
 				import_gedcom_file($gedcom_id, $FILE['tmp_name']);
 			}
 		}
