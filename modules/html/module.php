da12a9b1e1926c1142f5647cf73805ae6b50342e
fisharebest <fisharebest@gmail.com>
2010-05-26 20:40:55 +0000

    Broken help link

--- modules/html/module.php
+++ modules/html/module.php
@@ -123,11 +123,6 @@ class html_WT_Module extends WT_Module implements WT_Module_Block {
 		if ($ctype=="gedcom" && WT_USER_GEDCOM_ADMIN || $ctype=="user" && WT_USER_ID) {
 			$title .= "<a href=\"javascript: configure block\" onclick=\"window.open('index_edit.php?action=configure&amp;ctype={$ctype}&amp;block_id={$block_id}', '_blank', 'top=50,left=50,width=600,height=350,scrollbars=1,resizable=1'); return false;\">"
 			."<img class=\"adminicon\" src=\"{$WT_IMAGE_DIR}/{$WT_IMAGES['admin']['small']}\" width=\"15\" height=\"15\" border=\"0\" alt=\"".i18n::translate('Configure').'" /></a>';
-			;
-		}
-		if (WT_USER_GEDCOM_ADMIN) {
-			$title .= help_link('index_htmlplus_a');
-		} else {
 			$title .= help_link('index_htmlplus');
 		}
 		$title.=$title_tmp;
