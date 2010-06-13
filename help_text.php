358edf62407f8277cf315696bf38df2226821908
fisharebest <fisharebest@gmail.com>
2010-06-13 14:18:06 +0000

    Remove old/usused help-text messages

--- help_text.php
+++ help_text.php
@@ -3206,11 +3206,6 @@ case 'admin':
 	$text=i18n::translate('On this page you will find links to the configuration pages, administration pages, documentation, and log files.<br /><br /><b>Current Server Time:</b>, just below the page title, shows the time of the server on which your site is hosted. This means that if the server is located in New York while you\'re in France, the time shown will be six hours less than your local time, unless, of course, the server is running on Greenwich Mean Time (GMT).  The time shown is the server time when you opened or refreshed this page.<br /><br /><b>WARNING</b><br />When you see a red warning message under the system time, it means that your <i>config.php</i> is still writeable.  After configuring your site, you should, for <b>security</b>, set the permissions of this file back to read-only.  You have to do this <u>manually</u>, since <b>webtrees</b> cannot do this for you.');
 	break;
 
-case 'admin_help_contents_head':
-	$title=i18n::translate('Help contents');
-	$text=i18n::translate('<b>Administrator Help Items</b> added to the beginning of the list.');
-	break;
-
 case 'age_differences':
 	$title=i18n::translate('Show date differences');
 	$text=i18n::translate('When this option box is checked, the «Close Relatives» tab will show date differences as follows:<br /><ul><li>birth dates of partners.<br />A negative value indicates that the second partner is older than the first.<br /><br /></li><li>marriage date and birth date of the first child.<br />A negative value here indicates that the child was born before the marriage date or that either the birth date or the marriage date is wrong.<br /><br /></li><li>birth dates of siblings.<br />A negative value here indicates that either the order of the children is wrong or that one of the birth dates is wrong.</li></ul>');
