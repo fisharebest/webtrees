effcb808fdebe57a94fb89466128c50fa6169a9b
fisharebest <fisharebest@gmail.com>
2010-06-24 21:05:04 +0000

    #596407 Fatal error: Call to a member function getBirthDate() on a non-object - v8895

--- includes/functions/functions.php
+++ includes/functions/functions.php
@@ -1943,15 +1943,19 @@ function get_relationship_name_from_path($path, $pid1, $pid2) {
 	case 'dau': return i18n::translate('daughter');
 	case 'chi': return i18n::translate('child');
 	case 'bro':
-		$dob1=Person::GetInstance($pid1)->getBirthDate();
-		$dob2=Person::GetInstance($pid2)->getBirthDate();
-		if ($dob1->isOK() && $dob2->isOK()) {
-			if (abs($dob1->JD()-$dob2->JD())<2) {
-				return i18n::translate('twin brother');
-			} else if ($dob1->JD()<$dob2->JD()) {
-				return i18n::translate('younger brother');
-			} else {
-				return i18n::translate('elder brother');
+		$person1=Person::GetInstance($pid1);
+		$person2=Person::GetInstance($pid2);
+		if ($person1 && $person2) {
+			$dob1=$person1->getBirthDate();
+			$dob2=$person2->getBirthDate();
+			if ($dob1->isOK() && $dob2->isOK()) {
+				if (abs($dob1->JD()-$dob2->JD())<2) {
+					return i18n::translate('twin brother');
+				} else if ($dob1->JD()<$dob2->JD()) {
+					return i18n::translate('younger brother');
+				} else {
+					return i18n::translate('elder brother');
+				}
 			}
 		}
 		return i18n::translate('brother');
@@ -2655,28 +2659,26 @@ function get_relationship_name_from_path($path, $pid1, $pid2) {
 	// Try to the split the relationship into sub-relationships.  e.g. third-cousin's great-uncle's fourth-cousin.
 	// This next block of code is experimental.  If it doesn't work, we can remove it.....
 	if (preg_match('/^(.*)(hus|wif|spo)(.*)/', $path, $match)) {
-		// TODO: need the actual people
-		$dummy=new Person('0 @this_is_a_dummy@ INDI'); // temporary hack.  We really need to get these indis from somewhere
 		if ($match[1]=='') {
 			return i18n::translate(
 				// I18N: A complex relationship, such as "second cousin's great-uncle"
 				'%1$s\'s %2$s',
-				get_relationship_name_from_path($match[2], $dummy, $dummy),
-				get_relationship_name_from_path($match[3], $dummy, $dummy)
+				get_relationship_name_from_path($match[2], null, null), // TODO: need the actual people
+				get_relationship_name_from_path($match[3], null, null)
 			);
 		} elseif ($match[3]=='') {
 			return i18n::translate(
 				'%1$s\'s %2$s',
-				get_relationship_name_from_path($match[1], $dummy, $dummy),
-				get_relationship_name_from_path($match[2], $dummy, $dummy)
+				get_relationship_name_from_path($match[1], null, null),
+				get_relationship_name_from_path($match[2], null, null)
 			);
 		} else {
 			return i18n::translate(
 				// I18N: A complex relationship, such as "second cousin's great-uncle's third cousin"
 				'%1$s\'s %2$s\'s %3$s',
-				get_relationship_name_from_path($match[1], $dummy, $dummy),
-				get_relationship_name_from_path($match[2], $dummy, $dummy),
-				get_relationship_name_from_path($match[3], $dummy, $dummy)
+				get_relationship_name_from_path($match[1], null, null),
+				get_relationship_name_from_path($match[2], null, null),
+				get_relationship_name_from_path($match[3], null, null)
 			);
 		}
 	}
