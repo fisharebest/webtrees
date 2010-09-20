f3b6edbf9258c068e6914cafcad3d54b54f07037
fisharebest <fisharebest@gmail.com>
2010-09-20 16:56:13 +0000

    Add I18N comments

--- modules/lightbox/lb_editconfig.php
+++ modules/lightbox/lb_editconfig.php
@@ -45,14 +45,13 @@ print_header(i18n::translate('Lightbox-Album Configuration'));
 
 require WT_ROOT.'modules/lightbox/lb_defaultconfig.php';
 
-print "<span class=\"subheaders\">".i18n::translate('Lightbox-Album Configuration')."</span>";
-print "<br /><br />";
+echo '<span class="subheaders">', i18n::translate('Lightbox-Album Configuration'), '</span><br /><br />';
 
 if (!WT_USER_IS_ADMIN) {
-	print "<table class=\"facts_table\">\n";
-	print "<tr><td colspan=\"2\" class=\"facts_value\">".i18n::translate('Page only for Administrators');
-	print "</td></tr></table>\n";
-	print "<br/><br/><br/>\n";
+	echo "<table class=\"facts_table\">";
+	echo "<tr><td colspan=\"2\" class=\"facts_value\">".i18n::translate('Page only for Administrators');
+	echo "</td></tr></table>";
+	echo "<br/><br/><br/>";
 	print_footer();
 	exit;
 }
@@ -74,20 +73,19 @@ if ($action=='update' && !isset($security_user)) {
 }
 
 ?>
-<form method="post" name="configform" action="<?php print encode_url("module.php?mod=lightbox&mod_action=lb_editconfig&pid={$pid}"); ?>">
+<form method="post" name="configform" action="<?php echo encode_url("module.php?mod=lightbox&mod_action=lb_editconfig&pid={$pid}"); ?>">
 <input type="hidden" name="action" value="update" />
 
 	<table class="facts_table">
 
 		<tr>
-		<td class="descriptionbox"><b><?php print i18n::translate('Individual Page - Album Tab Header');?></b><?php echo help_link('lb_al_head_links', $this->getName()); ?><br />&nbsp;&nbsp;&nbsp;&nbsp;<?php print i18n::translate('Link appearance');?></td>
+		<td class="descriptionbox"><b><?php echo i18n::translate('Individual Page - Album Tab Header'); ?></b><?php echo help_link('lb_al_head_links', $this->getName()); ?><br />&nbsp;&nbsp;&nbsp;&nbsp;<?php echo i18n::translate('Link appearance'); ?></td>
 		<td class="optionbox">
 			<select name="NEW_LB_AL_HEAD_LINKS">
-				<option value="icon" <?php if ($LB_AL_HEAD_LINKS=="icon") print "selected=\"selected\""; ?>><?php print i18n::translate('Icon');?></option>
-				<option value="text" <?php if ($LB_AL_HEAD_LINKS=="text") print "selected=\"selected\""; ?>><?php print i18n::translate('Text');?></option>
-				<option value="both" <?php if ($LB_AL_HEAD_LINKS=="both") print "selected=\"selected\""; ?>><?php print i18n::translate('Both');?></option>
+				<option value="icon" <?php if ($LB_AL_HEAD_LINKS=="icon") echo 'selected="selected"'; ?>><?php echo i18n::translate('Icon'); ?></option>
+				<option value="text" <?php if ($LB_AL_HEAD_LINKS=="text") echo 'selected="selected"'; ?>><?php echo i18n::translate('Text'); ?></option>
+				<option value="both" <?php if ($LB_AL_HEAD_LINKS=="both") echo 'selected="selected"'; ?>><?php echo i18n::translate('Both'); ?></option>
 			</select>
-		&nbsp;&nbsp;&nbsp; <?php print i18n::translate('Icon');?>&nbsp;&nbsp;<?php print i18n::translate('Text');?>&nbsp;&nbsp;<?php print i18n::translate('Both');?>
 		</td>
 		</tr>
 	<tr><td>
@@ -95,12 +93,11 @@ if ($action=='update' && !isset($security_user)) {
 
 
 		<tr>
-		<td class="descriptionbox"><b><?php print i18n::translate('Individual Page - Album Tab Thumbnail - Notes Tooltip');?></b><?php echo help_link('lb_tt_balloon', $this->getName()); ?><br />&nbsp;&nbsp;&nbsp;&nbsp;<?php print i18n::translate('Notes - Tooltip appearance');?></td>
+		<td class="descriptionbox"><b><?php echo i18n::translate('Individual Page - Album Tab Thumbnail - Notes Tooltip'); ?></b><?php echo help_link('lb_tt_balloon', $this->getName()); ?><br />&nbsp;&nbsp;&nbsp;&nbsp;<?php echo i18n::translate('Notes - Tooltip appearance'); ?></td>
 		<td class="optionbox"><select name="NEW_LB_TT_BALLOON">
-				<option value="true"  <?php if ($LB_TT_BALLOON=="true")  print "selected=\"selected\""; ?>><?php print i18n::translate('Balloon');?></option>
-				<option value="false" <?php if ($LB_TT_BALLOON=="false") print "selected=\"selected\""; ?>><?php print i18n::translate('Normal');?></option>
+				<option value="true"  <?php if ($LB_TT_BALLOON=="true")  echo 'selected="selected"'; ?>><?php echo i18n::translate('Balloon'); ?></option>
+				<option value="false" <?php if ($LB_TT_BALLOON=="false") echo 'selected="selected"'; ?>><?php echo i18n::translate('Normal'); ?></option>
 			</select>
-		&nbsp;&nbsp;&nbsp; <?php print i18n::translate('Balloon');?>&nbsp;&nbsp;<?php print i18n::translate('Normal');?>
 		</td>
 		</tr>
 	<tr><td>
@@ -108,12 +105,11 @@ if ($action=='update' && !isset($security_user)) {
 
 
 		<tr>
-		<td class="descriptionbox"><b><?php print i18n::translate('Individual Page - Album Tab Thumbnails');?></b><?php echo help_link('lb_al_thumb_links', $this->getName()); ?><br />&nbsp;&nbsp;&nbsp;&nbsp;<?php print i18n::translate('Link appearance');?></td>
+		<td class="descriptionbox"><b><?php echo i18n::translate('Individual Page - Album Tab Thumbnails'); ?></b><?php echo help_link('lb_al_thumb_links', $this->getName()); ?><br />&nbsp;&nbsp;&nbsp;&nbsp;<?php echo i18n::translate('Link appearance'); ?></td>
 		<td class="optionbox"><select name="NEW_LB_AL_THUMB_LINKS">
-				<option value="icon" <?php if ($LB_AL_THUMB_LINKS=="icon") print "selected=\"selected\""; ?>><?php print i18n::translate('Icon');?></option>
-				<option value="text" <?php if ($LB_AL_THUMB_LINKS=="text") print "selected=\"selected\""; ?>><?php print i18n::translate('Text');?></option>
+				<option value="icon" <?php if ($LB_AL_THUMB_LINKS=="icon") echo 'selected="selected"'; ?>><?php echo i18n::translate('Icon'); ?></option>
+				<option value="text" <?php if ($LB_AL_THUMB_LINKS=="text") echo 'selected="selected"'; ?>><?php echo i18n::translate('Text'); ?></option>
 			</select>
-		&nbsp;&nbsp;&nbsp; <?php print i18n::translate('Icon');?>&nbsp;&nbsp;<?php print i18n::translate('Text');?>
 		</td>
 		</tr>
 	<tr><td>
@@ -121,61 +117,60 @@ if ($action=='update' && !isset($security_user)) {
 
 
 	<tr>
-		<td class="descriptionbox"><b><?php print i18n::translate('Slide Show speed');?></b><?php echo help_link('lb_ss_speed', $this->getName()); ?></td>
+		<td class="descriptionbox"><b><?php echo i18n::translate('Slide Show speed'); ?></b><?php echo help_link('lb_ss_speed', $this->getName()); ?></td>
 		<td class="optionbox"><select name="NEW_LB_SS_SPEED">
-				<option value= "2" <?php if ($LB_SS_SPEED == 2)  print "selected=\"selected\""; ?>><?php print  "2";?></option>
-				<option value= "3" <?php if ($LB_SS_SPEED == 3)  print "selected=\"selected\""; ?>><?php print  "3";?></option>
-				<option value= "4" <?php if ($LB_SS_SPEED == 4)  print "selected=\"selected\""; ?>><?php print  "4";?></option>
-				<option value= "5" <?php if ($LB_SS_SPEED == 5)  print "selected=\"selected\""; ?>><?php print  "5";?></option>
-				<option value= "6" <?php if ($LB_SS_SPEED == 6)  print "selected=\"selected\""; ?>><?php print  "6";?></option>
-				<option value= "7" <?php if ($LB_SS_SPEED == 7)  print "selected=\"selected\""; ?>><?php print  "7";?></option>
-				<option value= "8" <?php if ($LB_SS_SPEED == 8)  print "selected=\"selected\""; ?>><?php print  "8";?></option>
-				<option value= "9" <?php if ($LB_SS_SPEED == 9)  print "selected=\"selected\""; ?>><?php print  "9";?></option>
-				<option value="10" <?php if ($LB_SS_SPEED ==10)  print "selected=\"selected\""; ?>><?php print "10";?></option>
-				<option value="12" <?php if ($LB_SS_SPEED ==12)  print "selected=\"selected\""; ?>><?php print "12";?></option>
-				<option value="15" <?php if ($LB_SS_SPEED ==15)  print "selected=\"selected\""; ?>><?php print "15";?></option>
-				<option value="20" <?php if ($LB_SS_SPEED ==20)  print "selected=\"selected\""; ?>><?php print "20";?></option>
-				<option value="25" <?php if ($LB_SS_SPEED ==25)  print "selected=\"selected\""; ?>><?php print "25";?></option>
+				<option value= "2" <?php if ($LB_SS_SPEED == 2)  echo 'selected="selected"'; ?>><?php echo  "2"; ?></option>
+				<option value= "3" <?php if ($LB_SS_SPEED == 3)  echo 'selected="selected"'; ?>><?php echo  "3"; ?></option>
+				<option value= "4" <?php if ($LB_SS_SPEED == 4)  echo 'selected="selected"'; ?>><?php echo  "4"; ?></option>
+				<option value= "5" <?php if ($LB_SS_SPEED == 5)  echo 'selected="selected"'; ?>><?php echo  "5"; ?></option>
+				<option value= "6" <?php if ($LB_SS_SPEED == 6)  echo 'selected="selected"'; ?>><?php echo  "6"; ?></option>
+				<option value= "7" <?php if ($LB_SS_SPEED == 7)  echo 'selected="selected"'; ?>><?php echo  "7"; ?></option>
+				<option value= "8" <?php if ($LB_SS_SPEED == 8)  echo 'selected="selected"'; ?>><?php echo  "8"; ?></option>
+				<option value= "9" <?php if ($LB_SS_SPEED == 9)  echo 'selected="selected"'; ?>><?php echo  "9"; ?></option>
+				<option value="10" <?php if ($LB_SS_SPEED ==10)  echo 'selected="selected"'; ?>><?php echo "10"; ?></option>
+				<option value="12" <?php if ($LB_SS_SPEED ==12)  echo 'selected="selected"'; ?>><?php echo "12"; ?></option>
+				<option value="15" <?php if ($LB_SS_SPEED ==15)  echo 'selected="selected"'; ?>><?php echo "15"; ?></option>
+				<option value="20" <?php if ($LB_SS_SPEED ==20)  echo 'selected="selected"'; ?>><?php echo "20"; ?></option>
+				<option value="25" <?php if ($LB_SS_SPEED ==25)  echo 'selected="selected"'; ?>><?php echo "25"; ?></option>
 			</select>
-		&nbsp;&nbsp;&nbsp; <?php print i18n::translate('Slide show timing in seconds');?>
+		&nbsp;&nbsp;&nbsp; <?php echo i18n::translate('Slide show timing in seconds'); ?>
 		</td>
 		</tr>
 	<tr><td>
 	</td></tr>
 
 	<tr>
-		<td class="descriptionbox"><b><?php print i18n::translate('Slideshow sound track'); ?></b><?php echo help_link('lb_music_file', $this->getName()); ?><br />&nbsp;&nbsp;&nbsp;&nbsp;<?php print i18n::translate('(mp3 only)'); ?></td>
+		<td class="descriptionbox"><b><?php echo i18n::translate('Slideshow sound track'); ?></b><?php echo help_link('lb_music_file', $this->getName()); ?><br />&nbsp;&nbsp;&nbsp;&nbsp;<?php echo i18n::translate('(mp3 only)'); ?></td>
 		<td class="optionbox">
-			<input type="text" name="NEW_LB_MUSIC_FILE" value="<?php print $LB_MUSIC_FILE;?>" size="60" /><br />
-		<?php print i18n::translate('Location of sound track file (Leave blank for no sound track)');?>
+			<input type="text" name="NEW_LB_MUSIC_FILE" value="<?php echo $LB_MUSIC_FILE; ?>" size="60" /><br />
+		<?php echo i18n::translate('Location of sound track file (Leave blank for no sound track)'); ?>
 		</td>
 		</tr>
 	<tr><td>
 	</td></tr>
 
 		<tr>
-		<td class="descriptionbox"><b><?php print i18n::translate('Image Transition speed');?></b><?php echo help_link('lb_transition', $this->getName()); ?></td>
+		<td class="descriptionbox"><b><?php echo i18n::translate('Image Transition speed'); ?></b><?php echo help_link('lb_transition', $this->getName()); ?></td>
 		<td class="optionbox"><select name="NEW_LB_TRANSITION">
-				<option value="none"   <?php if ($LB_TRANSITION=="none")   print "selected=\"selected\""; ?>><?php print i18n::translate('None');?></option>
-				<option value="normal" <?php if ($LB_TRANSITION=="normal") print "selected=\"selected\""; ?>><?php print i18n::translate('Normal');?></option>
-				<option value="double" <?php if ($LB_TRANSITION=="double") print "selected=\"selected\""; ?>><?php print i18n::translate('Double');?></option>
-				<option value="warp"   <?php if ($LB_TRANSITION=="warp")   print "selected=\"selected\""; ?>><?php print i18n::translate('Warp');?></option>
+				<option value="none"   <?php if ($LB_TRANSITION=="none")   echo 'selected="selected"'; ?>><?php echo i18n::translate('None'); ?></option>
+				<option value="normal" <?php if ($LB_TRANSITION=="normal") echo 'selected="selected"'; ?>><?php echo /* I18N: normal speed */ i18n::translate('Normal'); ?></option>
+				<option value="double" <?php if ($LB_TRANSITION=="double") echo 'selected="selected"'; ?>><?php echo /* I18N: double speed */ i18n::translate('Double'); ?></option>
+				<option value="warp"   <?php if ($LB_TRANSITION=="warp")   echo 'selected="selected"'; ?>><?php echo /* I18N: warp speed (very fast) */ i18n::translate('Warp'); ?></option>
 						</select>
-		&nbsp;&nbsp;&nbsp; <?php print i18n::translate('None');?>&nbsp;&nbsp;<?php print i18n::translate('Normal');?>&nbsp;&nbsp;<?php print i18n::translate('Double');?>&nbsp;&nbsp;<?php print i18n::translate('Warp');?>
 		</td>
 		</tr>
 	<tr><td>
 	</td></tr>
 
 	<tr>
-		<td class="descriptionbox"><b><?php print i18n::translate('URL Window dimensions');?><b><?php echo help_link('lb_url_dimensions', $this->getName()); ?></td>
+		<td class="descriptionbox"><b><?php echo i18n::translate('URL Window dimensions'); ?><b><?php echo help_link('lb_url_dimensions', $this->getName()); ?></td>
 		<td class="optionbox">
-			<input type="text" name="NEW_LB_URL_WIDTH"  value="<?php print $LB_URL_WIDTH;?>"  size="4" />
-			<?php print i18n::translate('Width');?>
+			<input type="text" name="NEW_LB_URL_WIDTH"  value="<?php echo $LB_URL_WIDTH; ?>"  size="4" />
+			<?php echo i18n::translate('Width'); ?>
 			&nbsp;&nbsp;&nbsp;
-			<input type="text" name="NEW_LB_URL_HEIGHT" value="<?php print $LB_URL_HEIGHT;?>" size="4" />
-			<?php print i18n::translate('Height');?><br />
-		<?php print i18n::translate('Width and height of URL window in pixels');?>
+			<input type="text" name="NEW_LB_URL_HEIGHT" value="<?php echo $LB_URL_HEIGHT; ?>" size="4" />
+			<?php echo i18n::translate('Height'); ?><br />
+		<?php echo i18n::translate('Width and height of URL window in pixels'); ?>
 		</td>
 		</tr>
 	<tr><td><br>
@@ -184,15 +179,14 @@ if ($action=='update' && !isset($security_user)) {
 
 
 	<tr>
-		<td class="descriptionbox"><b><?php print i18n::translate('Multimedia Page - Thumbnails');?></b><?php echo help_link('lb_ml_thumb_links', $this->getName()); ?><br />&nbsp;&nbsp;&nbsp;&nbsp;<?php print i18n::translate('Link appearance');?></td>
+		<td class="descriptionbox"><b><?php echo i18n::translate('Multimedia Page - Thumbnails'); ?></b><?php echo help_link('lb_ml_thumb_links', $this->getName()); ?><br />&nbsp;&nbsp;&nbsp;&nbsp;<?php echo i18n::translate('Link appearance'); ?></td>
 		<td class="optionbox">
 			<select name="NEW_LB_ML_THUMB_LINKS">
-				<option value= "none" <?php if ($LB_ML_THUMB_LINKS == "none")  print "selected=\"selected\""; ?>><?php print  i18n::translate('None');?></option>
-				<option value= "text" <?php if ($LB_ML_THUMB_LINKS == "text")  print "selected=\"selected\""; ?>><?php print  i18n::translate('Text');?></option>
-				<option value= "icon" <?php if ($LB_ML_THUMB_LINKS == "icon")  print "selected=\"selected\""; ?>><?php print  i18n::translate('Icon');?></option>
-				<option value= "both" <?php if ($LB_ML_THUMB_LINKS == "both")  print "selected=\"selected\""; ?>><?php print  i18n::translate('Both');?></option>
+				<option value= "none" <?php if ($LB_ML_THUMB_LINKS == "none")  echo 'selected="selected"'; ?>><?php echo  i18n::translate('None'); ?></option>
+				<option value= "text" <?php if ($LB_ML_THUMB_LINKS == "text")  echo 'selected="selected"'; ?>><?php echo  i18n::translate('Text'); ?></option>
+				<option value= "icon" <?php if ($LB_ML_THUMB_LINKS == "icon")  echo 'selected="selected"'; ?>><?php echo  i18n::translate('Icon'); ?></option>
+				<option value= "both" <?php if ($LB_ML_THUMB_LINKS == "both")  echo 'selected="selected"'; ?>><?php echo  i18n::translate('Both'); ?></option>
 			</select>
-		&nbsp;&nbsp;&nbsp; <?php print i18n::translate('None');?>&nbsp;&nbsp;<?php print i18n::translate('Text');?>&nbsp;&nbsp;<?php print i18n::translate('Icon');?>&nbsp;&nbsp;<?php print i18n::translate('Both');?>
 		</td>
 		</tr>
 	<tr><td>
@@ -208,14 +202,14 @@ if ($action=='update' && !isset($security_user)) {
 		 <tr>
 
 				<td class="descriptionbox" colspan="2" align="center">
-						<input type="submit" value="<?php print i18n::translate('Save configuration');?>" onclick="closeHelp();" />
+						<input type="submit" value="<?php echo i18n::translate('Save configuration'); ?>" onclick="closeHelp();" />
 						&nbsp;&nbsp;
-						<input type="reset" value="<?php print i18n::translate('Reset');?>" />
+						<input type="reset" value="<?php echo i18n::translate('Reset'); ?>" />
 						&nbsp;&nbsp;
 			<?php if ($pid){ ?>
-				<INPUT TYPE="button" VALUE="<?php print i18n::translate('Return to Album page');?>" 		onclick="javascript:window.location='individual.php?pid=<?php echo $pid;?>&gedcom=<?php echo $GEDCOM;?>&tab=lightbox'" />
+				<INPUT TYPE="button" VALUE="<?php echo i18n::translate('Return to Album page'); ?>" onclick="javascript:window.location='individual.php?pid=<?php echo $pid; ?>&gedcom=<?php echo $GEDCOM; ?>&tab=lightbox'" />
 			<?php }else{ ?>
-				<INPUT TYPE="button" VALUE="<?php print i18n::translate('Return to Admin Page');?>" 	onclick="javascript:window.location='module_admin.php'" />
+				<INPUT TYPE="button" VALUE="<?php echo i18n::translate('Return to Admin Page'); ?>" onclick="javascript:window.location='module_admin.php'" />
 			<?php } ?>
 			
 			</td>
@@ -225,12 +219,4 @@ if ($action=='update' && !isset($security_user)) {
 </form>
 
 <?php
-
-if(empty($SEARCH_SPIDER))
-	print_footer();
-else {
-	print i18n::translate('Search Engine Spider Detected').": ".$SEARCH_SPIDER;
-	print "\n</div>\n\t</body>\n</html>";
-}
-
-?>
+print_footer();
