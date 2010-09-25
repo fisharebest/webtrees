f6340eb98d5c783fb8edd8ab7b8703f334107ff6
fisharebest <fisharebest@gmail.com>
2010-09-25 07:46:47 +0000

    Suppress the Report menu item, when the use has no access to any report

--- includes/classes/class_menubar.php
+++ includes/classes/class_menubar.php
@@ -38,13 +38,12 @@ define('WT_CLASS_MENUBAR_PHP', '');
 require_once WT_ROOT.'includes/classes/class_menu.php';
 require_once WT_ROOT.'includes/classes/class_module.php';
 
-class MenuBar
-{
+class MenuBar {
 	/**
 	* get the menu with links to the gedcom portals
 	* @return Menu the menu item
 	*/
-	static function getGedcomMenu() {
+	public static function getGedcomMenu() {
 		global $TEXT_DIRECTION, $WT_IMAGES;
 
 		if ($TEXT_DIRECTION=='rtl') $ff='_rtl'; else $ff='';
@@ -78,7 +77,7 @@ class MenuBar
 	* get the mypage menu
 	* @return Menu the menu item
 	*/
-	static function getMyPageMenu() {
+	public static function getMyPageMenu() {
 		global $MEDIA_DIRECTORY, $MULTI_MEDIA;
 		global $TEXT_DIRECTION, $WT_IMAGES;
 		global $PEDIGREE_FULL_DETAILS, $PEDIGREE_LAYOUT;
@@ -165,7 +164,7 @@ class MenuBar
 	* get the menu for the charts
 	* @return Menu the menu item
 	*/
-	static function getChartsMenu($rootid='') {
+	public static function getChartsMenu($rootid='') {
 		global $TEXT_DIRECTION, $WT_IMAGES, $SEARCH_SPIDER;
 		global $PEDIGREE_FULL_DETAILS, $PEDIGREE_LAYOUT;
 		global $controller;
@@ -452,7 +451,7 @@ class MenuBar
 	* get the menu for the lists
 	* @return Menu the menu item
 	*/
-	static function getListsMenu($surname="") {
+	public static function getListsMenu($surname="") {
 		global $TEXT_DIRECTION, $WT_IMAGES, $MULTI_MEDIA, $SEARCH_SPIDER, $controller;
 
 		$style = "top";
@@ -606,7 +605,7 @@ class MenuBar
 	* get the menu for the calendar
 	* @return Menu the menu item
 	*/
-	static function getCalendarMenu() {
+	public static function getCalendarMenu() {
 		global $TEXT_DIRECTION, $WT_IMAGES, $SEARCH_SPIDER;
 
 		if ($TEXT_DIRECTION=="rtl") $ff="_rtl"; else $ff="";
@@ -641,42 +640,25 @@ class MenuBar
 	* get the reports menu
 	* @return Menu the menu item
 	*/
-	static function getReportsMenu($pid="", $famid="") {
+	public static function getReportsMenu($pid="", $famid="") {
 		global $TEXT_DIRECTION, $WT_IMAGES, $SEARCH_SPIDER, $controller;
 
-		$style = "top";
-		if ($pid || $famid) $style = "sub";
-		if (isset($controller)) {
-			if (!$pid) {
-				if (isset($controller->pid)) $pid = $controller->pid;
-				if (isset($controller->rootid)) $pid = $controller->rootid;
-			}
-			if (!$famid) {
-				if (isset($controller->famid)) $famid = $controller->famid;
-			}
+		$active_reports=WT_Module::getActiveReports();
+		if ($SEARCH_SPIDER || !$active_reports) {
+			return new Menu('', '', '');
 		}
 
-
-		if ($TEXT_DIRECTION=="rtl") $ff="_rtl"; else $ff="";
-		if ((!file_exists(WT_ROOT.'reportengine.php')) || (!empty($SEARCH_SPIDER))) {
-			$menu = new Menu("", "", "");
-//			$menu->print_menu = null;
-			return $menu;
-			}
-
-		//-- main reports menu item
-		if ($style=="sub") {
-			$menu = new Menu(i18n::translate('Reports'), "#");
-			$menu->addIcon('reports');
-			$menu->addClass("submenuitem$ff", "submenuitem_hover$ff", "submenu$ff", "icon_small_reports");
+		if ($TEXT_DIRECTION=="rtl") {
+			$ff="_rtl";
 		} else {
-			// top menubar
-			$menu = new Menu(i18n::translate('Reports'), encode_url('reportengine.php?ged='.WT_GEDCOM), "down");
-			$menu->addIcon('reports');
-			$menu->addClass("menuitem$ff", "menuitem_hover$ff", "submenu$ff", "icon_large_reports");
+			$ff="";
 		}
 
-		foreach (WT_Module::getActiveReports() as $report) {
+		$menu = new Menu(i18n::translate('Reports'), encode_url('reportengine.php?ged='.WT_GEDCOM), "down");
+		$menu->addIcon('reports');
+		$menu->addClass("menuitem$ff", "menuitem_hover$ff", "submenu$ff", "icon_large_reports");
+
+		foreach ($active_reports as $report) {
 			foreach ($report->getReportMenus() as $submenu) {
 				$menu->addSubmenu($submenu);
 			}
@@ -688,13 +670,12 @@ class MenuBar
 	* get the optional site-specific menu
 	* @return Menu the menu item
 	*/
-	static function getOptionalMenu() {
+	public static function getOptionalMenu() {
 		global $TEXT_DIRECTION, $WT_IMAGES, $SEARCH_SPIDER;
 
 		if ($TEXT_DIRECTION=="rtl") $ff="_rtl"; else $ff="";
 		if (!file_exists(WT_ROOT.'includes/extras/optional_menu.php') || !empty($SEARCH_SPIDER)) {
 			$menu = new Menu("", "", "");
-//			$menu->print_menu = null;
 			return $menu;
 		}
 		require WT_ROOT.'includes/extras/optional_menu.php';
@@ -705,13 +686,12 @@ class MenuBar
 	* get the search menu
 	* @return Menu the menu item
 	*/
-	static function getSearchMenu() {
+	public static function getSearchMenu() {
 		global $TEXT_DIRECTION, $WT_IMAGES, $SHOW_MULTISITE_SEARCH, $SEARCH_SPIDER;
 
 		if ($TEXT_DIRECTION=="rtl") $ff="_rtl"; else $ff="";
 		if ((!file_exists(WT_ROOT.'search.php')) || (!empty($SEARCH_SPIDER))) {
 			$menu = new Menu("", "", "");
-//			$menu->print_menu = null;
 			return $menu;
 			}
 		//-- main search menu item
@@ -758,7 +738,7 @@ class MenuBar
 	* get an array of module menu objects
 	* @return array
 	*/
-	static function getModuleMenus() {
+	public static function getModuleMenus() {
 		$menus=array();
 		foreach (WT_Module::getActiveMenus() as $module) {
 			$menu=$module->getMenu();
@@ -773,7 +753,7 @@ class MenuBar
 	* get the help menu
 	* @return Menu the menu item
 	*/
-	static function getHelpMenu() {
+	public static function getHelpMenu() {
 		global $TEXT_DIRECTION, $WT_IMAGES, $SEARCH_SPIDER, $QUERY_STRING, $helpindex, $action;
 
 		if ($TEXT_DIRECTION=="rtl") $ff="_rtl"; else $ff="";
@@ -820,16 +800,6 @@ class MenuBar
 		$submenu->addIcon('wiki');
 		$submenu->addClass("submenuitem$ff", "submenuitem_hover$ff", "", "icon_small_wiki");
 		$menu->addSubmenu($submenu);
-// These two sub menu items temporarily removed as the WIKI pages do not exist
-//		$submenu = new Menu(i18n::translate('Wiki User\'s Guide'), WT_WEBTREES_WIKI.'/en/index.php?title=Users_Guide" target="_blank');
-//		$submenu->addClass("submenuitem$ff", "submenuitem_hover$ff", "", "icon_small_wiki");
-//		$menu->addSubmenu($submenu);
-//
-		if (WT_USER_GEDCOM_ADMIN) {
-//			$submenu = new Menu(i18n::translate('Wiki Administrator\'s Guide'), WT_WEBTREES_WIKI.'/en/index.php?title=Administrators_Guide" target="_blank');
-//			$submenu->addClass("submenuitem$ff", "submenuitem_hover$ff", "", "icon_small_wiki");
-//			$menu->addSubmenu($submenu);
-		}
 
 		//-- add contact links to help menu
 		$menu->addSeparator();
@@ -857,7 +827,7 @@ class MenuBar
 	* get the menu with links change to each theme
 	* @return Menu the menu item
 	*/
-	static function getThemeMenu() {
+	public static function getThemeMenu() {
 		global $SEARCH_SPIDER, $ALLOW_THEME_DROPDOWN;
 
 		if ($ALLOW_THEME_DROPDOWN && !$SEARCH_SPIDER && get_site_setting('ALLOW_USER_THEMES')) {
@@ -882,7 +852,7 @@ class MenuBar
 	* get the menu with links change to each color themes subcolor type
 	* @return Menu the menu item
 	*/
-	static function getColorMenu($COLOR_THEME_LIST) {
+	public static function getColorMenu($COLOR_THEME_LIST) {
 		isset($_SERVER["QUERY_STRING"]) == true?$tqstring = "?".$_SERVER["QUERY_STRING"]:$tqstring = "";
 		$frompage = WT_SCRIPT_NAME.decode_url($tqstring);
 		if (isset($_REQUEST['mod'])) {
@@ -909,7 +879,7 @@ class MenuBar
 	* get the menu with links to change language
 	* @return Menu the menu item
 	*/
-	static function getLanguageMenu() {
+	public static function getLanguageMenu() {
 		global $QUERY_STRING, $WT_IMAGES, $TEXT_DIRECTION;
 
 		if ($TEXT_DIRECTION=="rtl") $ff="_rtl"; else $ff="";
@@ -936,7 +906,7 @@ class MenuBar
 	* get the menu with links to the user/gedcom favorites
 	* @return Menu the menu item
 	*/
-	static function getFavoritesMenu() {
+	public static function getFavoritesMenu() {
 		global $REQUIRE_AUTHENTICATION, $GEDCOM, $QUERY_STRING, $WT_IMAGES, $TEXT_DIRECTION;
 		global $SEARCH_SPIDER;
 		global $controller; // Pages with a controller can be added to the favorites
