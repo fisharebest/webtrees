3b01656b143db5be2882f5f76adc41bd54707314
Łukasz Wilenski <wooc@gazeta.pl>
2010-09-29 19:14:43 +0000

    #638224 - Adoption shows multiple births of same child

--- includes/classes/class_person.php
+++ includes/classes/class_person.php
@@ -1158,7 +1158,9 @@ class Person extends GedcomRecord {
 							$factrec.="\n2 ASSO @".$spid."@\n3 RELA ".$rela;
 							$event = new Event($factrec, 0);
 							$event->setParentObject($this);
-							$this->indifacts[]=$event;
+							if (!in_array($event, $this->indifacts)) {
+								$this->indifacts[]=$event;
+							}
 							break;
 						}
 					}
@@ -1179,7 +1181,9 @@ class Person extends GedcomRecord {
 							$factrec.="\n2 ASSO @".$spid."@\n3 RELA ".$rela;
 							$event = new Event($factrec, 0);
 							$event->setParentObject($this);
-							$this->indifacts[] = $event;
+							if (!in_array($event, $this->indifacts)) {
+								$this->indifacts[]=$event;
+							}
 						}
 					}
 				}
@@ -1205,7 +1209,9 @@ class Person extends GedcomRecord {
 							$factrec.="\n2 ASSO @".$sfamily->getSpouseId($spid)."@\n3 RELA ".$rela2;
 							$event = new Event($factrec, 0);
 							$event->setParentObject($this);
-							$this->indifacts[] = $event;
+							if (!in_array($event, $this->indifacts)) {
+								$this->indifacts[]=$event;
+							}
 						}
 					}
 				}
