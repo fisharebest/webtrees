968855a7d9c41d25d5b5af361ca0cbce6dcf7dc7
fisharebest <fisharebest@gmail.com>
2010-10-05 08:03:57 +0000

    Fix: apply "auto close" setting to edit_changes.php

--- edit_changes.php
+++ edit_changes.php
@@ -36,18 +36,18 @@ if (!WT_USER_CAN_ACCEPT) {
 	exit;
 }
 
-$action=safe_GET('action');
+$action   =safe_GET('action');
 $change_id=safe_GET('change_id');
-$index =safe_GET('index');
-$ged   =safe_GET('ged');
+$index    =safe_GET('index');
+$ged      =safe_GET('ged');
 
 print_simple_header(i18n::translate('Review GEDCOM changes'));
+echo WT_JS_START;
 ?>
-<script language="JavaScript" type="text/javascript">
-<!--
 	function show_gedcom_record(xref) {
 		var recwin = window.open("gedrecord.php?fromfile=1&pid="+xref, "_blank", "top=50, left=50, width=600, height=400, scrollbars=1, scrollable=1, resizable=1");
 	}
+
 	function showchanges() {
 		window.location = '<?php echo WT_SCRIPT_NAME; ?>';
 	}
@@ -56,9 +56,8 @@ print_simple_header(i18n::translate('Review GEDCOM changes'));
 		window.opener.location = diffurl;
 		return false;
 	}
-//-->
-</script>
 <?php
+echo WT_JS_END;
 echo '<div class="center"><span class="subheaders">', i18n::translate('Review GEDCOM changes'), '</span><br /><br />';
 
 switch ($action) {
@@ -74,7 +73,6 @@ case 'undo':
 		" AND   xref       = ?".
 		" AND   change_id >= ?"
 	)->execute(array($gedcom_id, $xref, $change_id));
-	echo '<b>', i18n::translate('Undo successful'), '</b>';
 	break;
 case 'accept':
 	$gedcom_id=WT_DB::prepare("SELECT gedcom_id FROM `##change` WHERE change_id=?")->execute(array($change_id))->fetchOne();
@@ -101,7 +99,6 @@ case 'accept':
 		WT_DB::prepare("UPDATE `##change` SET status='accepted' WHERE change_id=?")->execute(array($change->change_id));
 		AddToLog("Accepted change {$change->change_id} for {$change->xref} / {$change->gedcom_name} into database", 'edit');
 	}
-	echo '<b>', i18n::translate('Changes successfully accepted into database'), '</b>';
 	break;
 case 'undoall':
 	WT_DB::prepare(
@@ -109,7 +106,6 @@ case 'undoall':
 		" SET status='rejected'".
 		" WHERE status='pending' AND gedcom_id=?"
 	)->execute(array(get_id_from_gedcom($ged)));
-	echo '<b>', i18n::translate('Undo successful'), '</b>';
 	break;
 case 'acceptall':
 	$changes=WT_DB::prepare(
@@ -130,7 +126,6 @@ case 'acceptall':
 		WT_DB::prepare("UPDATE `##change` SET status='accepted' WHERE change_id=?")->execute(array($change->change_id));
 		AddToLog("Accepted change {$change->change_id} for {$change->xref} / {$change->gedcom_name} into database", 'edit');
 	}
-	echo '<b>', i18n::translate('Changes successfully accepted into database'), '</b>';
 	break;
 }
 
@@ -142,9 +137,7 @@ $changed_gedcoms=WT_DB::prepare(
 	" GROUP BY g.gedcom_name"
 )->fetchOneColumn();
 
-if (!$changed_gedcoms) {
-	echo '<br /><br /><b>', i18n::translate('There are currently no changes to be reviewed.'), '</b>';
-} else {
+if ($changed_gedcoms) {
 	$changes=WT_DB::prepare(
 		"SELECT c.*, u.user_name, u.real_name, g.gedcom_name, IF(new_gedcom='', old_gedcom, new_gedcom) AS gedcom".
 		" FROM `##change` c".
@@ -178,7 +171,7 @@ if (!$changed_gedcoms) {
 			$output.='<b>'.PrintReady($record->getFullName()).'</b> '.getLRM().'('.$record->getXref().')'.getLRM().'<br />';
 			$output.='<a href="javascript:;" onclick="return show_diff(\''.encode_url($record->getLinkUrl().'&show_changes=yes').'\');">'.i18n::translate('View Change Diff').'</a> | ';
 			$output.="<a href=\"javascript:show_gedcom_record('".$change->xref."');\">".i18n::translate('View GEDCOM Record')."</a> | ";
-			$output.="<a href=\"javascript:;\" onclick=\"return edit_raw('".$change->xref."');\">".i18n::translate('Edit raw GEDCOM record')."</a><br />";
+			$output.="<a href=\"javascript:;\" onclick=\"return edit_raw('".$change->xref."');\">".i18n::translate('Edit raw GEDCOM record').'</a><br />';
 			$output.='<div class="indent">';
 			$output.=i18n::translate('The following changes were made to this record:').'<br />';
 			$output.='<table class="list_table"><tr>';
@@ -190,8 +183,8 @@ if (!$changed_gedcoms) {
 			$output.='<td class="list_label">'.i18n::translate('Undo').'</td>';
 			$output.='</tr>';
 		}
-		$output .= "<td class=\"list_value $TEXT_DIRECTION\"><a href=\"".encode_url("edit_changes.php?action=accept&change_id={$change->change_id}")."\">".i18n::translate('Accept')."</a></td>";
-		$output .= "<td class=\"list_value $TEXT_DIRECTION\"><b>";
+		$output .= '<td class="list_value"><a href="'.encode_url("edit_changes.php?action=accept&change_id={$change->change_id}").'">'.i18n::translate('Accept').'</a></td>';
+		$output .= '<td class="list_value"><b>';
 		if ($change->old_gedcom=='') {
 			$output.=i18n::translate('Append record');
 		} elseif ($change->new_gedcom=='') {
@@ -199,51 +192,63 @@ if (!$changed_gedcoms) {
 		} else {
 			$output.=i18n::translate('Replace record');
 		}
-		echo "</b></td>";
-		$output .= "<td class=\"list_value $TEXT_DIRECTION\"><a href=\"javascript:;\" onclick=\"return reply('".$change->user_name."', '".i18n::translate('Review GEDCOM Changes')."')\" alt=\"".i18n::translate('Send Message')."\">";
+		echo '</b></td>';
+		$output .= "<td class=\"list_value\"><a href=\"javascript:;\" onclick=\"return reply('".$change->user_name."', '".i18n::translate('Review GEDCOM Changes')."')\" alt=\"".i18n::translate('Send Message')."\">";
 		$output .= PrintReady($change->real_name);
 		$output .= PrintReady("&nbsp;(".$change->user_name.")")."</a></td>";
-		$output .= "<td class=\"list_value $TEXT_DIRECTION\">".$change->change_time."</td>";
-		$output .= "<td class=\"list_value $TEXT_DIRECTION\">".$change->gedcom_name."</td>";
-		$output .= "<td class=\"list_value $TEXT_DIRECTION\"><a href=\"".encode_url("edit_changes.php?action=undo&change_id={$change->change_id}")."\">".i18n::translate('Undo')."</a></td>";
+		$output .= "<td class=\"list_value\">".$change->change_time."</td>";
+		$output .= "<td class=\"list_value\">".$change->gedcom_name."</td>";
+		$output .= "<td class=\"list_value\"><a href=\"".encode_url("edit_changes.php?action=undo&change_id={$change->change_id}")."\">".i18n::translate('Undo')."</a></td>";
 		$output.='</tr>';
 	}
-	$output .= "</table></td></tr></td></tr></table>";
+	$output .= '</table></td></tr></td></tr></table>';
 
 	//-- Now for the global Action bar:
-	$output2 = "<br /><table class=\"list_table\">";
+	$output2 = '<br /><table class="list_table">';
 	// Row 1 column 1: title "Accept all"
-	$output2 .= "<tr><td class=\"list_label\">".i18n::translate('Accept all changes')."</td>";
+	$output2 .= '<tr><td class="list_label">'.i18n::translate('Accept all changes').'</td>';
 	// Row 1 column 2: separator
-	$output2 .= "<td class=\"list_label width25\">&nbsp;</td>";
+	$output2 .= '<td class="list_label width25">&nbsp;</td>';
 	// Row 1 column 3: title "Undo all"
-	$output2 .= "<td class=\"list_label\">".i18n::translate('Undo all changes')."</td></tr>";
+	$output2 .= '<td class="list_label">'.i18n::translate('Undo all changes').'</td></tr>';
 
 	// Row 2 column 1: action "Accept all"
-	$output2 .= "<tr><td class=\"list_value\">";
+	$output2 .= '<tr><td class="list_value">';
 	$count = 0;
 	foreach ($changed_gedcoms as $gedcom_name) {
-		if ($count!=0) $output2.="<br /><br />";
-		$output2 .= "<a href=\"".encode_url("edit_changes.php?action=acceptall&ged={$gedcom_name}")."\">$gedcom_name - ".i18n::translate('Accept all changes')."</a>";
+		if ($count!=0) $output2.='<br />';
+		$output2 .= '<a href="'.encode_url("edit_changes.php?action=acceptall&ged={$gedcom_name}")."\">$gedcom_name - ".i18n::translate('Accept all changes').'</a>';
 		$count ++;
 	}
-	$output2 .= "</td>";
+	$output2 .= '</td>';
 	// Row 2 column 2: separator
-	$output2 .= "<td class=\"list_value width25\">&nbsp;</td>";
+	$output2 .= '<td class="list_value width25">&nbsp;</td>';
 	// Row 2 column 3: action "Undo all"
-	$output2 .= "<td class=\"list_value\">";
+	$output2 .= '<td class="list_value">';
 	$count = 0;
 	foreach ($changed_gedcoms as $gedcom_name) {
-		if ($count!=0) $output2.="<br /><br />";
-		$output2 .= "<a href=\"".encode_url("edit_changes.php?action=undoall&ged={$gedcom_name}")."\" onclick=\"return confirm('".i18n::translate('Are you sure you want to undo all of the changes for this GEDCOM?')."');\">$gedcom_name - ".i18n::translate('Undo all changes')."</a>";
-		$count ++;
+		if ($count!=0) {
+			$output2.='<br />';
+		}
+		$output2 .= '<a href="'.encode_url("edit_changes.php?action=undoall&ged={$gedcom_name}")."\" onclick=\"return confirm('".i18n::translate('Are you sure you want to undo all of the changes for this GEDCOM?')."');\">$gedcom_name - ".i18n::translate('Undo all changes').'</a>';
+		$count++;
 	}
 	$output2 .= '</td></tr></table>';
 
-	echo '<center>', i18n::translate('Decide for each change to either accept or reject it.<br /><br />To accept all changes at once, click <b>"Accept all changes"</b> in the box below.<br />To get more information about a change,<br />click <b>"View change diff"</b> to see the differences,<br />or click <b>"View GEDCOM record"</b> to see the new data in GEDCOM format.'), '<br />', $output2, $output, $output2, '</center>';
+	echo
+		i18n::translate('Decide for each change to either accept or reject it.<br /><br />To accept all changes at once, click <b>"Accept all changes"</b> in the box below.<br />To get more information about a change,<br />click <b>"View change diff"</b> to see the differences,<br />or click <b>"View GEDCOM record"</b> to see the new data in GEDCOM format.'),
+		'<br />', $output2, $output, $output2, '<br /><br />',
+		'<a href="javascript:;" onclick="if (window.opener.showchanges) window.opener.showchanges(); window.close();">',
+		i18n::translate('Close Window'),
+		'</a>';
+} else {
+	// No pending changes - refresh the parent window and close this one
+	echo
+		WT_JS_START,
+		'if (window.opener.showchanges)	window.opener.showchanges();',
+		'window.close();',
+		WT_JS_END;
 }
 
 echo '</div>';
-
-echo "<br /><br /><center><a href=\"javascript:;\" onclick=\"if (window.opener.showchanges) window.opener.showchanges(); window.close();\">", i18n::translate('Close Window'), '</a><br /></center>';
 print_simple_footer();
