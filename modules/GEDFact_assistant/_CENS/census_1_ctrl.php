c3183b364c09b6b8855b6ccb98975f163e759743
Brian Holland <windmillway2@gmail.com>
2010-10-11 15:22:04 +0000

    GEDFact assistant/_CENS/: - Correct "@N.N" appearing in Title for Household Head when birth surname is not known. Now correctly displays "(unknown)"

--- modules/GEDFact_assistant/_CENS/census_1_ctrl.php
+++ modules/GEDFact_assistant/_CENS/census_1_ctrl.php
@@ -43,6 +43,8 @@ $censyear   = $censdate->date1->y;
 
 $ctry       = "UK";
 // $married    = GedcomDate::Compare($censdate, $marrdate);
+
+// === Set $married to "Not married as we only want the Birth name here" ===
 $married=-1;
 
 $person=Person::getInstance($pid);
@@ -51,9 +53,9 @@ $nam = $person->getAllNames();
 if (PrintReady($person->getDeathYear()) == 0) { $DeathYr = ""; }else{ $DeathYr = PrintReady($person->getDeathYear()); }
 if (PrintReady($person->getBirthYear()) == 0) { $BirthYr = ""; }else{ $BirthYr = PrintReady($person->getBirthYear()); }
 if ($married>=0 && isset($nam[1])){
-	$wholename = rtrim($nam[1]['fullNN']);
+	$wholename = rtrim($nam[1]['full']);
 } else {
-	$wholename = rtrim($nam[0]['fullNN']);
+	$wholename = rtrim($nam[0]['full']);
 }
 
 $currpid=$pid;
