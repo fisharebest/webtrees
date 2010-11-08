464dcd81bba5a18efd4a1384030fee99ef5bb1c1
fisharebest <fisharebest@gmail.com>
2010-11-08 23:16:42 +0000

    Use same labels/terminology on login_register page as useradmin page

--- help_text.php
+++ help_text.php
@@ -3420,7 +3420,7 @@ case 'edituser_email':
 	break;
 
 case 'edituser_gedcomid':
-	$title=i18n::translate('GEDCOM individual record ID');
+	$title=i18n::translate('Individual record');
 	$text=i18n::translate('This is an identification number that links you to your own data in the database.<br /><br />You cannot change this ID; it\'s set by the administrator.  If you think that this ID is not correct, you should contact the administrator to have it changed.');
 	break;
 
