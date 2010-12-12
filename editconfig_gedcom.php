a5e276ecca08bacbfcddb876331a305f2c2c14b0
fisharebest <fisharebest@gmail.com>
2010-12-12 14:05:59 +0000

    I18N: bad grammar in names of close relatives

--- editconfig_gedcom.php
+++ editconfig_gedcom.php
@@ -1222,14 +1222,14 @@ print_header(i18n::translate('GEDCOM configuration'));
 							<table>
 								<?php
 								$rel_events=array(
-									array(null,         null,         '_DEAT_SPOU'),
-									array('_BIRT_CHIL', '_MARR_CHIL', '_DEAT_CHIL'),
 									array('_BIRT_GCHI', '_MARR_GCHI', '_DEAT_GCHI'),
-									array(null,         '_MARR_FATH', '_DEAT_FATH'),
-									array(null,         '_MARR_FAMC', null),
-									array(null,         '_MARR_MOTH', '_DEAT_MOTH'),
+									array('_BIRT_CHIL', '_MARR_CHIL', '_DEAT_CHIL'),
 									array('_BIRT_SIBL', '_MARR_SIBL', '_DEAT_SIBL'),
 									array('_BIRT_HSIB', '_MARR_HSIB', '_DEAT_HSIB'),
+									array(null,         null,         '_DEAT_SPOU'),
+									array(null,         '_MARR_FATH', '_DEAT_FATH'),
+									array(null,         '_MARR_FAMC', null        ),
+									array(null,         '_MARR_MOTH', '_DEAT_MOTH'),
 									array(null,         null,         '_DEAT_GPAR'),
 								);
 								foreach ($rel_events as $row) {
