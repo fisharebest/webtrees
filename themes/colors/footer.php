4364d6e9ae8dd2939c772f17d1985777110c32f7
Rob Peters <makitso@gmail.com>
2011-02-10 01:04:16 +0000

    Cleanup code in footer and add place and icon style

--- themes/colors/footer.php
+++ themes/colors/footer.php
@@ -32,22 +32,25 @@ if (!defined('WT_WEBTREES')) {
 	exit;
 }
 
-echo "</div>"; // Close table started in toplinks.html
+echo '</div>';
+// closing div id=\"content\"
+echo '<div id="footer" class="', $TEXT_DIRECTION, '">';
 echo "<br />";
-echo "<div id=\"footer\" class=\"$TEXT_DIRECTION\">";
-echo "\n\t<br /><div align=\"center\" style=\"width:99%;\">";
-echo contact_links();
-echo "\n\t<br />";
-echo '<br /><a href="', WT_WEBTREES_URL, '" target="_blank"><img src="', $WT_IMAGES['webtrees'], '" width="100" border="0" alt="', WT_WEBTREES, WT_USER_IS_ADMIN? (" - " .WT_VERSION_TEXT): "", '" title="', WT_WEBTREES, WT_USER_IS_ADMIN? (" - " .WT_VERSION_TEXT): "", '" /></a><br />';
-echo "\n\t<br />";
-
-if ($SHOW_STATS || WT_DEBUG) {
-    echo execution_stats();
-}
-
-if (exists_pending_change()) {
-	echo "<br /><a href=\"javascript:;\" onclick=\"window.open('edit_changes.php', '_blank', 'width=600, height=500, resizable=1, scrollbars=1'); return false;\">", WT_I18N::translate('There are pending changes for you to moderate.'), "</a>";
-}
-echo "</div>";
-echo "</div> <!-- close div id=\"footer\" -->\n";
+	echo contact_links();
+	echo "<br />";
+	echo
+		'<p class="logo">',
+			'<a href="', WT_WEBTREES_URL, '" target="_blank">',
+			'<img src="', $WT_IMAGES['webtrees'], '" width="100" border="0" alt="', WT_WEBTREES, WT_USER_IS_ADMIN? (" - " .WT_VERSION_TEXT): "", '"',
+				' title="', WT_WEBTREES, WT_USER_IS_ADMIN? (" - " .WT_VERSION_TEXT): "" , '" /></a>',
+		'</p>';
+	if ($SHOW_STATS || WT_DEBUG) {
+				echo execution_stats();
+	}
+	if (exists_pending_change()) {
+		echo '<a href="javascript:;" onclick="window.open(\'edit_changes.php\', \'_blank\', \'width=600, height=500, resizable=1, scrollbars=1\'); return false;">';
+			echo '<p class="error center">', WT_I18N::translate('There are pending changes for you to moderate.'), '</p>';
+		echo '</a>';
+	}
+	echo '</div>'; // close div id=\"footer\"
 ?>
