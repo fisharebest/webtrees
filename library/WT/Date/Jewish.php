515cfaf75e07d07d71b058ca9d3e01070fdba29c
fisharebest <fisharebest@gmail.com>
2011-02-19 00:05:36 +0000

    Combine Hebrew/Jewish calendars into just Jewish.  Hebrew formatting of numbers is now automatic on Hebrew pages.

--- library/WT/Date/Jewish.php
+++ library/WT/Date/Jewish.php
@@ -37,6 +37,42 @@ class WT_Date_Jewish extends WT_Date_Calendar {
 		return '@#DHEBREW@';
 	}
 
+	const GERSHAYIM="״";
+	const GERSH="׳";
+	const ALAFIM="אלפים";
+
+	function FormatDayZeros() {
+		if (WT_LOCALE=='he') {
+			return $this->NumToHebrew($this->d);
+		} else {
+			return $this->d;
+		}
+	}
+
+	function FormatDay() {
+		if (WT_LOCALE=='he') {
+			return $this->NumToHebrew($this->d);
+		} else {
+			return $this->d;
+		}
+	}
+
+	function FormatShortYear() {
+		if (WT_LOCALE=='he') {
+			return $this->NumToHebrew($this->y%1000);
+		} else {
+			return $this->d;
+		}
+	}
+
+	function FormatLongYear() {
+		if (WT_LOCALE=='he') {
+			return $this->NumToHebrew($this->y);
+		} else {
+			return $this->d;
+		}
+	}
+
 	static function MONTH_TO_NUM($m) {
 		static $months=array(''=>0, 'tsh'=>1, 'csh'=>2, 'ksl'=>3, 'tvt'=>4, 'shv'=>5, 'adr'=>6, 'ads'=>7, 'nsn'=>8, 'iyr'=>9, 'svn'=>10, 'tmz'=>11, 'aav'=>12, 'ell'=>13);
 		if (isset($months[$m])) {
@@ -173,5 +209,64 @@ class WT_Date_Jewish extends WT_Date_Calendar {
 		else
 			list($m, $d, $y)=array(0, 0, 0);
 		return array($y, $m, $d);
+	}
+
+	// Convert a decimal number to hebrew - like roman numerals, but with extra punctuation
+	// and special rules.
+	static function NumToHebrew($num) {
+		global $DISPLAY_JEWISH_THOUSANDS;
+
+		static $jHundreds = array("", "ק", "ר", "ש", "ת", "תק", "תר","תש", "תת", "תתק");
+		static $jTens     = array("", "י", "כ", "ל", "מ", "נ", "ס", "ע", "פ", "צ");
+		static $jTenEnds  = array("", "י", "ך", "ל", "ם", "ן", "ס", "ע", "ף", "ץ");
+		static $tavTaz    = array("ט״ו", "ט״ז");
+		static $jOnes     = array("", "א", "ב", "ג", "ד", "ה", "ו", "ז", "ח", "ט");
+
+		$shortYear = $num %1000; //discard thousands
+		//next check for all possible single Hebrew digit years
+		$singleDigitYear=($shortYear < 11 || ($shortYear <100 && $shortYear % 10 == 0)  || ($shortYear <= 400 && $shortYear % 100 ==0));
+		$thousands = $num / 1000; //get # thousands
+		$sb = "";
+		//append thousands to String
+		if ($num % 1000 == 0) { // in year is 5000, 4000 etc
+			$sb .= $jOnes[$thousands];
+			$sb .= self::GERSH;
+			$sb .= " ";
+			$sb .= self::ALAFIM; //add # of thousands plus word thousand (overide alafim boolean)
+		} else if ($DISPLAY_JEWISH_THOUSANDS) { // if alafim boolean display thousands
+			$sb .= $jOnes[$thousands];
+			$sb .= self::GERSH; //append thousands quote
+			$sb .= " ";
+		}
+		$num = $num % 1000; //remove 1000s
+		$hundreds = $num / 100; // # of hundreds
+		$sb .= $jHundreds[$hundreds]; //add hundreds to String
+		$num = $num % 100; //remove 100s
+		if ($num == 15) { //special case 15
+			$sb .= $tavTaz[0];
+		} else if ($num == 16) { //special case 16
+			$sb .= $tavTaz[1];
+		} else {
+			$tens = $num / 10;
+			if ($num % 10 == 0) {                                    // if evenly divisable by 10
+				if ($singleDigitYear == false) {
+					$sb .= $jTenEnds[$tens]; // use end letters so that for example 5750 will end with an end nun
+				} else {
+					$sb .= $jTens[$tens]; // use standard letters so that for example 5050 will end with a regular nun
+				}
+			} else {
+				$sb .= $jTens[$tens];
+				$num = $num % 10;
+				$sb .= $jOnes[$num];
+			}
+		}
+		if ($singleDigitYear == true) {
+			$sb .= self::GERSH; //append single quote
+		} else { // append double quote before last digit
+			$pos1 = strlen($sb)-2;
+			$sb = substr($sb, 0, $pos1) . self::GERSHAYIM . substr($sb, $pos1);
+			$sb = str_replace(self::GERSHAYIM . self::GERSHAYIM, self::GERSHAYIM, $sb); //replace double gershayim with single instance
+		}
+		return $sb;
 	}
 }
