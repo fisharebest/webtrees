7a2a1b4678e65eb3be7749daf06b3cf6e8b72f0b
Nigel Osborne <kiwi3685@me.com>
2011-03-08 04:10:38 +0000

    Further adjustment to admin styles for IE7 and for RTL in IE7

--- themes/_administration/header.php
+++ themes/_administration/header.php
@@ -38,11 +38,6 @@ echo
 	$javascript,
 	'</head>';
 ?>
-	<!-- This is a script that claims solve all pre-IE9 compatability issues -->
-	<!--[if lt IE 9]>
-	<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js" type="text/javascript">IE7_PNG_SUFFIX=".png";</script>
-	<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/ie7-squish.js" type="text/javascript"></script>
-	<![endif]-->	
 <?php
 echo	
 	'<body id="body">',
