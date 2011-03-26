c6bba9b2d2ff8f6f83be793eab6af7b0fbae8644
Veit Olschinski <veit@olschinski.de>
2011-03-26 18:57:40 +0000

    tidy up html

--- individual.php
+++ individual.php
@@ -103,8 +103,7 @@ jQuery(document).ready(function() {
 		if (strlen($controller->indi->getAddName()) > 0) echo "<span class=\"name_head\">", PrintReady($controller->indi->getAddName()), "</span><br />";
 	?>
 	<div id="indi_header">
-		<h1>
-			<?php
+		<h1><?php
 				if ($TEXT_DIRECTION=="rtl") echo "&nbsp;"; {
 					echo PrintReady($controller->indi->getFullName());
 				}
@@ -116,8 +115,7 @@ jQuery(document).ready(function() {
 						echo printReady("<a href=\"admin_users.php?action=edituser&amp;username={$user_name}\">({$user_name})</a>");
 					}
 				}
-			?>
-		</h1>
+			?></h1>
 			<div id="indi_mainimage">
 				<?php if ($MULTI_MEDIA && $controller->canShowHighlightedObject()) {
 					echo $controller->getHighlightedObject();
