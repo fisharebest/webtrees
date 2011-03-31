5855fe7b7f6150c2a1292eb454a9e676d8b3dc7c
fisharebest <fisharebest@gmail.com>
2011-03-31 17:09:53 +0000

    Fix: descendancy chart does not show descendants of one-parent families

--- library/WT/Controller/Descendancy.php
+++ library/WT/Controller/Descendancy.php
@@ -124,81 +124,85 @@ class WT_Controller_Descendancy extends WT_Controller_Base {
 		}
 	}
 
-/**
- * print a child descendancy
- *
- * @param string $pid individual Gedcom Id
- * @param int $depth the descendancy depth to show
- */
-function print_child_descendancy(&$person, $depth) {
-	global $WT_IMAGES, $Dindent, $personcount;
-
-	if (is_null($person)) return;
-	//print_r($person);
-	echo "<li>";
-	echo "<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\"><tr><td>";
-	if ($depth==$this->generations) echo "<img src=\"".$WT_IMAGES["spacer"]."\" height=\"3\" width=\"$Dindent\" border=\"0\" alt=\"\" /></td><td>";
-	else {
-		echo "<img src=\"".$WT_IMAGES["spacer"]."\" height=\"3\" width=\"3\" border=\"0\" alt=\"\" />";
-		echo "<img src=\"".$WT_IMAGES["hline"]."\" height=\"3\" width=\"".($Dindent-3)."\" border=\"0\" alt=\"\" /></td><td>";
-	}
-	print_pedigree_person($person, 1, 0, $personcount);
-	echo "</td>";
-
-	// check if child has parents and add an arrow
-	echo "<td>&nbsp;</td>";
-	echo "<td>";
-	foreach ($person->getChildFamilies() as $cfamily) {
-		foreach ($cfamily->getSpouses() as $parent) {
-			print_url_arrow($parent->getXref().$personcount.$person->getXref(), "?pid=".$parent->getXref()."&amp;generations={$this->generations}&amp;chart_style={$this->chart_style}&amp;show_full={$this->show_full}&amp;box_width={$this->box_width}", WT_I18N::translate('Start at parents'), 2);
+	/**
+	 * print a child descendancy
+	 *
+	 * @param string $pid individual Gedcom Id
+	 * @param int $depth the descendancy depth to show
+	 */
+	function print_child_descendancy(&$person, $depth) {
+		global $WT_IMAGES, $Dindent, $personcount;
+	
+		if (is_null($person)) return;
+		//print_r($person);
+		echo "<li>";
+		echo "<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\"><tr><td>";
+		if ($depth==$this->generations) echo "<img src=\"".$WT_IMAGES["spacer"]."\" height=\"3\" width=\"$Dindent\" border=\"0\" alt=\"\" /></td><td>";
+		else {
+			echo "<img src=\"".$WT_IMAGES["spacer"]."\" height=\"3\" width=\"3\" border=\"0\" alt=\"\" />";
+			echo "<img src=\"".$WT_IMAGES["hline"]."\" height=\"3\" width=\"".($Dindent-3)."\" border=\"0\" alt=\"\" /></td><td>";
+		}
+		print_pedigree_person($person, 1, 0, $personcount);
+		echo "</td>";
+	
+		// check if child has parents and add an arrow
+		echo "<td>&nbsp;</td>";
+		echo "<td>";
+		foreach ($person->getChildFamilies() as $cfamily) {
+			foreach ($cfamily->getSpouses() as $parent) {
+				print_url_arrow($parent->getXref().$personcount.$person->getXref(), "?pid=".$parent->getXref()."&amp;generations={$this->generations}&amp;chart_style={$this->chart_style}&amp;show_full={$this->show_full}&amp;box_width={$this->box_width}", WT_I18N::translate('Start at parents'), 2);
+				$personcount++;
+				// only show the arrow for one of the parents
+				break;
+			}
+		}
+	
+		// d'Aboville child number
+		$level =$this->generations-$depth;
+		if ($this->show_full) echo "<br /><br />&nbsp;";
+		echo "<span dir=\"ltr\">"; //needed so that RTL languages will display this properly
+		if (!isset($this->dabo_num[$level])) $this->dabo_num[$level]=0;
+		$this->dabo_num[$level]++;
+		$this->dabo_num[$level+1]=0;
+		$this->dabo_sex[$level]=$person->getSex();
+		for ($i=0; $i<=$level;$i++) {
+			$isf=$this->dabo_sex[$i];
+			if ($isf=="M") $isf="";
+			if ($isf=="U") $isf="NN";
+			echo "<span class=\"person_box".$isf."\">&nbsp;".$this->dabo_num[$i]."&nbsp;</span>";
+			if ($i<$level) echo ".";
+		}
+		echo "</span>";
+		echo "</td></tr>";
+		echo "</table>";
+		echo "</li>";
+	
+		// loop for each spouse
+		foreach ($person->getSpouseFamilies() as $family) {
 			$personcount++;
-			// only show the arrow for one of the parents
-			break;
+			$this->print_family_descendancy($person, $family, $depth);
 		}
 	}
-
-	// d'Aboville child number
-	$level =$this->generations-$depth;
-	if ($this->show_full) echo "<br /><br />&nbsp;";
-	echo "<span dir=\"ltr\">"; //needed so that RTL languages will display this properly
-	if (!isset($this->dabo_num[$level])) $this->dabo_num[$level]=0;
-	$this->dabo_num[$level]++;
-	$this->dabo_num[$level+1]=0;
-	$this->dabo_sex[$level]=$person->getSex();
-	for ($i=0; $i<=$level;$i++) {
-		$isf=$this->dabo_sex[$i];
-		if ($isf=="M") $isf="";
-		if ($isf=="U") $isf="NN";
-		echo "<span class=\"person_box".$isf."\">&nbsp;".$this->dabo_num[$i]."&nbsp;</span>";
-		if ($i<$level) echo ".";
-	}
-	echo "</span>";
-	echo "</td></tr>";
-	echo "</table>";
-	echo "</li>";
-
-	// loop for each spouse
-	foreach ($person->getSpouseFamilies() as $family) {
-		$personcount++;
-		$this->print_family_descendancy($person, $family, $depth);
-	}
-}
-
-/**
- * print a family descendancy
- *
- * @param string $pid individual Gedcom Id
- * @param Family $famid family record
- * @param int $depth the descendancy depth to show
- */
-function print_family_descendancy(&$person, &$family, $depth) {
-	global $GEDCOM, $WT_IMAGES, $Dindent, $personcount;
-
-	if (is_null($family)) return;
-	if (is_null($person)) return;
-
-	$spouse=$family->getSpouse($person);
-	if ($spouse) {
+	
+	/**
+	 * print a family descendancy
+	 *
+	 * @param string $pid individual Gedcom Id
+	 * @param Family $famid family record
+	 * @param int $depth the descendancy depth to show
+	 */
+	function print_family_descendancy(&$person, &$family, $depth) {
+		global $GEDCOM, $WT_IMAGES, $Dindent, $personcount;
+	
+		if (is_null($family)) return;
+		if (is_null($person)) return;
+	
+		$spouse=$family->getSpouse($person);
+		if (!$spouse) {
+			// One parent families have no spouse
+			$spouse=new WT_Person('');
+		}
+	
 		// print marriage info
 		echo "<li>";
 		echo "<img src=\"".$WT_IMAGES["spacer"]."\" height=\"2\" width=\"".($Dindent+4)."\" border=\"0\" alt=\"\" />";
@@ -211,14 +215,14 @@ function print_family_descendancy(&$person, &$family, $depth) {
 			echo '</a>';
 		}
 		echo '</span>';
-
+	
 		// print spouse
 		echo "<ul style=\"list-style: none; display: block;\" id=\"".$family->getXref().$personcount."\">";
 		echo "<li>";
 		echo "<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\"><tr><td>";
 		print_pedigree_person($spouse, 1, 0, $personcount);
 		echo "</td>";
-
+	
 		// check if spouse has parents and add an arrow
 		echo "<td>&nbsp;</td>";
 		echo "<td>";
@@ -232,7 +236,7 @@ function print_family_descendancy(&$person, &$family, $depth) {
 		}
 		if ($this->show_full) echo "<br /><br />&nbsp;";
 		echo "</td></tr>";
-
+	
 		// children
 		$children = $family->getChildren();
 		echo "<tr><td colspan=\"3\" class=\"details1\" >&nbsp;&nbsp;";
@@ -256,5 +260,4 @@ function print_family_descendancy(&$person, &$family, $depth) {
 		echo "</ul>";
 		echo "</li>";
 	}
-}
 }
