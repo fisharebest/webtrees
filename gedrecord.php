d63ead5e93b9ad03df43c11ff8ee9d24210bf951
fisharebest <fisharebest@gmail.com>
2011-04-07 09:29:06 +0000

    gedrecord.php should only be used for records with no built-in viewer

--- gedrecord.php
+++ gedrecord.php
@@ -1,65 +1,54 @@
 <?php
-/**
- * Parses gedcom file and displays record for given id in raw text
- *
- * webtrees: Web based Family History software
- * Copyright (C) 2010 webtrees development team.
- *
- * Derived from PhpGedView
- * Copyright (C) 2002 to 2009  PGV Development Team.  All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * @version $Id$
- * @package webtrees
- * @subpackage Charts
- */
+// This is a default viewer for non-standard records (e.g. SUBM, SUBN)
+// that have no dedicated page of their own.
+//
+// webtrees: Web based Family History software
+// Copyright (C) 2011 webtrees development team.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// $Id$
 
 define('WT_SCRIPT_NAME', 'gedrecord.php');
 require './includes/session.php';
 
-header('Content-Type: text/html; charset=UTF-8');
+$obj=WT_GedcomRecord::getInstance(safe_GET_xref('pid'));
 
-$pid=safe_GET_xref('pid');
-
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php echo WT_I18N::html_markup(); ?>>
-	<head>
-		<title><?php echo WT_I18N::translate('Record'), ': ',$pid ; ?></title>
-	</head>
-	<body>
-<?php
-
-if (!$SHOW_GEDCOM_RECORD && !WT_USER_CAN_ACCEPT) {
-	echo "<span class=\"error\">", WT_I18N::translate('This page has been disabled by the site administrator.'), "</span>\n";
-	echo "</body></html>";
+if (!$obj) {
+	header('Location: '.WT_SERVER_NAME.WT_SCRIPT_PATH);
 	exit;
 }
 
-$obj=WT_GedcomRecord::getInstance($pid);
-
-if (is_null($obj) || !$obj->canDisplayDetails()) {
-	print_privacy_error();
-	echo "</body></html>";
+if (
+	$obj instanceof WT_Person ||
+	$obj instanceof WT_Family ||
+	$obj instanceof WT_Source ||
+	$obj instanceof WT_Repository ||
+	$obj instanceof WT_Note ||
+	$obj instanceof WT_Media
+) {
+	header('Location: '.WT_SERVER_NAME.WT_SCRIPT_PATH.$obj->getRawUrl());
 	exit;
 }
 
-$indirec=find_gedcom_record($pid, WT_GED_ID, true);
-$indirec=privatize_gedcom($indirec);
-$indirec=htmlspecialchars($indirec);
-$indirec=preg_replace("/@(\w+)@/", "@<a href=\"gedrecord.php?pid=$1\">$1</a>@", $indirec);
-echo "<pre style=\"white-space: -moz-pre-wrap; white-space: -pre-wrap; white-space: -o-pre-wrap; white-space: pre-wrap; word-wrap: break-word;\">", $indirec, "</pre>";
-echo "</body></html>";
+print_header($obj->getFullName());
+echo
+	'<pre style="white-space:pre-wrap; word-wrap:break-word;">',
+	preg_replace(
+		'/@('.WT_REGEX_XREF.')@/', '@<a href="gedrecord.php?pid=$1">$1</a>@',
+		htmlspecialchars($obj->getGedcomRecord())
+	),
+	'</pre>';
+print_footer();
