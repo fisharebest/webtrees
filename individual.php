818a1e2cfd6c6db43df0f3ac6016bec183920586
Łukasz Wilenski <wooc@gazeta.pl>
2011-04-26 15:47:55 +0000

    don't load tabs and sidebar when the record is private

--- individual.php
+++ individual.php
@@ -33,9 +33,9 @@ require './includes/session.php';
 $showFull = ($PEDIGREE_FULL_DETAILS) ? 1 : 0;
 
 // -- array of GEDCOM elements that will be found but should not be displayed
-$nonfacts = array("FAMS", "FAMC", "MAY", "BLOB", "CHIL", "HUSB", "WIFE", "RFN", "_WT_OBJE_SORT", "");
+$nonfacts = array('FAMS', 'FAMC', 'MAY', 'BLOB', 'CHIL', 'HUSB', 'WIFE', 'RFN', '_WT_OBJE_SORT', '');
 
-$nonfamfacts = array(/*"NCHI",*/ "UID", "");
+$nonfamfacts = array(/*'NCHI',*/ 'UID', '');
 
 $controller=new WT_Controller_Individual();
 $controller->init();
@@ -49,20 +49,18 @@ Zend_Session::writeClose();
 print_header($controller->getPageTitle());
 
 if (!$controller->indi) {
-	echo "<b>", WT_I18N::translate('Unable to find record with ID'), "</b><br /><br />";
+	echo '<b>', WT_I18N::translate('Unable to find record with ID'), '</b><br /><br />';
 	print_footer();
 	exit;
-}
-else if (!$controller->indi->canDisplayName()) {
+} else if (!$controller->indi->canDisplayDetails()) {
+	echo '<table><tr><td class="facts_value" >';
 	print_privacy_error();
+	echo '</td></tr></table>';
 	print_footer();
 	exit;
 }
 $linkToID=$controller->pid; // -- Tell addmedia.php what to link to
-
-?>
-
-<?php echo WT_JS_START;?>
+echo WT_JS_START; ?>
 // javascript function to open a window with the raw gedcom in it
 function show_gedcom_record(shownew) {
 	fromfile="";
@@ -89,7 +87,6 @@ jQuery(document).ready(function() {
 	jQuery('#tabs').bind('tabsshow', function(event, ui) {
 		var selectedTab = ui.tab.name;
 		tabCache[selectedTab] = true;
-
 	<?php
 	foreach ($controller->tabs as $tab) {
 		echo $tab->getJSCallback()."\n";
@@ -99,39 +96,33 @@ jQuery(document).ready(function() {
 });
 
 jQuery(document).ready(function(){
-
 	// Variables
 	var objMain			= jQuery('#main');
 	var objTabs			= jQuery('#tabs');
 	var objBar			= jQuery('#sidebar');
 	var objSeparator	= jQuery('#separator');
-	
 	// Show sidebar
 	function showSidebar(){
 		objMain.addClass('use-sidebar');
 		objSeparator.css('height', objBar.outerHeight() + 'px');
 		jQuery.cookie('sidebar-pref', 'use-sidebar', { expires: 30 });
 	}
-
 	// Hide sidebar
 	function hideSidebar(){
 		objMain.removeClass('use-sidebar');
 		objSeparator.css('height', objTabs.outerHeight() + 'px');
 		jQuery.cookie('sidebar-pref', null, { expires: 30 });
 	}
-
 	// Sidebar separator
 	objSeparator.click(function(e){
 		e.preventDefault();
 		if ( objMain.hasClass('use-sidebar') ){
 			hideSidebar();
-		}
-		else {
+		} else {
 			showSidebar();
 		}
 	});
 ;
-
 	// Load preference
 	if ( jQuery.cookie('sidebar-pref') == null ){
 		objMain.removeClass('use-sidebar');
@@ -141,144 +132,132 @@ jQuery(document).ready(function(){
 	}
 	
 });
-
 <?php
 echo WT_JS_END;
-
 // ===================================== header area =======================================
-if ((empty($SEARCH_SPIDER))&&($controller->accept_success)) echo "<b>", WT_I18N::translate('Changes successfully accepted into database'), "</b><br />";
-if ($controller->indi->isMarkedDeleted()) echo "<span class=\"error\">".WT_I18N::translate('This record has been marked for deletion upon admin approval.')."</span>";
-if (strlen($controller->indi->getAddName()) > 0) echo "<span class=\"name_head\">", PrintReady($controller->indi->getAddName()), "</span><br />";
-
+if ((empty($SEARCH_SPIDER))&&($controller->accept_success)) echo '<b>', WT_I18N::translate('Changes successfully accepted into database'), '</b><br />';
+if ($controller->indi->isMarkedDeleted()) echo '<span class="error">', WT_I18N::translate('This record has been marked for deletion upon admin approval.'), '</span>';
+if (strlen($controller->indi->getAddName()) > 0) echo '<span class="name_head">', PrintReady($controller->indi->getAddName()), '</span><br />';
 echo '<div id="main" class="use-sidebar sidebar-at-right">'; //overall page container
 ?>
-	<div id="indi_header">
-		<h1><?php
-				if ($TEXT_DIRECTION=="rtl") echo "&nbsp;"; {
-					echo PrintReady($controller->indi->getFullName());
-				}
-				if (WT_USER_IS_ADMIN) {
-					$user_id=get_user_from_gedcom_xref(WT_GED_ID, $controller->pid);
-					if ($user_id) {
-						$user_name=get_user_name($user_id);
-						echo "&nbsp;";
-						echo printReady("<a href=\"admin_users.php?action=edituser&amp;username={$user_name}\">({$user_name})</a>");
+<div id="indi_header">
+	<h1><?php
+		if ($TEXT_DIRECTION=='rtl') echo '&nbsp;'; {
+			echo PrintReady($controller->indi->getFullName());
+		}
+		if (WT_USER_IS_ADMIN) {
+			$user_id=get_user_from_gedcom_xref(WT_GED_ID, $controller->pid);
+			if ($user_id) {
+				$user_name=get_user_name($user_id);
+				echo '&nbsp;';
+				echo printReady('<a href="admin_users.php?action=edituser&amp;username='.$user_name.'">('.$user_name.')</a>');
+			}
+		}
+	?></h1>
+	<div id="indi_mainimage">
+		<?php if ($MULTI_MEDIA && $controller->canShowHighlightedObject()) {
+			echo $controller->getHighlightedObject();
+		} ?>
+	</div>
+	<div id="indi_name_details">
+		<?php
+		//Display name details
+		if ($controller->indi->canDisplayDetails()) {
+			$globalfacts=$controller->getGlobalFacts();
+			$nameSex = array('NAME', 'SEX');
+			foreach ($globalfacts as $key=>$value) {
+				if ($key == 0) {
+					// First name
+					$fact = $value->getTag();
+					if (in_array($fact, $nameSex)) {
+						if ($fact=='NAME') $controller->print_name_record($value);
 					}
-				}
-			?></h1>
-			<div id="indi_mainimage">
-				<?php if ($MULTI_MEDIA && $controller->canShowHighlightedObject()) {
-					echo $controller->getHighlightedObject();
-				} ?>
-			</div>
-			<div id="indi_name_details">
-				<?php
-					//Display name details
-					if ($controller->indi->canDisplayDetails()) {
-						$globalfacts=$controller->getGlobalFacts();
-						$nameSex = array('NAME', 'SEX');
-						foreach ($globalfacts as $key=>$value) {
-							if ($key == 0) {
-							// First name
-								$fact = $value->getTag();
-								if (in_array($fact, $nameSex)) {
-									if ($fact=="NAME") $controller->print_name_record($value);
-								}
-								//Display facts
-									echo '<div id="indi_facts">';
-									//Display gender
-									foreach ($globalfacts as $key=>$value) {
-										$fact = $value->getTag();
-										if (in_array($fact, $nameSex)) {
-											if ($fact=="SEX") $controller->print_sex_record($value);
-										}
-									}
-									// Display summary birth/death info.
-									$summary=$controller->indi->format_first_major_fact(WT_EVENTS_BIRT, 2);
-									// If alive display age
-									if (!$controller->indi->isDead()) {
-										$bdate=$controller->indi->getBirthDate();
-										$age = WT_Date::GetAgeGedcom($bdate);
-										if ($age!="") $summary.= "<dl><dt class=\"label\">".WT_I18N::translate('Age')."</dt><span class=\"field\">".get_age_at_event($age, true)."</span></dl>";
-									}
-									$summary.=$controller->indi->format_first_major_fact(WT_EVENTS_DEAT, 2);
-									if ($SHOW_LDS_AT_GLANCE) {
-										$summary.="<dl><span><b>".get_lds_glance($controller->indi->getGedcomRecord())."</b></span></dl>";
-									}
-									if ($summary) {
-										echo $summary;
-									}
-								echo '</div>';
-							} else {
-								// 2nd and more names
-								$fact = $value->getTag();
-								if (in_array($fact, $nameSex)) {
-									if ($fact=="NAME") $controller->print_name_record($value);
-								}
-							}
+					//Display facts
+					echo '<div id="indi_facts">';
+					//Display gender
+					foreach ($globalfacts as $key=>$value) {
+						$fact = $value->getTag();
+						if (in_array($fact, $nameSex)) {
+							if ($fact=='SEX') $controller->print_sex_record($value);
 						}
 					}
-				?>
-			</div>
-	</div>
-	<div id="hitcounter" class="clearfloat">
-		<?php
-			if ($SHOW_COUNTER && (empty($SEARCH_SPIDER))) {
-				//print indi counter only if displaying a non-private person
-				require WT_ROOT.'includes/hitcount.php';
-				echo WT_I18N::translate('Hit Count:'), " ", $hitCount;
+					// Display summary birth/death info.
+					$summary=$controller->indi->format_first_major_fact(WT_EVENTS_BIRT, 2);
+					// If living display age
+					if (!$controller->indi->isDead()) {
+						$bdate=$controller->indi->getBirthDate();
+						$age = WT_Date::GetAgeGedcom($bdate);
+						if ($age!='') $summary.= '<dl><dt class="label">'.WT_I18N::translate('Age').'</dt><span class="field">'.get_age_at_event($age, true).'</span></dl>';
+					}
+					$summary.=$controller->indi->format_first_major_fact(WT_EVENTS_DEAT, 2);
+					if ($SHOW_LDS_AT_GLANCE) {
+						$summary.='<dl><span><b>'.get_lds_glance($controller->indi->getGedcomRecord()).'</b></span></dl>';
+					}
+					if ($summary) {
+						echo $summary;
+					}
+					echo '</div>';
+				} else {
+					// 2nd and more names
+					$fact = $value->getTag();
+					if (in_array($fact, $nameSex)) {
+						if ($fact=='NAME') $controller->print_name_record($value);
+					}
+				}
 			}
+		}
 		?>
 	</div>
-	
+</div>
+<div id="hitcounter" class="clearfloat">
+	<?php
+	if ($SHOW_COUNTER && (empty($SEARCH_SPIDER))) {
+		//print indi counter only if displaying a non-private person
+		require WT_ROOT.'includes/hitcount.php';
+		echo WT_I18N::translate('Hit Count:'), " ", $hitCount;
+	}
+	?>
+</div>	
 <?php
 // ===================================== main content tabs
 foreach ($controller->tabs as $tab) {
 	echo $tab->getPreLoadContent();
 }
 $showFull = ($PEDIGREE_FULL_DETAILS) ? 1 : 0;
-	echo '<div id="tabs" >';
-	echo '<ul>';
-	foreach ($controller->tabs as $tab) {
-		if ($tab->hasTabContent()) {
-			if ($tab->getName()==$controller->default_tab) {
-				// Default tab loads immediately
-				echo '<li><a title="', $tab->getName(), '" href="#', $tab->getName(), '">';
-			} elseif ($tab->canLoadAjax()) {
-				// AJAX tabs load later
-				echo '<li><a title="', $tab->getName(), '" href="',$controller->indi->getHtmlUrl(),'&amp;action=ajax&amp;module=', $tab->getName(), '">';
-			} else {
-				// Non-AJAX tabs load immediately (search engines don't load ajax)
-				echo '<li><a title="', $tab->getName(), '" href="#', $tab->getName(), '">';
-			}
-			echo '<span title="', $tab->getTitle(), '">', $tab->getTitle(), '</span></a></li>';
+echo '<div id="tabs" >';
+echo '<ul>';
+foreach ($controller->tabs as $tab) {
+	if ($tab->hasTabContent()) {
+		if ($tab->getName()==$controller->default_tab) {
+			// Default tab loads immediately
+			echo '<li><a title="', $tab->getName(), '" href="#', $tab->getName(), '">';
+		} else if ($tab->canLoadAjax()) {
+			// AJAX tabs load later
+			echo '<li><a title="', $tab->getName(), '" href="',$controller->indi->getHtmlUrl(),'&amp;action=ajax&amp;module=', $tab->getName(), '">';
+		} else {
+			// Non-AJAX tabs load immediately (search engines don't load ajax)
+			echo '<li><a title="', $tab->getName(), '" href="#', $tab->getName(), '">';
+		}
+		echo '<span title="', $tab->getTitle(), '">', $tab->getTitle(), '</span></a></li>';
+	}
+}
+echo '</ul>';
+foreach ($controller->tabs as $tab) {
+	if ($tab->hasTabContent()) {
+		if ($tab->getName()==$controller->default_tab || !$tab->canLoadAjax()) {
+			echo '<div id="', $tab->getName(), '">';
+			echo $tab->getTabContent();
+			echo '</div>';
 		}
 	}
-	echo '</ul>';
-	foreach ($controller->tabs as $tab) {
-		if ($tab->hasTabContent()) {
-			if ($tab->getName()==$controller->default_tab || !$tab->canLoadAjax()) {
-				echo '<div id="', $tab->getName(), '">';
-				echo $tab->getTabContent();
-				echo '</div>';
-			}
-		}
-	}
-	echo '</div>'; // close #tabs 
-
+}
+echo '</div>'; // close #tabs 
 // ===================================== sidebar area
-	echo '<div id="sidebar">'; // sidebar code
-	if (!$controller->indi->canDisplayDetails()) {
-		echo "<table><tr><td class=\"facts_value\" >";
-		print_privacy_error();
-		echo "</td></tr></table>";
-	} else {
-		require './sidebar.php';
-	}
+echo '<div id="sidebar">'; // sidebar code
+require './sidebar.php';
 echo
 	'</div>',  // close #sidebar
 	'<a href="#" id="separator" title="', WT_I18N::translate('Click here to open or close the sidebar'), '"></a>'; //clickable element to open/close sidebar
-
 // =======================================footer and other items 
 echo '</div>', // close #main
 		WT_JS_START,
