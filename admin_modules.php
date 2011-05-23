ab754b1aa4b6e5983b8276517fcff694d1fee78b
fisharebest <fisharebest@gmail.com>
2011-05-23 09:47:22 +0000

    Module administration - add the ability to delete the configuration for old/deleted modules.

--- admin_modules.php
+++ admin_modules.php
@@ -33,32 +33,51 @@ if (!WT_USER_IS_ADMIN) {
 
 // New modules may have been added...
 $installed_modules=WT_Module::getInstalledModules();
-foreach ($installed_modules as $module_name=>$module) {
-	// New module
-	WT_DB::prepare("INSERT IGNORE INTO `##module` (module_name) VALUES (?)")->execute(array($module_name));
-}
+$all_modules=WT_DB::prepare("SELECT module_name, status FROM `##module`")->fetchAssoc();
 
-// Disable modules that no longer exist.  Don't delete the config.  The module
-// may have only been removed temporarily, e.g. during an upgrade / migration
-$module_names=WT_DB::prepare("SELECT module_name FROM `##module` WHERE status='enabled'")->fetchOneColumn();
-foreach ($module_names as $module_name) {
-	if (!array_key_exists($module_name, $installed_modules)) {
-		WT_DB::prepare(
-			"UPDATE `##module` SET status='disabled' WHERE module_name=?"
-		)->execute(array($module_name));
+foreach ($installed_modules as $module_name=>$module) {
+	if (!array_key_exists($module_name, $all_modules)) {
+		WT_DB::prepare("INSERT INTO `##module` (module_name, status) VALUES (?, 'disabled')")->execute(array($module_name));
+		$all_modules[$module_name]='disabled';
 	}
 }
 
-$action = safe_POST('action');
-
-if ($action=='update_mods') {
-	foreach (WT_Module::getInstalledModules() as $module) {
-		$module_name=$module->getName();
-		$status=safe_POST("status-{$module_name}");
-		if ($status!==null) {
-			WT_DB::prepare("UPDATE `##module` SET status=? WHERE module_name=?")->execute(array($status ? 'enabled' : 'disabled', $module_name));
+switch (safe_POST('action')) {
+case 'update_mods':
+	foreach ($all_modules as $module_name=>$status) {
+		$new_status=safe_POST("status-{$module_name}");
+		if ($new_status!==null) {
+			$new_status=$new_status ? 'enabled' : 'disabled';
+			if ($new_status!=$status) {
+				WT_DB::prepare("UPDATE `##module` SET status=? WHERE module_name=?")->execute(array($new_status, $module_name));
+				$all_modules[$module_name]=$new_status;
+			}
 		}
 	}
+	break;
+}
+
+switch (safe_GET('action')) {
+case 'delete_module':
+	$module_name=safe_GET('module_name');
+	WT_DB::prepare(
+		"DELETE `##block_setting`".
+		" FROM `##block_setting`".
+		" JOIN `##block` USING (block_id)".
+		" JOIN `##module` USING (module_name)".
+		" WHERE module_name=?"
+	)->execute(array($module_name));
+	WT_DB::prepare(
+		"DELETE `##block`".
+		" FROM `##block`".
+		" JOIN `##module` USING (module_name)".
+		" WHERE module_name=?"
+	)->execute(array($module_name));
+	WT_DB::prepare("DELETE FROM `##module_setting` WHERE module_name=?")->execute(array($module_name));
+	WT_DB::prepare("DELETE FROM `##module_privacy` WHERE module_name=?")->execute(array($module_name));
+	WT_DB::prepare("DELETE FROM `##module`         WHERE module_name=?")->execute(array($module_name));
+	unset($all_modules[$module_name]);
+	break;
 }
 
 print_header(WT_I18N::translate('Module administration'));
@@ -77,7 +96,7 @@ print_header(WT_I18N::translate('Module administration'));
   
 	var oTable = jQuery('#installed_table').dataTable( {
 		"oLanguage": {
-			"sLengthMenu": '<?php echo /* I18N: %s is a placeholder for listbox containing numeric options */ WT_I18N::translate('Display %s', '<select><option value="10">10</option><option value="20">20</option><option value="30">30</option><option value="40">40</option><option value="50">50</option><option value="-1">'.WT_I18N::translate('All').'</option></select>'); ?>',
+			"sLengthMenu": '<?php echo /* I18N: %s is a placeholder for listbox containing numeric options */ WT_I18N::translate('Display %s', '<select><option value="5">5</option><option value="10">10</option><option value="20">20</option><option value="30">30</option><option value="40">40</option><option value="50">50</option><option value="-1">'.WT_I18N::translate('All').'</option></select>'); ?>',
 			"sZeroRecords": '<?php echo WT_I18N::translate('No records to display');?>',
 			"sInfo": '<?php echo /* I18N: %s are placeholders for numbers */ WT_I18N::translate('Showing %1$s to %2$s of %3$s', '_START_', '_END_', '_TOTAL_'); ?>',
 			"sInfoEmpty": '<?php echo /* I18N: %s are placeholders for numbers */ WT_I18N::translate('Showing %1$s to %2$s of %3$s', '0', '0', '0'); ?>',
@@ -96,8 +115,17 @@ print_header(WT_I18N::translate('Module administration'));
 		"aaSorting": [[ 1, "asc" ]],
 		"iDisplayLength": 10,
 		"sPaginationType": "full_numbers",
-		"aoColumnDefs": [
-			{ "bSortable": false, "aTargets": [ 0 ] }
+		"aoColumns" : [
+			{ bSortable: false, sClass: "center" },
+			null,
+			null,
+			{ sClass: "center" },
+			{ sClass: "center" },
+			{ sClass: "center" },
+			{ sClass: "center" },
+			{ sClass: "center" },
+			{ sClass: "center" },
+			{ sClass: "center" }
 		]
 	});
 });
@@ -125,23 +153,30 @@ print_header(WT_I18N::translate('Module administration'));
 				</thead>
 				<tbody>
 					<?php
-					foreach (WT_Module::getInstalledModules() as $module) {
-						$status=WT_DB::prepare(
-							"SELECT status FROM `##module` WHERE module_name=?"
-						)->execute(array($module->getName()))->fetchOne();
-						echo '<tr><td>', two_state_checkbox('status-'.$module->getName(), $status=='enabled'), '</td>';
-						?>
-						<td><?php echo $module->getTitle(); ?></td>
-						<td class="<?php echo $TEXT_DIRECTION; ?>" ><?php echo $module->getDescription(); ?></td>
-						<td><?php if ($module instanceof WT_Module_Menu) echo WT_I18N::translate('Yes'); else echo WT_I18N::translate('No'); ?></td>
-						<td><?php if ($module instanceof WT_Module_Tab) echo WT_I18N::translate('Yes'); else echo WT_I18N::translate('No'); ?></td>
-						<td><?php if ($module instanceof WT_Module_Sidebar) echo WT_I18N::translate('Yes'); else echo WT_I18N::translate('No'); ?></td>
-						<td><?php if ($module instanceof WT_Module_Block) echo WT_I18N::translate('Yes'); else echo WT_I18N::translate('No'); ?></td>
-						<td><?php if ($module instanceof WT_Module_Chart) echo WT_I18N::translate('Yes'); else echo WT_I18N::translate('No'); ?></td>
-						<td><?php if ($module instanceof WT_Module_Report) echo WT_I18N::translate('Yes'); else echo WT_I18N::translate('No'); ?></td>
-						<td><?php if ($module instanceof WT_Module_Theme) echo WT_I18N::translate('Yes'); else echo WT_I18N::translate('No'); ?></td>
-						</tr>
-					<?php
+					foreach ($all_modules as $module_name=>$status) {
+						if (array_key_exists($module_name, $installed_modules)) {
+							$module=$installed_modules[$module_name];
+							echo
+								'<tr><td>', two_state_checkbox('status-'.$module->getName(), $status=='enabled'), '</td>',
+								'<td>', $module->getTitle(), '</td>',
+								'<td>', $module->getDescription(), '</td>',
+								'<td>', $module instanceof WT_Module_Menu    ? WT_I18N::translate('yes') : WT_I18N::translate('no'), '</td>',
+								'<td>', $module instanceof WT_Module_Tab     ? WT_I18N::translate('yes') : WT_I18N::translate('no'), '</td>',
+								'<td>', $module instanceof WT_Module_Sidebar ? WT_I18N::translate('yes') : WT_I18N::translate('no'), '</td>',
+								'<td>', $module instanceof WT_Module_Block   ? WT_I18N::translate('yes') : WT_I18N::translate('no'), '</td>',
+								'<td>', $module instanceof WT_Module_Chart   ? WT_I18N::translate('yes') : WT_I18N::translate('no'), '</td>',
+								'<td>', $module instanceof WT_Module_Report  ? WT_I18N::translate('yes') : WT_I18N::translate('no'), '</td>',
+								'<td>', $module instanceof WT_Module_Theme   ? WT_I18N::translate('yes') : WT_I18N::translate('no'), '</td>',
+								'</tr>';
+						} else {
+							// Module can't be found on disk?
+							// Don't delete it automatically.  It may be temporarily missing, after a re-installation, etc.
+							echo
+								'<tr class="error"><td>&nbsp;</td><td>', $module_name, '</td><td>',
+								'<a href="'.WT_SCRIPT_NAME.'?action=delete_module&amp;module_name='.$module_name.'">',
+								WT_I18N::translate('This module cannot be found.  Delete its configuration settings.'),
+								'</a></td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>';
+						}
 					}
 					?>
 				</tbody>
