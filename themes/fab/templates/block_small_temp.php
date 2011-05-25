80feae7012a17dd64eb5c1734e7f5c860edc93ca
Veit Olschinski <veit@olschinski.de>
2011-05-25 07:27:03 +0000

    Some tidy up

--- themes/fab/templates/block_small_temp.php
+++ themes/fab/templates/block_small_temp.php
@@ -1,12 +1,14 @@
 <?php
-// Template for drawing the height-restricted blocks on the portal pages
-//
-// This template expects that the following variables will be set
-// $id - the DOM id for the block div
-// $title - the title of the block
-// $content - the content of the block
-//
-// Copyright (C) 2010 Greg Roach.  All rights reserved.
+/**
+ * Template for drawing the height-restricted blocks on the portal pages
+ *
+ * This template expects that the following variables will be set
+ * $id - the DOM id for the block div
+ * $title - the title of the block
+ * $content - the content of the block
+ *
+ * Copyright (C) 2010 Greg Roach.  All rights reserved.
+ */
 
 if (!defined('WT_WEBTREES')) {
 	header('HTTP/1.0 403 Forbidden');
