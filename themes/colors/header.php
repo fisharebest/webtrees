ac8e7ee244e289354e93df40a44de5ce41c4a85e
Rob Peters <makitso@gmail.com>
2011-06-08 20:24:39 +0000

    Move menu icon layout to single .css file in main colors directory -- for easier maintenance.  Fix some indi icon problems

--- themes/colors/header.php
+++ themes/colors/header.php
@@ -65,8 +65,9 @@ if (!empty($META_GENERATOR)) {
 ?>
 <?php
 	echo $javascript,
-	'<link type="text/css" href="js/jquery/css/jquery-ui.custom.css" rel="Stylesheet" />',
+	'<link rel="stylesheet" href="js/jquery/css/jquery-ui.custom.css "type="text/css"  />',
 	'<link rel="stylesheet" href="', $stylesheet, '" type="text/css" media="all" />',
+	'<link rel="stylesheet" href="', WT_THEME_DIR, 'sprite-menu-icons.css" type="text/css" />',
 	'<link rel="stylesheet" href="', $print_stylesheet,'" type="text/css" media="print" />';
 
 if ((!empty($rtl_stylesheet))&&($TEXT_DIRECTION=="rtl")) {
