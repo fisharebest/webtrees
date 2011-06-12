e016d91ba8c60418e0f4b0a20ddae34f3c3a5f69
Rob Peters <makitso@gmail.com>
2011-06-12 11:20:12 +0000

    Update clouds theme, remove header tables and replace with css

--- themes/clouds/header.php
+++ themes/clouds/header.php
@@ -1,28 +1,32 @@
 <?php
-// Header for Clouds theme
-//
-// webtrees: Web based Family History software
-// Copyright (C) 2011 webtrees development team.
-//
-// Derived from PhpGedView Cloudy theme
-// Original author w.a. bastein http://genealogy.bastein.biz
-// Copyright (C) 2010 PGV Development Team.  All rights reserved.
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-// $Id$
+/**
+ * Header for Clouds theme
+ *
+ * webtrees: Web based Family History software
+ * Copyright (C) 2011 webtrees development team.
+ *
+ * Derived from PhpGedView Cloudy theme
+ * Original author w.a. bastein http://genealogy.bastein.biz
+ * Copyright (C) 2010 PGV Development Team.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @package webtrees
+ * @subpackage Themes
+ * @version $Id$
+ */
 
 if (!defined('WT_WEBTREES')) {
 	header('HTTP/1.0 403 Forbidden');
@@ -93,108 +97,112 @@ echo
 
 // begin header section -->
 if ($view!='simple') {
-	echo '<div id="rapcontainer"><div id="header" class="', $TEXT_DIRECTION, '">';
-?>
-<table class="header" style="background:url('<?php echo WT_THEME_DIR; ?>images/clouds.gif')" >
-<?php
 	echo 
-		'<tr><td align="', $TEXT_DIRECTION=="ltr"?"left":"right", '" valign="middle" >',
-		'<div class="title">';
-	print_gedcom_title_link(TRUE);
-	echo '</div></td>';
-if (empty($SEARCH_SPIDER)) {
-	echo '<td  align="center" valign="middle"><div class="blanco" style="COLOR: #6699ff;" >';
-	if (WT_USER_ID) {
-		echo '<a href="edituser.php" class="link">', WT_I18N::translate('Logged in as '), ' (', WT_USER_NAME, ')</a>', logout_link();
-	} else {
-		echo login_link();
-	}
-	echo '</div></td>',
-		 '<td align="', $TEXT_DIRECTION=="ltr"?"left":"right", '" valign="middle" >';
-?>
-	<div style="white-space: normal;" align="<?php echo $TEXT_DIRECTION=="rtl"?"left":"right"; ?>">
-<?php
-	echo '<form action="search.php" method="post">',
-		 '<input type="hidden" name="action" value="general" />',
-		 '<input type="hidden" name="topsearch" value="yes" />',
-		 '<input type="text" class="formbut" name="query" size="15" value="', WT_I18N::translate('Search'), '" onfocus="if (this.value==\'', WT_I18N::translate('Search'), '\') this.value=\'\'; focusHandler();" onblur="if (this.value==\'\') this.value=\'', WT_I18N::translate('Search'), '\';" />',
-		 '<input type="image" src="', WT_THEME_DIR, 'images/go.gif', '" align="top" alt="', WT_I18N::translate('Search'), '" title="', WT_I18N::translate('Search'), '" />',
-		 '</form>',
-		 '</div>';
-}
-	echo '</td></tr></table></div>';
+	'<div id="clouds-container">',
+		'<div id="header">',
+			'<div class="header" >',
+				'<span class="title">';
+				print_gedcom_title_link(TRUE);
+				echo 
+				'</span>',
+				
+
+				'<div class="hsearch">';
+				if (empty($SEARCH_SPIDER)) {
+					echo 
+					'<form action="search.php" method="post">',
+					'<input type="hidden" name="action" value="general" />',
+					'<input type="hidden" name="topsearch" value="yes" />',
+					'<input type="text" name="query" size="12" value="', WT_I18N::translate('Search'), '"',
+							'onfocus="if (this.value==\'', WT_I18N::translate('Search'), '\') this.value=\'\'; focusHandler();"',
+							'onblur="if (this.value==\'\') this.value=\'', WT_I18N::translate('Search'), '\';" />',
+					'<input align="top" type="image" title="Search" alt="Search" src="themes/clouds/images/go.gif">',
+					'</form>',
+				'</div>';
+				}
+			echo 
+			'</div>',
+	'</div>';
 ?>
 <!-- end header section -->
 <!-- begin menu section -->
 <?php
-echo '<table id="toplinks">',
-	 '<tr>',
-		'<td class="toplinks_left">',
-		'<table align="', $TEXT_DIRECTION=="ltr"?"left":"right", '">',
-			'<tr>';
-	$menu=WT_MenuBar::getGedcomMenu();
-	if ($menu) {
-		$menu->addLabel('', 'none');
-		echo '<td>', $menu->getMenu(), '</td>';
-	}
-	$menu=WT_MenuBar::getMyPageMenu();
-	if ($menu) {
-		$menu->addLabel('', 'none');
-		echo '<td>', $menu->getMenu(), '</td>';
-	}
-	$menu=WT_MenuBar::getChartsMenu();
-	if ($menu) {
-		$menu->addLabel('', 'none');
-		echo '<td>', $menu->getMenu(), '</td>';
-	}
-	$menu=WT_MenuBar::getListsMenu();
-	if ($menu) {
-		$menu->addLabel('', 'none');
-		echo '<td>', $menu->getMenu(), '</td>';
-	}
-	$menu=WT_MenuBar::getCalendarMenu();
-	if ($menu) {
-		$menu->addLabel('', 'none');
-		echo '<td>', $menu->getMenu(), '</td>';
-	}
-	$menu=WT_MenuBar::getReportsMenu();
-	if ($menu) {
-		$menu->addLabel('', 'none');
-		echo '<td>', $menu->getMenu(), '</td>';
-	}
-	$menu=WT_MenuBar::getSearchMenu();
-	if ($menu) {
-		$menu->addLabel('', 'none');
-		echo '<td>', $menu->getMenu(), '</td>';
-	}
-	$menus=WT_MenuBar::getModuleMenus();
+	echo
+		'<div id="topMenu">',
+		'<ul id="main-menu">'; 
+		$menu=WT_MenuBar::getGedcomMenu();
+		if ($menu) {
+			$menu->addIcon(null);
+			echo $menu->getMenuAsList();
+		}
+		$menu=WT_MenuBar::getMyPageMenu();
+		if ($menu) {
+			echo $menu->getMenuAsList();
+		}
+		$menu=WT_MenuBar::getChartsMenu();
+		if ($menu) {
+			$menu->addIcon(null);
+			echo $menu->getMenuAsList();
+		}
+		$menu=WT_MenuBar::getListsMenu();
+		if ($menu) {
+			$menu->addIcon(null);
+			echo $menu->getMenuAsList();
+		}
+		$menu=WT_MenuBar::getCalendarMenu();
+		if ($menu) {
+			$menu->addIcon(null);
+			echo $menu->getMenuAsList();
+		}
+		$menu=WT_MenuBar::getReportsMenu();
+		if ($menu) {
+			$menu->addIcon(null);
+			echo $menu->getMenuAsList();
+		}
+		$menu=WT_MenuBar::getSearchMenu();
+		if ($menu) {
+			$menu->addIcon(null);
+			echo $menu->getMenuAsList();
+		}
+		$menus=WT_MenuBar::getModuleMenus();
 		foreach ($menus as $menu) {
 			if ($menu) {
-				$menu->addLabel('', 'none');
-				echo '<td>', $menu->getMenu(), '</td>';
+				$menu->addIcon(null);
+			echo $menu->getMenuAsList();
 			}
 		}
-	$menu=WT_MenuBar::getHelpMenu();
-	if ($menu) {
-		$menu->addLabel('', 'none');
-		echo '<td>', $menu->getMenu(), '</td>';
-	}
-	echo '</tr></table></td>';
-
+		$menu=WT_MenuBar::getHelpMenu();
+		if ($menu) {
+			$menu->addIcon(null);
+			echo $menu->getMenuAsList();
+		}
+	echo 
+	'</ul>';
 	if (empty($SEARCH_SPIDER)) {
-		echo '<td class="toplinks_right">';
-		echo '<div style="float:', WT_CSS_REVERSE_ALIGN, ';"><ul class="makeMenu">';
+		echo 
+		'<span id="menu-right">',
+		'<ul class="makeMenu">';
+		if (WT_USER_ID) {
+			echo '<li><a href="edituser.php" class="link">', getUserFullName(WT_USER_ID), '</a></li><li>', logout_link(), '</li>';
+			if (WT_USER_CAN_ACCEPT && exists_pending_change()) {
+				echo ' <li><a href="javascript:;" onclick="window.open(\'edit_changes.php\',\'_blank\',\'width=600,height=500,resizable=1,scrollbars=1\'); return false;" style="color:red;">', WT_I18N::translate('Pending changes'), '</a></li>';
+			}
+		} else {
+			echo '<li>', login_link(),'</li>';
+		}
 		echo WT_MenuBar::getFavoritesMenu()->getMenuAsList();
 		$language_menu=WT_MenuBar::getLanguageMenu();
 		if ($language_menu) {
 			echo $language_menu->getMenuAsList();
 		}
-		if (get_gedcom_setting(WT_GED_ID, 'ALLOW_THEME_DROPDOWN') && get_site_setting('ALLOW_USER_THEMES')) {
+		global $ALLOW_THEME_DROPDOWN;
+		if ($ALLOW_THEME_DROPDOWN && get_site_setting('ALLOW_USER_THEMES')) {
 			echo WT_MenuBar::getThemeMenu()->getMenuAsList();
 		}
-		echo '</ul></div></td>';
 	}
-	echo '</tr></table>';
+				echo '</span></div>';
+	
+
 }
 ?>
 <!-- end menu section -->
