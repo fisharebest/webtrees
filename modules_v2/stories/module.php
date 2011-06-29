3649f0b529782321fd6e05666443f97a4453ff4f
Nigel Osborne <kiwi3685@me.com>
2011-06-29 00:23:52 +0000

    Sub-menu for Stories module menu item.

--- modules_v2/stories/module.php
+++ modules_v2/stories/module.php
@@ -417,8 +417,10 @@ class stories_WT_Module extends WT_Module implements WT_Module_Block, WT_Module_
 			}
 			//-- Stories menu item
 			$menu = new WT_Menu($this->getTitle(), 'module.php?mod='.$this->getName().'&amp;mod_action=show_list', 'menu-story', 'down');
-			$menu->addIcon('edit_media');
-			$menu->addClass('menuitem', 'menuitem_hover', 'submenu', 'icon_large_edit_media');		
+			$menu->addClass('menuitem', 'menuitem_hover', 'submenu', '');		
+			$submenu = new WT_Menu($this->getTitle(), 'module.php?mod='.$this->getName().'&amp;mod_action=show_list', 'menu-story-sub');
+			$submenu->addClass('submenuitem', 'submenuitem_hover', 'submenu', '');
+			$menu->addSubmenu($submenu);
 			return $menu;
 		}
 
