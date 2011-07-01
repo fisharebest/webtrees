af8a24a6bc3b95b01e4ce2515763f8ff16fa20e2
Rob Peters <makitso@gmail.com>
2011-07-01 23:17:09 +0000

    Fix W3C validation error.

--- themes/xenea/header.php
+++ themes/xenea/header.php
@@ -1,5 +1,5 @@
 <?php
-// Header for xenea theme
+// Header for colors theme
 //
 // webtrees: Web based Family History software
 // Copyright (C) 2011 webtrees development team.
@@ -27,11 +27,6 @@ if (!defined('WT_WEBTREES')) {
 	header('HTTP/1.0 403 Forbidden');
 	exit;
 }
-
-global $DATE_FORMAT;
-
-$displayDate=timestamp_to_gedcom_date(client_time())->Display(false, $DATE_FORMAT);
-
 echo
 	'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
 	'<html xmlns="http://www.w3.org/1999/xhtml" ', WT_I18N::html_markup(), '>',
@@ -40,8 +35,9 @@ echo
 	'<title>', htmlspecialchars($title), '</title>',
 	header_links($META_DESCRIPTION, $META_ROBOTS, $META_GENERATOR, $LINK_CANONICAL),
 	'<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />',
-	'<link rel="stylesheet" type="text/css" href="js/jquery/css/jquery-ui.custom.css" />',
-	'<link rel="stylesheet" type="text/css" href="', $stylesheet, '" />';
+	'<link rel="stylesheet" href="js/jquery/css/jquery-ui.custom.css" type="text/css"  />',	'<link rel="stylesheet" href="', WT_THEME_DIR, 'css/colors.css" type="text/css" />',
+	'<link rel="stylesheet" href="', $stylesheet, '" type="text/css" media="all" />',
+	'<link rel="stylesheet" href="', $print_stylesheet,'" type="text/css" media="print" />';
 
 switch ($BROWSERTYPE) {
 case 'chrome':
@@ -53,11 +49,11 @@ case 'msie':
 // Additional css files required (Only if Lightbox installed)
 if (WT_USE_LIGHTBOX) {
 	if ($TEXT_DIRECTION=='rtl') {
-		echo '<link rel="stylesheet" type="text/css" href="', WT_MODULES_DIR, 'lightbox/css/clearbox_music_RTL.css" />';
-		echo '<link rel="stylesheet" type="text/css" href="', WT_MODULES_DIR, 'lightbox/css/album_page_RTL_ff.css" media="screen" />';
+		echo	'<link rel="stylesheet" type="text/css" href="', WT_MODULES_DIR, 'lightbox/css/clearbox_music_RTL.css" />',
+		    	'<link rel="stylesheet" type="text/css" href="', WT_MODULES_DIR, 'lightbox/css/album_page_RTL_ff.css" media="screen" />';
 	} else {
-		echo '<link rel="stylesheet" type="text/css" href="', WT_MODULES_DIR, 'lightbox/css/clearbox_music.css" />';
-		echo '<link rel="stylesheet" type="text/css" href="', WT_MODULES_DIR, 'lightbox/css/album_page.css" media="screen" />';
+		echo	'<link rel="stylesheet" type="text/css" href="', WT_MODULES_DIR, 'lightbox/css/clearbox_music.css" />',
+			'<link rel="stylesheet" type="text/css" href="', WT_MODULES_DIR, 'lightbox/css/album_page.css" media="screen" />';
 	}
 }
 echo
@@ -65,8 +61,10 @@ echo
 	$javascript,
 	'</head>',
 	'<body id="body">';
+?>
 
-// Begin Headers 
+<!-- begin header section -->
+<?php
 
 if ($SEARCH_SPIDER) {
 	echo '<div id="header"><span class="title">';
@@ -92,97 +90,99 @@ if ($SEARCH_SPIDER) {
 	'</ul><div>';
 
 // Regular headers
-} elseif ($view!='simple') { // Use "simple" headers for popup windows
-	echo 
+
+} elseif  ($view!='simple') { // Use "simple" headers for popup windows
+	echo
+	// Top row left
 	'<div id="header">',
-		'<span class="title">';
+	'<span class="title">';
 		print_gedcom_title_link(TRUE);
 	echo 
-		'</span>',
-		'<div class="hsearch">';
+	'</span>';
+
+	// Top row right 
 	echo 
-			'<form action="search.php" method="post">',
-			'<input type="hidden" name="action" value="general" />',
-			'<input type="hidden" name="topsearch" value="yes" />',
-			'<input type="text" name="query" size="12" value="', WT_I18N::translate('Search'), '" onfocus="if (this.value==\'', WT_I18N::translate('Search'), '\') this.value=\'\'; focusHandler();" onblur="if (this.value==\'\') this.value=\'', WT_I18N::translate('Search'), '\';" />',
-			'<input type="submit" name="search" value="&gt;" />',
-			'</form>',
-		'</div>',
-	'</div>';
-	echo
-	'<div id="optionsmenu">',
-		'<div id="theme-menu">',
-			'<ul class="makeMenu">';
-	$menu=WT_MenuBar::getThemeMenu();
-	if ($menu) {
-		echo $menu->getMenuAsList();
+	'<div>',
+	'<ul class="makeMenu">';
+
+	if (WT_USER_ID) {
+		echo '<li><a href="edituser.php" class="link">', getUserFullName(WT_USER_ID), '</a></li><li>', logout_link(), '</li>';
+		if (WT_USER_CAN_ACCEPT && exists_pending_change()) {
+			echo ' <li><a href="javascript:;" onclick="window.open(\'edit_changes.php\',\'_blank\',\'width=600,height=500,resizable=1,scrollbars=1\'); return false;" style="color:red;">', WT_I18N::translate('Pending changes'), '</a></li>';
+		}
+	} else {
+		echo '<li>', login_link(),'</li>';
 	}
-	echo
-			'</ul>',
-		'</div>',
-		'<div id="fav-menu">',
-			'<ul class="makeMenu">';
 	$menu=WT_MenuBar::getFavoritesMenu();
 	if ($menu) {
 		echo $menu->getMenuAsList();
 	}
+	$menu=WT_MenuBar::getLanguageMenu();
+	if ($menu) {
+		echo $menu->getMenuAsList();
+	}
+	$menu=WT_MenuBar::getThemeMenu();
+	if ($menu) {
+		echo $menu->getMenuAsList();
+		$allow_color_dropdown=true;
+	} else {
+		$allow_color_dropdown=false;
+	}
 	echo
-			'</ul>',
-		'</div>',
-		'<div id="login-menu">',
-			'<ul class="makeMenu">';
-			if (WT_USER_ID) {
-				echo '<li><a href="edituser.php">', getUserFullName(WT_USER_ID), '</a></li> <li>', logout_link(), '</li>';
-				if (WT_USER_CAN_ACCEPT && exists_pending_change()) {
-					echo ' <li><a href="javascript:;" onclick="window.open(\'edit_changes.php\',\'_blank\',\'width=600,height=500,resizable=1,scrollbars=1\'); return false;" style="color:red;">', WT_I18N::translate('Pending changes'), '</a></li>';
-				}
-			} else {
-				echo '<li>', login_link(), '</li> ';
-			}
-	echo	
-			'</ul>',
-		'</div>';
-	echo
-		'<div id="lang-menu" class="makeMenu">',
-			'<ul class="makeMenu">';
-				$menu=WT_MenuBar::getLanguageMenu();
-				if ($menu) {
-					echo $menu->getMenuAsList();
-				}
-	echo 
-			'</ul>',
-			'<div id="favdate">', $displayDate, '</div>',
-		'</div>',
-	'</div>';
-// Menu 
-		$menu_items=array(
-			WT_MenuBar::getGedcomMenu(),
-			WT_MenuBar::getMyPageMenu(),
-			WT_MenuBar::getChartsMenu(),
-			WT_MenuBar::getListsMenu(),
-			WT_MenuBar::getCalendarMenu(),
-			WT_MenuBar::getReportsMenu(),
-			WT_MenuBar::getSearchMenu(),
-		);
-		foreach (WT_MenuBar::getModuleMenus() as $menu) {
-			$menu_items[]=$menu;
-		}
-		$menu_items[]=WT_MenuBar::getHelpMenu();
+		'<li>',
+			'<form style="display:inline;" action="search.php" method="get">',
+			'<input type="hidden" name="action" value="general" />',
+			'<input type="hidden" name="topsearch" value="yes" />',
+			'<input type="text" name="query" size="15" value="', WT_I18N::translate('Search'), '"', 
+			'onfocus="if (this.value==\'', WT_I18N::translate('Search'), '\') this.value=\'\'; focusHandler();"', 
+			'onblur="if (this.value==\'\') this.value=\'', WT_I18N::translate('Search'), '\';" />',		
+			'<input type="image" src="', WT_THEME_DIR, 'images/go.gif', '" align="top" alt="', WT_I18N::translate('Search'), '" title="', WT_I18N::translate('Search'), '" />',
+			'</form>',
+		'</li>',
+	'</ul>',
+	'</div>',
+	'</div>'; // end header
+
+	// Second Row menu and palette selection
+	// Menu
+	$menu_items=array(
+		WT_MenuBar::getGedcomMenu(),
+		WT_MenuBar::getMyPageMenu(),
+		WT_MenuBar::getChartsMenu(),
+		WT_MenuBar::getListsMenu(),
+		WT_MenuBar::getCalendarMenu(),
+		WT_MenuBar::getReportsMenu(),
+		WT_MenuBar::getSearchMenu(),
+	);
+	foreach (WT_MenuBar::getModuleMenus() as $menu) {
+		$menu_items[]=$menu;
+	}
+	$menu_items[]=WT_MenuBar::getHelpMenu();
 
 	// Print the menu bar
 	echo
-		'<div id="topMenu">',
-		'<ul id="main-menu">';
-	foreach ($menu_items as $menu) {
-		if ($menu) {
+	'<div id="topMenu">',
+		'<ul id="main-menu">'; 
+		foreach ($menu_items as $menu) {
+			if ($menu) {
 			echo $menu->getMenuAsList();
+			}
 		}
-	}
 	unset($menu_items, $menu);
 	echo
-		'</ul>',  // <ul id="main-menu">
-		'</div>', // <div id="topMenu">
-		'<img src="', $WT_IMAGES['hline'], '" width="100%" height="3" alt="" />',
-		'</div>', // <div id="header">
-		'<div id="content">';
+	'</ul>';
+	// Palette
+	if ($allow_color_dropdown) {
+		echo '<div class="toplinks_right">';
+		echo color_theme_dropdown();
+		echo '</div>';
+	}
+
+echo 
+	'</div>'; // close topMenu
 }
+// end header section -->
+?>
+<!-- end menu section -->
+<!-- begin content section -->
+<div id="content">
