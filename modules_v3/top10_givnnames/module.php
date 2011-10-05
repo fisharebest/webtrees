31a49fd431c3456889fcbec94d1ccbf137b2f6df
fisharebest <fisharebest@gmail.com>
2011-10-05 15:49:59 +0000

    Merge duplicate translations that differ only in upper/lower case

--- modules_v3/top10_givnnames/module.php
+++ modules_v3/top10_givnnames/module.php
@@ -152,7 +152,7 @@ class top10_givnnames_WT_Module extends WT_Module implements WT_Module_Block {
 		echo '<tr><td class="descriptionbox wrap width33">';
 		echo WT_I18N::translate('Presentation style');
 		echo '</td><td class="optionbox">';
-		echo select_edit_control('infoStyle', array('list'=>WT_I18N::translate('List'), 'table'=>WT_I18N::translate('Table')), null, $infoStyle, '');
+		echo select_edit_control('infoStyle', array('list'=>WT_I18N::translate('list'), 'table'=>WT_I18N::translate('table')), null, $infoStyle, '');
 		echo '</td></tr>';
 
 		$showUnknown=get_block_setting($block_id, 'showUnknown', true);
