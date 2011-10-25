1bd5fc0fef5c4ba98ecedd85a78ccbb99407c1f2
Łukasz Wilenski <wooc@gazeta.pl>
2011-10-25 16:00:46 +0000

    add sposue lifespan on brnaches

--- branches.php
+++ branches.php
@@ -170,7 +170,7 @@ function print_fams($person, $famid=null) {
 			}
 		$txt .=
 			$spouse->getSexImage().
-			'<a class="'.$class.'" href="'.$spouse->getHtmlUrl().'">'.$spouse->getFullName().' </a>';
+			'<a class="'.$class.'" href="'.$spouse->getHtmlUrl().'">'.$spouse->getFullName().' </a>'.$spouse->getLifeSpan().' '.$sosa2;
 		}
 		echo $txt;
 		echo '<ol>';
