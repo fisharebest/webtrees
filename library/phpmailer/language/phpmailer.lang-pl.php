aebdce9e1d86a7ec80e7906258cc897cd21f7a3c
Łukasz Wilenski <wooc@gazeta.pl>
2011-10-26 16:46:03 +0000

    update phpmailer class from 5.1 to 5.2

--- library/phpmailer/language/phpmailer.lang-pl.php
+++ library/phpmailer/language/phpmailer.lang-pl.php
@@ -3,23 +3,22 @@
 * PHPMailer language file: refer to English translation for definitive list
 * Polish Version
 */
-
-$PHPMAILER_LANG['authenticate'] = 'Błąd SMTP: Nie można przeprowadzić uwierzytelniania.';
-$PHPMAILER_LANG['connect_host'] = 'Błąd SMTP: Nie można połączyć się z wybranym hostem.';
-$PHPMAILER_LANG['data_not_accepted'] = 'Błąd SMTP: Dane nie zostały przyjęte.';
-$PHPMAILER_LANG['empty_message']        = 'Pusta treść wiadomości';
-$PHPMAILER_LANG['encoding'] = 'Nieznany sposób kodowania znaków: ';
-$PHPMAILER_LANG['execute'] = 'Nie można uruchomić: ';
-$PHPMAILER_LANG['file_access'] = 'Brak dostępu do pliku: ';
-$PHPMAILER_LANG['file_open'] = 'Nie można otworzyć pliku: ';
-$PHPMAILER_LANG['from_failed'] = 'Następujący adres Nadawcy jest jest nieprawidłowy: ';
-$PHPMAILER_LANG['instantiate'] = 'Nie można wywołać funkcji mail(). Sprawdź konfigurację serwera.';
-$PHPMAILER_LANG['invalid_email']        = 'Nie wysłano, adres email jest nieprawidłowy: ';
-$PHPMAILER_LANG['provide_address'] = 'Należy podać prawidłowy adres email Odbiorcy.';
-$PHPMAILER_LANG['mailer_not_supported'] = 'Wybrana metoda wysyłania wiadomości nie jest obsługiwana.';
-$PHPMAILER_LANG['recipients_failed'] = 'Błąd SMTP: Następujący odbiorcy są nieprawidłowi: ';
-$PHPMAILER_LANG['signing']              = 'Błąd logowania: ';
-$PHPMAILER_LANG['smtp_connect_failed']  = 'Funkcja SMTP Connect() nie powiodła się.';
-$PHPMAILER_LANG['smtp_error']           = 'Błąd serwera SMTP: ';
-$PHPMAILER_LANG['variable_set']         = 'Nie można ustawić lub zresetować zmiennej: ';
-?>
+$PHPMAILER_LANG = array(
+	'provide_address' => 'Należy podać prawidłowy adres email Odbiorcy.',
+	'mailer_not_supported' => 'Wybrana metoda wysyłania wiadomości nie jest obsługiwana.',
+	'execute' => 'Nie można uruchomić: ',
+	'instantiate' => 'Nie można wywołać funkcji mail(). Sprawdź konfigurację serwera.',
+	'authenticate' => 'Błąd SMTP: Nie można przeprowadzić uwierzytelniania.',
+	'from_failed' => 'Następujący adres Nadawcy jest jest nieprawidłowy: ',
+	'recipients_failed' => 'Błąd SMTP: Następujący odbiorcy są nieprawidłowi: ',
+	'data_not_accepted' => 'Błąd SMTP: Dane nie zostały przyjęte.',
+	'connect_host' => 'Błąd SMTP: Nie można połączyć się z wybranym hostem.',
+	'file_access' => 'Brak dostępu do pliku: ',
+	'file_open' => 'Nie można otworzyć pliku: ',
+	'encoding' => 'Nieznany sposób kodowania znaków: ',
+	'signing' => 'Błąd logowania: ',
+	'smtp_error' => 'Błąd serwera SMTP: ',
+	'empty_message' => 'Pusta treść wiadomości',
+	'invalid_address' => 'Adres email jest nieprawidłowy',
+	'variable_set' => 'Nie można ustawić lub zresetować zmiennej: '
+);
