d5cc1b2d5f9bd9c830e1c785c193757c0e21d909
fisharebest <fisharebest@gmail.com>
2011-11-19 21:36:11 +0000

    Create a dummy file which contains recently deleted messages.  This will allow translators to update the translations for the latest stable releases at the same time as the latest development version.

---
+++ includes/old_messages.php
@@ -0,0 +1,108 @@
+<?php
+// This file contains old messages which have been deleted since the last
+// stable release of webtrees.
+//
+// Moving them here (instead of simply deleting them) allows translators
+// to target both the latest development code as well as the latest
+// stable release.
+//
+// Without this, anyone who is using the latest stable release, and who
+// updates to the latest translation file, will lose some previously
+// translated messages.
+//
+// webtrees: Web based Family History software
+// Copyright (C) 2011 webtrees development team.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// $Id$
+
+exit;
+
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('%1$s (%2$s)');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('%1$s [%2$s]');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('<br />In this box you type your user name.<br /><br /><b>The user name is case sensitive.</b>  This means that <b>MyName</b> is <u>not</u> the same as <b>myname</b> or <b>MYNAME</b>.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('A-Z list of names');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Adding multimedia files (MM) to the GEDCOM is a very nice feature.  Although this program already has a great look without media, if you add pictures or other MM to your relatives, it will only get better.<br /><br /><b>What you should understand about MM.</b><br />There are many formats of MM. Although <b>webtrees</b> can handle most of them, there some things to consider.<br /><ul><li><b>Formats</b><br />Pictures can be edited and saved in many formats.  For example, .jpg, .png, .bmp, .gif, etc.  If the same original picture was used to create each of the formats, the viewed image will appear to be the same size no matter which format is used.  However, the image files stored in the database will vary considerably in size.  Generally, .jpg images are considered to the most efficient in terms of storage space.</li><li><b>Image size</b><br />The larger the original image, the larger will be the resultant file\'s size. The picture should fit on the screen without scrolling; the maximum width or height should not be more than the width or height of the screen. <b>webtrees</b> is designed for screens of 1024x768 pixels but not all of this space is available for viewing pictures; the picture\'s size should be set accordingly.  To reduce file sizes, smaller pictures are more desirable.</li><li><b>Resolution</b><br />The resolution of a picture is usually measured in \"dpi\" (dots/inch), but this is valid only for printed pictures.  When considering pictures shown on screen, the only correct way is to use total dots or pixels. When printed, the picture could have a resolution of 150 - 300 dpi or more depending on the printer. Screen resolutions are rarely better than 50 pixels per inch.  If your picture will never be printed, you can safely lower its resolution (and consequently its file size) without affecting picture quality.  If a low-resolution picture is printed with too great a magnification, its quality will suffer; it will have a grainy appearance.</li><li><b>Color depth</b><br />Another way to keep a file small is to decrease the number of colors that you use.  The number of colors can differ from pure black and white (two colors) to true colors (millions of colors) and anything in between.  You can see that the more colors are used, the bigger the size of the files.</li></ul><b>Why is it important to keep the file size small?</b><br /><ul><li>First of all: Our webspace is limited.  The more large files there are, the more web space we need on the server. The more space we need, the higher our costs.</li><li>Bandwidth.  The more data our server has to send to the remote location (your location), the more we have to pay.  This is because the carrying capacity of the server\'s connection to the Internet is limited, and the link has to be shared (and paid for) by all of the applications running on the server.  <b>webtrees</b> is one of many applications that share the server.  The cost is normally apportioned according to the amount of data each application sends and receives.</li><li>Download time. If you have large files, the user (also you) will have to wait long for the page to download from the server.  Not everybody is blessed with a cable connection, broadband or DSL.</li></ul><b>How to upload your MM</b><br />There are two ways to upload media to the site.  If you have a lot of media items to upload you should contact the site administrator to discuss the best ways.  If it has been enabled by your site administrator, you can use the Upload Media form under your My Page menu.  You can also use the Upload option on the Multimedia form to upload media items.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Alive ');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Alive in Year');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Allows a user to zoom boxes on charts to get more information.<br /><br />Set to <b>Disabled</b> to disable this feature.  Set to <b>On Mouse Over</b> to zoom boxes when the user mouses over the icon in the box.  Set to <b>On Mouse Click</b> to zoom boxes when the user clicks on the icon in the box.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Allows the user to select links to other charts and close relatives of the person.<br /><br />Set to <b>Disabled</b> to disable this feature.  Set to <b>On Mouse Over</b> to popup the links when the user mouses over the icon in the box.  Set to <b>On Mouse Click</b> to popup the links when the user clicks on the icon in the box.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Both alive ');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Both dead ');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Checking this option will remove any custom tags that may have been added to the records by <b>webtrees</b>.<br /><br />Custom tags used by <b>webtrees</b> include the <b>_WT_USER</b> tag which identifies the user who changed a record online and the <b>_THUM</b> tag which tells <b>webtrees</b> that the image should be used as a thumbnail.<br /><br />Custom tags may cause errors when importing the downloaded GEDCOM to another genealogy application.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Choose a letter to show families where a spouse has a given name which starts with that letter.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Choose a letter to show families whose name starts with that letter.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Choose a letter to show individuals who have a given name which starts with that letter.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Choose a letter to show individuals whose family name starts with that letter.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Dead ');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Disabled');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Enter latitude or Longitude');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Enter place name');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Enter search terms');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Ex-Husband');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Ex-Spouse');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Ex-Wife');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Exclude married names');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Families with surname %s');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Here the latitude and longitude can be entered. First select the area you want to set (E/W or N/S). Next enter the value for latitude or longitude. This should be a decimal value.<br />The decimal value can be determined by converting the minutes and seconds using the following formula:<br />degrees_decimal = ((seconds / 60) + minutes) / 60 + degrees.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Here you can enter or change the name of the place.<br />The \'Search on this level\' option allow to search the latitude and longitude of entered place name only among the places with that level.<br />The \'Search on this level\' option allow to search the latitude and longitude of all places having entered name. Some the lower levels places can not be displayed with this method of search.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('ID');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('If a person’s name is not known, it should be left blank.  If you enter “Unknown” as a name, it will be listed under “U”.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('If you have changed your password, you need to confirm it as well.  This is just to make sure that you did not make a typing error in the password field.<br /><br />If the password and its confirmation are not identical, you will get a suitable error message.  You will have to re-type both the original password and its confirmation.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Image viewer');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('In this box you can change your real name.  This is the name that other users see when you are logged in.<br /><br />Although the choice of what to put into this field is yours, you should inform the administrator when you change it.  When others see an unknown person on-line, they might wonder and ask questions.  The admin can find out without having received your notice, but you should save him that unnecessary work.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('In this box you have to type your real name.<br /><br />We need your first and last names to determine whether you qualify for an account at this site, and what your rights should be.  This name will be visible to other logged-in family members and users.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('In this box you type your password.<br /><br /><b>The password is case sensitive.</b>  This means that <b>MyPassword</b> is <u>not</u> the same as <b>mypassword</b> or <b>MYPASSWORD</b>.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Include married names');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Include people whose gender is unknown');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Links to charts, families, and close relatives of this person. Click this icon to view this page, starting at this person.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('On Mouse Click');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('On Mouse Down');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('On Mouse Over');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Partner');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Passwords must be at least 6 characters long and are case-sensitive, so that \"s3CR#t\" is different to \"S3CR#t\".');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('PopUp links on charts');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Remove custom tags');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Remove custom webtrees tags? (eg. _WT_USER, _THUM)');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Result Page');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Results per page');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Search all');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Search on this level');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Show married names on individual list');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Show persons alive in the indicated year.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Show surname lists');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Skip surname lists');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Sort by this column.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('The A-Z list allows you to select all names beginning with a certain letter.  Unknown names are listed under “…” and people who do not have a surname are listed under “None”.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('The individual and family list pages can either include or exclude married names.  This option can be helpful when searching for individuals or families where you only know the married name.  Married names can only be included if they already exist in the database.<br /><br />On the family list, this value defaults to exclude.  On the individual list, the default value is set in the GEDCOM Configuration page.<br /><br />When you change this option, your selection will be remembered until you log off or your session ends.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('The script timed out before a relationship could be found.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('The standard setting is that, after you have clicked a letter of the Alphabetical index, you will get a sub-list with surnames.  If you click this link, all individuals with surnames that have the currently selected initial letter will be displayed immediately. Thereafter, the list of individuals will be displayed directly whenever you click on a new initial letter in the Alphabetical list.<br /><br />To reverse this action, click on the Show Surname lists link.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('This option will show the married names of females on the Individual list.  This option requires that you calculate the married names when you import the GEDCOM file.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('This will be used to send you password reminders, site notifications and messages from other family members who register on your site.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('To Repository');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('To Shared Note');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Total events: %s');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Total surnames: %s');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Unable to authenticate user.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('User name');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('You can change your username by updating it here.  Usernames are case insensitive and ignore accented letters, so that \"chloe\", \"chlo&euml;\" and \"CHLOE\" are considered to be the same.  Usernames may not contain the following characters: &lt;&gt;\"%%{};');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Your correct email address is important to us to keep in touch with you.<br /><br />If you get a new email address, as usually happens when you change your Internet provider, please do not forget to change the address here as well.  You won\'t get a confirmation message from this site when you change this address, but any future messages directed to you will go this new address.');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate('Zoom boxes on charts');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate_c('East', 'E');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate_c('North', 'N');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate_c('South', 'S');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate_c('West', 'W');
+/* I18N: This message is deleted in webtrees-1.2.5  */ WT_I18N::translate_c('first page', 'first');
