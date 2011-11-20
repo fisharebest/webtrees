f050f715286bb3c869247a77588c8f80582a43d6
Rob Peters <makitso@gmail.com>
2011-11-20 22:09:39 +0000

    Remove obsolete variables and update comments

--- themes/clouds/theme.php
+++ themes/clouds/theme.php
@@ -186,21 +186,19 @@ $fanChart = array(
 //-- This section defines variables for the charts
 $bwidth = 260; // -- width of boxes on all person-box based charts
 $bheight = 80; // -- height of boxes on all person-box based chart
-$baseyoffset = 10; // -- position the entire pedigree tree relative to the top of the page
-$basexoffset = 10; // -- position the entire pedigree tree relative to the left of the page
+$baseyoffset = 10; // -- position the timeline chart relative to the top of the page
+$basexoffset = 10; // -- position the pedigree and timeline charts relative to the left of the page
 $bxspacing = 4; // -- horizontal spacing between boxes on the pedigree chart
 $byspacing = 5; // -- vertical spacing between boxes on the pedigree chart
-$brborder = 1; // -- box right border thickness
+$brborder = 1; // -- pedigree chart box right border thickness 
 
-// -- global variables for the descendancy chart
-$Dbaseyoffset = 20; // -- position the entire descendancy tree relative to the top of the page
-$Dbasexoffset = 20; // -- position the entire descendancy tree relative to the left of the page
-$Dbxspacing = 0; // -- horizontal spacing between boxes
-$Dbyspacing = 10; // -- vertical spacing between boxes
-$Dbwidth = 250; // -- width of DIV layer boxes
-$Dbheight = 78; // -- height of DIV layer boxes
-$Dindent = 15; // -- width to indent descendancy boxes
-$Darrowwidth = 30; // -- additional width to include for the up arrows
+//-- Other settings that should not be touched
+$Dbxspacing = 0; // -- position vertical line between boxes in relationship chart
+$Dbyspacing = 10; // -- position vertical spacing between boxes in relationship chart
+$Dbwidth = 250; // -- horizontal spacing between boxes in all charts
+$Dbheight = 78; // -- horizontal spacing between boxes in all charts
+$Dindent = 15; // -- width to indent ancestry and descendancy charts boxes
+$Darrowwidth = 300; // -- not used that I can see ***
 
 // --  The largest possible area for charts is 300,000 pixels. As the maximum height or width is 1000 pixels
 $WT_STATS_S_CHART_X = "440";
