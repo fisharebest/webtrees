8fca2a53f403b4e81e0d02d1a13a2f57b4f595ed
fisharebest <fisharebest@gmail.com>
2012-01-17 10:48:42 +0000

    Refactor family book chart into separate view and controller

--- familybook.php
+++ familybook.php
@@ -27,206 +27,20 @@ define('WT_SCRIPT_NAME', 'familybook.php');
 require './includes/session.php';
 require_once WT_ROOT.'includes/functions/functions_charts.php';
 
-$controller=new WT_Controller_Chart();
+$controller=new WT_Controller_Familybook();
 
-// Extract form variables
-$pid        =safe_GET_xref('rootid');
-$show_full  =safe_GET('show_full',     array('0', '1'), $PEDIGREE_FULL_DETAILS);
-$show_spouse=safe_GET('show_spouse',   '1', '0');
-$descent    =safe_GET_integer('descent',       0, 9, 5);
-$generations=safe_GET_integer('generations',   2, $MAX_DESCENDANCY_GENERATIONS, 2);
-$box_width  =safe_GET_integer('box_width',     50, 300, 100);
-
-// -- size of the detailed boxes based upon optional width parameter
-$Dbwidth=($box_width*$bwidth)/100;
-$Dbheight=($box_width*$bheight)/100;
-$bwidth=$Dbwidth;
-$bheight=$Dbheight;
-		
-// -- adjust size of the non-detailed boxes
-if ($show_full==false) {
-	$bwidth = $bwidth / 1.5;
-	$bheight = $bheight / 2 ;
-}
-
-// -- root id
-$person=WT_Person::getInstance($pid);
-if (!$person) {
-	$person=$controller->getSignificantIndividual();
-}
-$name=$person->getFullName();
-
-function print_descendency($person, $count) {
-	global $show_spouse, $dgenerations, $bwidth, $bheight, $bhalfheight;
-	global $TEXT_DIRECTION, $WT_IMAGES, $generations, $box_width, $show_full;
-
-	if ($count>=$dgenerations) {
-		return 0;
-	}
-
-	$pid=$person->getXref();
-
-	echo "<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\">";
-	echo "<tr>";
-	echo "<td width=\"".($bwidth-2)."\">";
-	$numkids = 0;
-	$sfamilies=$person->getSpouseFamilies();
-	if (count($sfamilies)>0) {
-		$firstkids = 0;
-		foreach ($sfamilies as $family) {
-			$children=$family->getChildren();
-			if (count($children)>0) {
-				echo "<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\">";
-				foreach ($children as $i=>$child) {
-					$rowspan = 2;
-					if ($i>0 && $i<count($children)-1) {
-						$rowspan=1;
-					}
-					echo "<tr><td rowspan=\"$rowspan\" width=\"$bwidth\" style=\"padding-top: 2px;\">";
-					if ($count < $dgenerations-1) {
-						$kids = print_descendency($child, $count+1);
-						if ($i==0) $firstkids = $kids;
-						$numkids += $kids;
-					} else {
-						print_pedigree_person($child);
-						$numkids++;
-					}
-					echo "</td>";
-					$twidth = 7;
-					if (count($children)==1) {
-						$twidth+=3;
-					}
-					echo "<td rowspan=\"$rowspan\"><img class=\"line4\" src=\"".$WT_IMAGES["hline"]."\" width=\"$twidth\" height=\"3\" alt=\"\"></td>";
-					if (count($children)>1) {
-						if ($i==0) {
-							echo "<td height=\"".($bhalfheight+3)."\"><img class=\"lineb\" src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td></tr>";
-							echo "<tr><td height=\"".($bhalfheight+3)."\" style=\"background: url('".$WT_IMAGES["vline"]."');\"><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td>";
-						}
-						else if ($i==count($children)-1) {
-							echo "<td height=\"".($bhalfheight+4)."\" style=\"background: url('".$WT_IMAGES["vline"]."');\"><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td></tr>";
-							echo "<tr><td height=\"".($bhalfheight+4)."\"><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td>";
-						}
-						else {
-							echo "<td style=\"background: url('".$WT_IMAGES["vline"]."');\"><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td>";
-						}
-					}
-					echo "</tr>";
-				}
-				echo "</table>";
-			}
-		}
-		echo "</td>";
-		echo "<td width=\"$bwidth\">";
-	}
-
-	if ($numkids==0) {
-		$numkids = 1;
-		$tbwidth = $bwidth+16;
-		for ($j=$count; $j<$dgenerations; $j++) {
-			echo "</td><td width=\"$bwidth\">";
-		}
-	}
-	//-- add offset divs to make things line up better
-	if ($show_spouse) {
-		foreach ($sfamilies as $family) {
-			echo '<br><div style="height:', $bheight, 'px;width:', $bwidth, 'px;"><br></div>';
-		}
-	}
-	print_pedigree_person($person);
-
-	if ($show_spouse) {
-		foreach ($sfamilies as $family) {
-			echo '<br>', $family->getMarriage()->print_simple_fact();
-			print_pedigree_person($family->getSpouse($person));
-		}
-	}
-
-	echo "</td></tr>";
-	echo "</table>";
-	return $numkids;
-}
-
-function print_person_pedigree($person, $count) {
-	global $generations, $SHOW_EMPTY_BOXES, $WT_IMAGES, $bheight, $bhalfheight;
-	if ($count>=$generations || !$person) return;
-	$hheight = ($bhalfheight+3) * pow(2,($generations-$count-1));
-	foreach ($person->getChildFamilies() as $family) {
-		echo "<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\" style=\"empty-cells: show;\">";
-		$height="100%";
-		echo "<tr>";
-		if ($count<$generations-1) {
-			echo "<td height=\"".$hheight."\"><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td>";
-			echo "<td rowspan=\"2\"><img src=\"".$WT_IMAGES["hline"]."\" width=\"7\" height=\"3\" alt=\"\"></td>";
-		}
-		echo "<td rowspan=\"2\">";
-		print_pedigree_person($family->getHusband());
-		echo "</td>";
-		echo "<td rowspan=\"2\">";
-		print_person_pedigree($family->getHusband(), $count+1);
-		echo "</td>";
-		echo "</tr><tr><td height=\"".$hheight."\"";
-		if ($count<$generations-1) {
-			echo " style=\"background: url('".$WT_IMAGES["vline"]."');\" ";
-		}
-		echo "><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td></tr><tr>";
-		if ($count<$generations-1) {
-			echo "<td height=\"".$hheight."\" style=\"background: url('".$WT_IMAGES["vline"]."');\"><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td>";
-			echo "<td rowspan=\"2\"><img src=\"".$WT_IMAGES["hline"]."\" width=\"7\" height=\"3\" alt=\"\"></td>";
-		}
-		echo "<td rowspan=\"2\">";
-		print_pedigree_person($family->getWife());
-		echo "</td>";
-		echo "<td rowspan=\"2\">";
-		print_person_pedigree($family->getWife(), $count+1);
-		echo "</td>";
-		echo "</tr>";
-		if ($count<$generations-1) {
-			echo "<tr><td height=\"".$hheight."\"><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td></tr>";
-		}
-		echo "</table>";
-	}
-}
-
-function print_family_book($person, $descent) {
-	global $generations, $dgenerations, $firstrun;
-
-	if ($descent==0 || !$person->canDisplayName()) {
-		return;
-	}
-	$families=$person->getSpouseFamilies();
-	if (count($families)>0 || empty($firstrun)) {
-		$firstrun=true;
-		echo
-			'<h2 style="text-align:center">',
-			/* I18N: A title/heading. %s is a person's name */ WT_I18N::translate('Family of %s', $person->getFullName()),
-			'</h2><table cellspacing="0" cellpadding="0" border="0"><tr><td valign="middle">';
-		$dgenerations = $generations;
-		print_descendency($person, 1);
-		echo '</td><td valign="middle">';
-		print_person_pedigree($person, 1);
-		echo '</td></tr></table><br><br><hr style="page-break-after:always;"><br><br>';
-
-		foreach ($families as $family) {
-			foreach ($family->getChildren() as $child) {
-				print_family_book($child, $descent-1);
-			}
-		}
-	}
-}
-
-$controller->setPageTitle(/* I18N: %s is a person's name */ WT_I18N::translate('Family book of %s', $person->getFullName()));
 $controller->pageHeader();
 
-if ($ENABLE_AUTOCOMPLETE) require WT_ROOT.'js/autocomplete.js.htm';
+if ($ENABLE_AUTOCOMPLETE) {
+	require WT_ROOT.'js/autocomplete.js.htm';
+}
 
-// LBox =====================================================================================
 if (WT_USE_LIGHTBOX) {
 	require WT_ROOT.WT_MODULES_DIR.'lightbox/functions/lb_call_js.php';
 }
-// ==========================================================================================
 
 echo '<table><tr><td valign="top">';
-echo '<h2>'.WT_I18N::translate('Family book of %s', $person->getFullName()), '</h2>';
+echo '<h2>'.$controller->getPageTitle(), '</h2>';
 ?>
 
 <script type="text/javascript">
@@ -242,9 +56,6 @@ echo '<h2>'.WT_I18N::translate('Family book of %s', $person->getFullName()), '</
 //-->
 </script>
 
-<?php
-$gencount=0;
-?>
 </td><td width="50px">&nbsp;</td><td><form method="get" name="people" action="?">
 <table><tr>
 
@@ -252,17 +63,17 @@ $gencount=0;
 	<?php echo WT_I18N::translate('Individual'); ?>
 </td>
 <td class="optionbox">
-	<input class="pedigree_form" type="text" name="rootid" id="rootid" size="3" value="<?php echo $pid; ?>">
-	<?php print_findindi_link("pid",""); ?>
+	<input class="pedigree_form" type="text" name="rootid" id="rootid" size="3" value="<?php echo $controller->rootid; ?>">
+	<?php print_findindi_link('rootid',''); ?>
 </td>
 
 <td class="descriptionbox">
 <?php echo WT_I18N::translate('Show Details'); ?>
 </td>
 <td class="optionbox">
-<input type="hidden" name="show_full" value="<?php echo $show_full; ?>">
+<input type="hidden" name="show_full" value="<?php echo $controller->show_full; ?>">
 <input type="checkbox" value="<?php
-	if ($show_full) echo "1\" checked=\"checked\" onclick=\"document.people.show_full.value='0';";
+	if ($controller->show_full) echo "1\" checked=\"checked\" onclick=\"document.people.show_full.value='0';";
 else echo "0\" onclick=\"document.people.show_full.value='1';"; ?>">
 </td>
 
@@ -278,7 +89,7 @@ else echo "0\" onclick=\"document.people.show_full.value='1';"; ?>">
 <?php
 for ($i=2; $i<=$MAX_DESCENDANCY_GENERATIONS; $i++) {
 	echo "<option value=\"".$i."\"" ;
-	if ($i == $generations) echo " selected=\"selected\"";
+	if ($i == $controller->generations) echo " selected=\"selected\"";
 	echo ">".WT_I18N::number($i)."</option>";
 }
 ?>
@@ -291,13 +102,13 @@ for ($i=2; $i<=$MAX_DESCENDANCY_GENERATIONS; $i++) {
 <td class="optionbox">
 <input type="checkbox" value="1" name="show_spouse"
 <?php
-if ($show_spouse) echo " checked=\"checked\""; ?>>
+if ($controller->show_spouse) echo " checked=\"checked\""; ?>>
 </td></tr>
 
 <tr><td class="descriptionbox">
 	<?php echo WT_I18N::translate('Box width'); ?>
 </td>
-<td class="optionbox"><input type="text" size="3" name="box_width" value="<?php echo $box_width; ?>">
+<td class="optionbox"><input type="text" size="3" name="box_width" value="<?php echo $controller->box_width; ?>">
 <b>%</b>
 </td>
 
@@ -306,7 +117,7 @@ if ($show_spouse) echo " checked=\"checked\""; ?>>
 <tr><td class="descriptionbox">
 	<?php echo WT_I18N::translate('Descent Steps'), help_link('fambook_descent'); ?>
 </td>
-<td class="optionbox"><input type="text" size="3" name="descent" value="<?php echo $descent; ?>">
+<td class="optionbox"><input type="text" size="3" name="descent" value="<?php echo $controller->descent; ?>">
 </td>
 
 <td class="descriptionbox">&nbsp;</td><td class="optionbox">&nbsp;</td></tr>
@@ -315,11 +126,16 @@ if ($show_spouse) echo " checked=\"checked\""; ?>>
 
 <?php
 
+if ($controller->error_message) {
+	echo '<p class="ui-state-error">', $controller->error_message, '</p>';
+	exit;
+}
+
 echo
 	'<div id="familybook_chart',
 	($TEXT_DIRECTION=="ltr")?"":"_rtl",
 	'" style="width:98%; z-index:1;">';
 
-print_family_book($person, $descent);
+$controller->print_family_book($controller->root, $controller->descent);
 
 echo '</div>';
---
+++ library/WT/Controller/Familybook.php
@@ -0,0 +1,240 @@
+<?php
+//	Controller for the compact chart
+//
+// webtrees: Web based Family History software
+// Copyright (C) 2012 webtrees development team.
+//
+// Derived from PhpGedView
+// Copyright (C) 2002 to 2009  PGV Development Team.  All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// $Id$
+
+if (!defined('WT_WEBTREES')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+require_once WT_ROOT.'includes/functions/functions_charts.php';
+
+class WT_Controller_Familybook extends WT_Controller_Chart {
+	// Data for the view
+	public $show_full  =null;
+	public $show_spouse=null;
+	public $descent    =null;
+	public $generations=null;
+	public $box_width  =null;
+
+	// Data for the controller
+	private $dgenerations=null;
+
+	public function __construct() {
+		parent::__construct();
+
+		$PEDIGREE_FULL_DETAILS      =get_gedcom_setting(WT_GED_ID, 'PEDIGREE_FULL_DETAILS');
+		$MAX_DESCENDANCY_GENERATIONS=get_gedcom_setting(WT_GED_ID, 'MAX_DESCENDANCY_GENERATIONS');
+
+		// Extract the request parameters
+		$this->show_full  =safe_GET('show_full',     array('0', '1'), $PEDIGREE_FULL_DETAILS);
+		$this->show_spouse=safe_GET('show_spouse',   '1', '0');
+		$this->descent    =safe_GET_integer('descent',       0, 9, 5);
+		$this->generations=safe_GET_integer('generations',   2, $MAX_DESCENDANCY_GENERATIONS, 2);
+		$this->box_width  =safe_GET_integer('box_width',     50, 300, 100);
+
+		// Box sizes are set globally in the theme.  Modify them here.
+		global $bwidth, $bheight, $Dbwidth, $Dbheight;
+		$Dbwidth =$this->box_width * $bwidth  / 100;
+		$Dbheight=$this->box_width * $bheight / 100;
+		$bwidth  =$Dbwidth;
+		$bheight =$Dbheight;
+		
+		// -- adjust size of the non-detailed boxes
+		if ($this->show_full==false) {
+			$bwidth = $bwidth / 1.5;
+			$bheight = $bheight / 2 ;
+		}
+
+		if ($this->root && $this->root->canDisplayName()) {
+			$this->setPageTitle(
+				/* I18N: %s is a person's name */
+				WT_I18N::translate('Family book of %s', $this->root->getFullName())
+			);
+		} else {
+			$this->setPageTitle(WT_I18N::translate('Family book'));
+		}
+	}
+
+	function print_descendency($person, $count) {
+		global $bwidth, $bheight, $bhalfheight;
+		global $TEXT_DIRECTION, $WT_IMAGES;
+
+		// print_pedigree_person() requires these globals.
+		global $show_full, $box_width;
+		$show_full=$this->show_full;
+		$box_width=$this->box_width;
+
+		if ($count>=$this->dgenerations) {
+			return 0;
+		}
+
+		echo "<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\">";
+		echo "<tr>";
+		echo "<td width=\"".($bwidth-2)."\">";
+		$numkids = 0;
+		$sfamilies=$person->getSpouseFamilies();
+		if (count($sfamilies)>0) {
+			$firstkids = 0;
+			foreach ($sfamilies as $family) {
+				$children=$family->getChildren();
+				if (count($children)>0) {
+					echo "<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\">";
+					foreach ($children as $i=>$child) {
+						$rowspan = 2;
+						if ($i>0 && $i<count($children)-1) {
+							$rowspan=1;
+						}
+						echo "<tr><td rowspan=\"$rowspan\" width=\"$bwidth\" style=\"padding-top: 2px;\">";
+						if ($count < $this->dgenerations-1) {
+							$kids = $this->print_descendency($child, $count+1);
+							if ($i==0) $firstkids = $kids;
+							$numkids += $kids;
+						} else {
+							print_pedigree_person($child);
+							$numkids++;
+						}
+						echo "</td>";
+						$twidth = 7;
+						if (count($children)==1) {
+							$twidth+=3;
+						}
+						echo "<td rowspan=\"$rowspan\"><img class=\"line4\" src=\"".$WT_IMAGES["hline"]."\" width=\"$twidth\" height=\"3\" alt=\"\"></td>";
+						if (count($children)>1) {
+							if ($i==0) {
+								echo "<td height=\"".($bhalfheight+3)."\"><img class=\"lineb\" src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td></tr>";
+								echo "<tr><td height=\"".($bhalfheight+3)."\" style=\"background: url('".$WT_IMAGES["vline"]."');\"><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td>";
+							}
+							else if ($i==count($children)-1) {
+								echo "<td height=\"".($bhalfheight+4)."\" style=\"background: url('".$WT_IMAGES["vline"]."');\"><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td></tr>";
+								echo "<tr><td height=\"".($bhalfheight+4)."\"><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td>";
+							}
+							else {
+								echo "<td style=\"background: url('".$WT_IMAGES["vline"]."');\"><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td>";
+							}
+						}
+						echo "</tr>";
+					}
+					echo "</table>";
+				}
+			}
+			echo "</td>";
+			echo "<td width=\"$bwidth\">";
+		}
+
+		if ($numkids==0) {
+			$numkids = 1;
+			$tbwidth = $bwidth+16;
+			for ($j=$count; $j<$this->dgenerations; $j++) {
+				echo "</td><td width=\"$bwidth\">";
+			}
+		}
+		//-- add offset divs to make things line up better
+		if ($this->show_spouse) {
+			foreach ($sfamilies as $family) {
+				echo '<br><div style="height:', $bheight, 'px;width:', $bwidth, 'px;"><br></div>';
+			}
+		}
+		print_pedigree_person($person);
+
+		if ($this->show_spouse) {
+			foreach ($sfamilies as $family) {
+				echo '<br>', $family->getMarriage()->print_simple_fact();
+				print_pedigree_person($family->getSpouse($person));
+			}
+		}
+
+		echo "</td></tr>";
+		echo "</table>";
+		return $numkids;
+	}
+
+	function print_person_pedigree($person, $count) {
+		global $SHOW_EMPTY_BOXES, $WT_IMAGES, $bheight, $bhalfheight;
+		if ($count>=$this->generations || !$person) return;
+		$hheight = ($bhalfheight+3) * pow(2,($this->generations-$count-1));
+		foreach ($person->getChildFamilies() as $family) {
+			echo "<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\" style=\"empty-cells: show;\">";
+			$height="100%";
+			echo "<tr>";
+			if ($count<$this->generations-1) {
+				echo "<td height=\"".$hheight."\"><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td>";
+				echo "<td rowspan=\"2\"><img src=\"".$WT_IMAGES["hline"]."\" width=\"7\" height=\"3\" alt=\"\"></td>";
+			}
+			echo "<td rowspan=\"2\">";
+			print_pedigree_person($family->getHusband());
+			echo "</td>";
+			echo "<td rowspan=\"2\">";
+			$this->print_person_pedigree($family->getHusband(), $count+1);
+			echo "</td>";
+			echo "</tr><tr><td height=\"".$hheight."\"";
+			if ($count<$this->generations-1) {
+				echo " style=\"background: url('".$WT_IMAGES["vline"]."');\" ";
+			}
+			echo "><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td></tr><tr>";
+			if ($count<$this->generations-1) {
+				echo "<td height=\"".$hheight."\" style=\"background: url('".$WT_IMAGES["vline"]."');\"><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td>";
+				echo "<td rowspan=\"2\"><img src=\"".$WT_IMAGES["hline"]."\" width=\"7\" height=\"3\" alt=\"\"></td>";
+			}
+			echo "<td rowspan=\"2\">";
+			print_pedigree_person($family->getWife());
+			echo "</td>";
+			echo "<td rowspan=\"2\">";
+			$this->print_person_pedigree($family->getWife(), $count+1);
+			echo "</td>";
+			echo "</tr>";
+			if ($count<$this->generations-1) {
+				echo "<tr><td height=\"".$hheight."\"><img src=\"".$WT_IMAGES["spacer"]."\" width=\"3\" alt=\"\"></td></tr>";
+			}
+			echo "</table>";
+		}
+	}
+
+	function print_family_book($person, $descent) {
+		global $firstrun;
+
+		if ($descent==0 || !$person->canDisplayName()) {
+			return;
+		}
+		$families=$person->getSpouseFamilies();
+		if (count($families)>0 || empty($firstrun)) {
+			$firstrun=true;
+			echo
+				'<h2 style="text-align:center">',
+				/* I18N: A title/heading. %s is a person's name */ WT_I18N::translate('Family of %s', $person->getFullName()),
+				'</h2><table cellspacing="0" cellpadding="0" border="0"><tr><td valign="middle">';
+			$this->dgenerations = $this->generations;
+			$this->print_descendency($person, 1);
+			echo '</td><td valign="middle">';
+			$this->print_person_pedigree($person, 1);
+			echo '</td></tr></table><br><br><hr style="page-break-after:always;"><br><br>';
+
+			foreach ($families as $family) {
+				foreach ($family->getChildren() as $child) {
+					$this->print_family_book($child, $descent-1);
+				}
+			}
+		}
+	}
+}
