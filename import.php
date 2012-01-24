84752d93cf0e3f443ac7bfe86d1201f22dff2b25
Nigel Osborne <kiwi3685@me.com>
2012-01-24 23:22:50 +0000

    Error importing ANSI formatted GEDCOM file - Bug #921281

--- import.php
+++ import.php
@@ -142,7 +142,7 @@ for ($end_time=microtime(true)+1.0; microtime(true)<$end_time; ) {
 			break;
 		case 'ANSI': // ANSI could be anything.  Most applications seem to treat it as latin1.
 			$controller->addInlineJavaScript(
-				'alert("', /* I18N: %1$s and %2$s are the names of character encodings, such as ISO-8859-1 or ASCII */ WT_I18N::translate('This GEDCOM is encoded using %1$s.  Assume this to mean %2$s.', $charset, 'ISO-8859-1'), '");'
+				'alert("'. /* I18N: %1$s and %2$s are the names of character encodings, such as ISO-8859-1 or ASCII */ WT_I18N::translate('This GEDCOM is encoded using %1$s.  Assume this to mean %2$s.', $charset, 'ISO-8859-1'). '");'
 			);
 		case 'WINDOWS':
 		case 'CP1252':
