bdf0032886d6930998cecbfcf2658ae875f62ad8
Rob Peters <makitso@gmail.com>
2012-02-01 22:38:58 +0000

    Revert z-index change in 13348 patch

--- relationship.php
+++ relationship.php
@@ -516,12 +516,8 @@ if ((!empty($pid1))&&(!empty($pid2))) {
 					echo '</div>';
 				}
 
-				// Test to see which way the blocks flow
-				if ($pid1>$pid2) {
-					$zIndex = 200 - ($colNum * $depth + $rowNum);
-				} else {
-					$zIndex = 200 + ($colNum * $depth + $rowNum);
-				}
+				// Determine the z-index for this box
+				$zIndex = 200 - ($colNum * $depth + $rowNum);
 
 				echo "<div id=\"box$pid.0\" style=\"position:absolute; ".($TEXT_DIRECTION=='ltr'?'left':'right').':'.$pxoffset.'px; top:'.$pyoffset.'px; width:'.$Dbwidth.'px; height:'.$Dbheight.'px; z-index:'.$zIndex.";\"><table><tr><td colspan=\"2\" width=\"$Dbwidth\" height=\"$Dbheight\">";
 				print_pedigree_person(WT_Person::getInstance($pid), 1);
