1508c5ccd31e586a9bc60a8a70d68891aa108a0e
fisharebest <fisharebest@gmail.com>
2012-02-13 10:23:47 +0000

    Updates for 1.2.7 release

--- includes/old_messages.php
+++ includes/old_messages.php
@@ -31,6 +31,9 @@
 
 exit;
 
+// MESSAGES DELETED IN WEBTREES-1.2.8
+
+
 // MESSAGES DELETED IN WEBTREES-1.2.7
 
 /* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('%d day ago', '%d days ago', 0, 0);
@@ -100,64 +103,3 @@ exit;
 /* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('grandchildren');
 /* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('webtrees user login');
 /* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('webtrees wiki');
-
-// MESSAGES DELETED IN WEBTREES-1.2.6
-
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('A pedigree is an enumeration of all ancestors of the starting person.  Users who are not logged in see the pedigree of the starting (root) person chosen by the site administrator.  Logged in users can select their own starting (root) person.<br /><br />In this context, "All ancestors" means the father and mother, their parents, and so on.  The pedigree is displayed graphically; you don\'t have to struggle through pages of text to determine your ancestors.<br /><br />All individuals are displayed in Name boxes on the screen.<br /><ul><li><b>Name boxes on the pedigree</b><br />If the Pedigree page is set to show details, you will see the person\'s name and birth and death dates.  You can click on a person\'s name to take you directly to the Individual Information page of that person.<br /><br />When <b>Show details</b> is on there are two icons inside the name box.</li><li><b>Pedigree icon inside the Name box</b><br />When the option <b>Show Details</b> is on, you see a Pedigree icon in the Name box. Depending on the site settings, you have to hover over the icon or click on it.  When you click on or hover over this icon, a small sub-menu appears.<br /><br />The items <b>Pedigree Tree</b> and <b>Descendancy Chart</b> are similar to those items in the main menu, but the difference is that the starting person is now the individual of mentioned in the Name box.  You also see <b>Family with Spouse</b>. Underneath that you see the name of the spouse followed by the names of the children.  All names are clickable.</li><li><b>Magnifying glass inside the Name box</b><br />Depending on the site settings, you have to hover over the icon or click on it.  This magnifies the Name box so that more details will be displayed.  You will see more dates and events. Names are clickable.</li><li><b>Arrows</b><br />On the left or right of the leftmost or rightmost Name boxes you may see arrows.  When you click on these arrows the screen display will shift in the direction of the arrow.</li></ul>');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('After generating the sitemap files, you should upload them to the webtrees installation directory on the web-server.');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Align left');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Align right');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Boundaries for Z axis');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Center');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Enter the name to be used in the &laquo;From:&raquo; field of e-mails originating at this site.<br /><br />For example, if your name is <b>John Smith</b> and you are the site administrator for a site that is  known as <b>Jones Genealogy</b>, you could enter something like <b>John Smith</b> or <b>Jones Genealogy</b> or even <b>John Smith, Administrator: Jones Genealogy</b>.  You may enter whatever you wish, but HTML is not permitted.');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Family information');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Find all places called “%s” on the map');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Find “%s” on the map');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('For example, <b>interval 2 years</b> describes the following set of age ranges:<div style="padding-left:30px;">younger than 16 years<br />16 to 18<br />19 to 20<br />21 to 22<br />23 to 24<br />25 to 26<br />27 to 28<br />29 to 30<br />31 to 32<br />33 to 35<br />36 to 40<br />41 to 50<br />older than 50 years</div>');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('For example, <b>interval 10 years</b> describes the following set of age ranges:<div style="padding-left:30px;">younger than one year<br />one year to 5 years<br />6 to 10<br />11 to 20<br />21 to 30<br />31 to 40<br />41 to 50<br />51 to 60<br />61 to 70<br />71 to 80<br />81 to 90<br />91 to 100<br />older than 100 years</div>');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('For example, <b>interval one child</b> describes the following set of child count ranges:<div style="padding-left:30px;">without children<br />one child<br />two children<br />3, 4, 5, 6, 7, 8, 9, 10 children<br />more than 10 children</div>');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Full screen');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('GEDCOMs to store in Sitemap:');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Generate Sitemap files');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Generate');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('In normal mail headers for external mails, the email address as well as the name are used. Some mail systems will not accept this. When set to <b>Yes</b>, only the email address will be used.');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Item');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Map type');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Move the toolbar');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('On this chart you can display one or more persons along a timeline.  You can, for example, visualize the status of two or more persons at a certain moment.<br /><br />If you click the <b>Time Line</b> link on an other page you will already see one person on the Time Line.  If you clicked the <b>Time Line</b> menu item in a page header, you have to supply the starting person\'s ID.');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Place all the files in the root of your webtrees installation.');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Print');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Priority');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Select GEDCOMs');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Select items');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Select the GEDCOMs for which you want to create a Sitemap file. You must select at least one.<br /><br />When the <b>No links to private information</b> option is selected, only links to data that is publicly available will be included.');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Select the elements to be included in the Sitemap file.<br /><br />A priority can be specified for all selected elements. This priority is relative to the other priorities in the file.  The update frequency can also be specified. This is an indication of how frequently the data in these items might change. This can influence the time between visits by the search engine, and thus will influence the amount of traffic the site generates.');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Select what you want to see on the map chart. You can choose:<p style="padding-left: 25px"><b>Individual distribution chart</b>&nbsp;&nbsp;shows the countries in which persons from this database occur.<br /><b>Surname distribution chart</b>&nbsp;&nbsp;shows the countries in which the specified surname occurs.</p>');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Selected items to store in Sitemap:');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Select the desired starting year and interval<br /><br />For example, <b>from 1700 interval 50 years</b> describes the following set of date ranges:<div style="padding-left:30px;">before 1700<br />1700 to 1749<br />1750 to 1799<br />1800 to 1849<br />1850 to 1899<br />1900 to 1949<br />1950 to 1999<br />2000 or later</div>');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Sender name');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Sending domain name');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Should the names of living people be shown to visitors?');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Show living names');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Show slideshow controls?');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Show year of birth and death');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Style');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('The Ancestry page is very similar to the <a href="?help=pedigree.php">Pedigree Tree</a>, but with more details and alternate <a href="?help=chart_style">Chart style</a> displays.<br /><br />Each ancestry is shown with a unique number, calculated according to the <i>Sosa-Stradonitz</i> system:<div style="padding-left:30px;"><b>Even</b> numbers for men (child*2)<br /><b>Odd</b> numbers for women (husband+1) except for <b>1</b></div><br />Example:<br /><div style="padding-left:30px;">The root person is <b>1</b>, regardless of gender.<br /><b>1</b>\'s father is <b>2</b> (<b>1</b> * 2), mother is <b>3</b> (<b>2</b> + 1).<br /><b>2</b>\'s father is <b>4</b> (<b>2</b> * 2), mother is <b>5</b> (<b>4</b> + 1).<br /><b>3</b>\'s father is <b>6</b> (<b>3</b> * 2), mother is <b>7</b> (<b>6</b> + 1).<br /><b>7</b>\'s father is <b>14</b> (<b>7</b> * 2), mother is <b>15</b> (<b>14</b> +1).</div><br />etc.');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('The Hourglass chart will show the ancestors and descendants of the selected root person on the same chart.  This chart is a mix between the Descendancy chart and the Pedigree chart.<br /><br />The root person is centered in the middle of the page with his descendants listed to the left and his ancestors listed to the right.  In this view, each generation is lined up across the page starting with the earliest generation and ending with the latest.<br /><br />If there is a downwards arrow on the screen under the root person, clicking on it will display a list of the root person\'s close family members that you can use the navigate down the chart.  Selecting a name from this list will reload the chart with the selected person as the new root person.');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('The following Sitemap files have been generated and can be downloaded:');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('This chart is very similar to the Hourglass chart.  It will show the ancestors and descendants of the selected root person on the same chart.  It will also show the descendants of the root person in the same Hourglass format.<br /><br />The root person is centered in the middle of the page with his descendants listed to the left and his ancestors listed to the right.  In this view, each generation is lined up across the page starting with the earliest generation and ending with the latest.<br /><br />Each descendant of the root person will become the root person of an additional hourglass chart, printed on the same page.  This process repeats until the specified number of descendant generations have been printed.');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('This is the domain part of a valid e-mail address on the SMTP server.<br /><br />For example, if you have an e-mail account such as <b>yourname@abc.xyz.com</b>, you would enter <b>abc.xyz.com</b> here.');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Updates');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Use simple mail headers in external mails');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('Use simple mail headers');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('always');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('boundaries for ages:');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('boundaries for date periods:');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('boundaries for month:');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('boundaries for numbers:');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('daily');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('hourly');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('monthly');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('never');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('weekly');
-/* I18N: This message is deleted in webtrees-1.2.6  */ WT_I18N::translate('yearly');
