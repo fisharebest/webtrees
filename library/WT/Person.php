09608f8bf6f3eaf9840b42e452784570d1c50069
fisharebest <fisharebest@gmail.com>
2012-03-31 18:50:39 +0000

    Fix: double-encoding of HTML entities in lifespans

--- library/WT/Person.php
+++ library/WT/Person.php
@@ -427,8 +427,8 @@ class WT_Person extends WT_GedcomRecord {
 		return
 			/* I18N: A range of years, e.g. "1870–", "1870–1920", "–1920" */ WT_I18N::translate(
 				'%1$s–%2$s',
-				'<span title="'.htmlspecialchars(strip_tags($this->getBirthDate()->Display())).'">'.$this->getBirthDate()->MinDate()->Format('%Y').'</span>',
-				'<span title="'.htmlspecialchars(strip_tags($this->getDeathDate()->Display())).'">'.$this->getDeathDate()->MinDate()->Format('%Y').'</span>'
+				'<span title="'.strip_tags($this->getBirthDate()->Display()).'">'.$this->getBirthDate()->MinDate()->Format('%Y').'</span>',
+				'<span title="'.strip_tags($this->getDeathDate()->Display()).'">'.$this->getDeathDate()->MinDate()->Format('%Y').'</span>'
 			);
 	}
 
