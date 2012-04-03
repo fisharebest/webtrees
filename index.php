8958a0534b343fa6ae4e16ae9f7535f052087373
Nigel Osborne <kiwi3685@me.com>
2012-04-03 04:09:55 +0000

    Minor change to "Change the blocks on this page"

--- index.php
+++ index.php
@@ -143,6 +143,6 @@ if ($blocks['side']) {
 echo '<div id="link_change_blocks">';
 	if ($ctype=='user') echo '<a href="index_edit.php?user_id='.WT_USER_ID.'" onclick="return modalDialog(\'index_edit.php?user_id='.WT_USER_ID.'\', \'', WT_I18N::translate('Change the blocks on this page'), '\');">', WT_I18N::translate('Change the blocks on this page'), '</a>';
 	if (WT_USER_IS_ADMIN && $ctype=='gedcom') echo '<a href="index_edit.php?gedcom_id='.WT_GED_ID.'" onclick="return modalDialog(\'index_edit.php?gedcom_id='.WT_GED_ID.'\', \'', WT_I18N::translate('Change the blocks on this page'), '\');">', WT_I18N::translate('Change the blocks on this page'), '</a>';
-	if ($SHOW_COUNTER) {echo '&nbsp;|&nbsp;'.WT_I18N::translate('Hit Count:')." ".$hitCount;}
-echo '</div>';
-echo '</div>'; // <div id="home-page">
+	if ($SHOW_COUNTER) {echo '<span>'.WT_I18N::translate('Hit Count:').' '.$hitCount.'</span>';}
+echo '</div>', // <div id="link_change_blocks">
+	 '</div>'; // <div id="home-page">
