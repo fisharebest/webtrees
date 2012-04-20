3c8c2be9d494e4b2392da0e65a7a62e378e36d17
fisharebest <fisharebest@gmail.com>
2012-04-20 13:18:48 +0000

    Edit news/journal entry - close window automatically after save

--- editnews.php
+++ editnews.php
@@ -27,13 +27,10 @@ define('WT_SCRIPT_NAME', 'editnews.php');
 require './includes/session.php';
 
 $controller=new WT_Controller_Simple();
-$controller->setPageTitle(WT_I18N::translate('Add/edit journal/news entry'));
-$controller->pageHeader();
-
-if (!WT_USER_ID) {
-	echo WT_I18N::translate('<b>Access Denied</b><br />You do not have access to this resource.');
-	exit;
-}
+$controller
+	->setPageTitle(WT_I18N::translate('Add/edit journal/news entry'))
+	->requireMemberLogin()
+	->pageHeader();
 
 $action   =safe_GET('action', array('compose', 'save', 'delete'), 'compose');
 $news_id  =safe_GET('news_id');
@@ -43,47 +40,27 @@ $date     =safe_POST('date', WT_REGEX_UNSAFE);
 $title    =safe_POST('title', WT_REGEX_UNSAFE);
 $text     =safe_POST('text', WT_REGEX_UNSAFE);
 
-if ($action=="compose") {
-	echo '<span class="subheaders">'.WT_I18N::translate('Add/edit journal/news entry').'</span>';
-	?>
-	<script type="text/javascript">
-		function checkForm(frm) {
-			if (frm.title.value=="") {
-				alert('<?php echo WT_I18N::translate('Please enter a title.'); ?>');
-				document.messageform.title.focus();
-				return false;
-			}
-			<?php if (!array_key_exists('ckeditor', WT_Module::getActiveModules())) { //will be empty for FCK. FIXME, use FCK API to check for content.
-			?>
-			if (frm.text.value=="") {
-				alert('<?php echo WT_I18N::translate('Please enter some text for this News or Journal entry.'); ?>');
-				document.messageform.text.focus();
-				return false;
-			}
-			<?php } ?>
-			return true;
-		}
-	</script>
-	<?php
-	echo "<br><form name=\"messageform\" method=\"post\" action=\"editnews.php?action=save&news_id=".$news_id."\" onsubmit=\"return checkForm(this);";
-	echo "\">";
+switch ($action) {
+case 'compose':
+	echo '<h3>'.WT_I18N::translate('Add/edit journal/news entry').'</h3>';
+	echo '<form name="messageform" method="post" action="editnews.php?action=save&news_id='.$news_id.'">';
 	if ($news_id) {
 		$news = getNewsItem($news_id);
 	} else {
 		$news = array();
-		$news["user_id"] = $user_id;
-		$news["gedcom_id"] = $gedcom_id;
-		$news["date"] = time();
-		$news["title"] = "";
-		$news["text"] = "";
+		$news['user_id'] = $user_id;
+		$news['gedcom_id'] = $gedcom_id;
+		$news['date'] = time();
+		$news['title'] = '';
+		$news['text'] = '';
 	}
-	echo "<input type=\"hidden\" name=\"user_id\" value=\"".$news["user_id"]."\">";
-	echo "<input type=\"hidden\" name=\"gedcom_id\" value=\"".$news["gedcom_id"]."\">";
-	echo "<input type=\"hidden\" name=\"date\" value=\"".$news["date"]."\">";
-	echo "<table>";
-	echo "<tr><td align=\"right\">".WT_I18N::translate('Title:').'</td><td><input type="text" name="title" size="50" dir="auto" autofocus value="'.$news['title'].'"><br></td></tr>';
-	echo "<tr><td valign=\"top\" align=\"right\">".WT_I18N::translate('Entry Text:')."<br></td>";
-	echo "<td>";
+	echo '<input type="hidden" name="user_id" value="'.$news['user_id'].'">';
+	echo '<input type="hidden" name="gedcom_id" value="'.$news['gedcom_id'].'">';
+	echo '<input type="hidden" name="date" value="'.$news['date'].'">';
+	echo '<table>';
+	echo '<tr><td align="right">'.WT_I18N::translate('Title:').'</td><td><input type="text" name="title" size="50" dir="auto" autofocus value="'.$news['title'].'"></td></tr>';
+	echo '<tr><td valign="top" align="right">'.WT_I18N::translate('Entry Text:').'</td>';
+	echo '<td>';
 	if (array_key_exists('ckeditor', WT_Module::getActiveModules())) {
 		require_once WT_ROOT.WT_MODULES_DIR.'ckeditor/ckeditor.php';
 		$oCKeditor = new CKEditor();
@@ -92,30 +69,30 @@ if ($action=="compose") {
 		$oCKeditor->config['height'] = 250;
 		$oCKeditor->config['AutoDetectLanguage'] = false ;
 		$oCKeditor->config['DefaultLanguage'] = 'en';
-		$oCKeditor->editor('text', $news["text"]);
+		$oCKeditor->editor('text', $news['text']);
 	} else { //use standard textarea
-		echo '<textarea name="text" cols="80" rows="10" dir="auto">'.$news['text'].'</textarea>';
+		echo '<textarea name="text" cols="80" rows="10" dir="auto">'.htmlspecialchars($news['text']).'</textarea>';
 	}
-	echo "<br></td></tr>";
-	echo "<tr><td></td><td><input type=\"submit\" value=\"".WT_I18N::translate('Save')."\"></td></tr>";
-	echo "</table>";
-	echo "</form>";
-} else if ($action=="save") {
-	if (empty($title)) $title="No Title";
-	if (empty($text)) $text="No Text";
-	$message = array();
+	echo '</td></tr>';
+	echo '<tr><td></td><td><input type="submit" value="'.WT_I18N::translate('Save').'"></td></tr>';
+	echo '</table>';
+	echo '</form>';
+	break;
+case 'save':
+	$message=array();
 	if ($news_id) {
-		$message["id"]=$news_id;
+		$message['id']=$news_id;
 	}
-	$message["user_id"] = $user_id;
-	$message["gedcom_id"] = $gedcom_id;
-	$message["date"]=time();
-	$message["title"] = $title;
-	$message["text"] = $text;
-	if (addNews($message)) {
-		echo WT_I18N::translate('News/Journal entry successfully saved.');
-	}
-} else if ($action=="delete") {
-	if (deleteNews($news_id)) echo WT_I18N::translate('The news/journal entry has been deleted.');
+	$message['user_id'] = $user_id;
+	$message['gedcom_id'] = $gedcom_id;
+	$message['date']=time();
+	$message['title'] = $title;
+	$message['text'] = $text;
+	addNews($message);
+	$controller->addInlineJavaScript('window.opener.location.reload();window.close();');
+	break;
+case 'delete':
+	deleteNews($news_id);
+	$controller->addInlineJavaScript('window.opener.location.reload();window.close();');
+	break;
 }
-echo "<center><br><br><a href=\"#\" onclick=\"window.opener.location.reload(); window.close();\">".WT_I18N::translate('Close Window')."</a><br></center>";
