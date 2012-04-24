c166680f76f913bad96d30cb7472bf2e38a67749
Łukasz Wilenski <wooc@gazeta.pl>
2012-04-24 16:58:57 +0000

    cache dir for tcpdf should be in data directory not in tcpdf

--- library/tcpdf/config/tcpdf_config.php
+++ library/tcpdf/config/tcpdf_config.php
@@ -81,7 +81,7 @@ if (!defined('K_TCPDF_EXTERNAL_CONFIG')) {
 	/**
 	 * cache directory for temporary files (full path)
 	 */
-	define ('K_PATH_CACHE', K_PATH_MAIN.'cache/');
+	define ('K_PATH_CACHE', WT_DATA_DIR.'cache/');
 
 	/**
 	 * cache directory for temporary files (url path)
