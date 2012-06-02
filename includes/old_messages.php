eaa3fee62d819b34a4e4a6dbdcc1af634ff88a6d
fisharebest <fisharebest@gmail.com>
2012-06-02 18:16:38 +0000

    Delete old messages - in preparation for 1.3.0 release

--- includes/old_messages.php
+++ includes/old_messages.php
@@ -31,6 +31,8 @@
 
 exit;
 
+// MESSAGES DELETED IN WEBTREES-1.3.1
+
 // MESSAGES DELETED IN WEBTREES-1.3.0
 
 /* I18N: This message is deleted in webtrees-1.3.0  */ WT_I18N::translate('<span class="label">%1$s:</span> <span class="field">%2$s</span>');
@@ -221,72 +223,3 @@ exit;
 /* I18N: This message is deleted in webtrees-1.3.0  */ WT_I18N::translate('tag');
 /* I18N: This message is deleted in webtrees-1.3.0  */ WT_I18N::translate('too few');
 /* I18N: This message is deleted in webtrees-1.3.0  */ WT_I18N::translate('too many');
-
-// MESSAGES DELETED IN WEBTREES-1.2.7
-
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('%d day ago', '%d days ago', 0, 0);
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('%d day', '%d days', 0, 0);
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('%d hour ago', '%d hours ago', 0, 0);
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('%d minute ago', '%d minutes ago', 0, 0);
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('%d month ago', '%d months ago', 0, 0);
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('%d month', '%d months', 0, 0);
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('%d second ago', '%d seconds ago', 0, 0);
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('%d week', '%d weeks', 0, 0);
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('%d year ago', '%d years ago', 0, 0);
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('%d year', '%d years', 0, 0);
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('Changes in the last %d day', 'Changes in the last %d days', 0, 0);
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('No events exist for the next %d day.', 'No events exist for the next %d days.', 0, 0);
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('No events for living people exist for the next %d day.', 'No events for living people exist for the next %d days.', 0, 0);
-// We cannot include these two, as they conflict with a non-plural translation of the same string
-// WT_I18N::plural('Top given name', 'Top %d given names', 0, 0);
-// WT_I18N::plural('Top surname', 'Top %d surnames', 0, 0);
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('from %2$d interval %1$d year', 'from %2$d interval %1$d years', 0, 0, 0);
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::plural('interval %d year', 'interval %d years', 0, 0);
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('%d B.C.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('A request was received at %s to create a webtrees account with your email address %s.', '', '');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Account Information');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Administrator help items');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Click on any of the boxes to get more information about that person.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Contextual Help links');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Data request at %s', '');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Enter the number of children for this individual or family. This is an optional field.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('FAQ List: Edit');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Fields marked with * are mandatory.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('GEDCOM information');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('GEDCOM: Administration page');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('GEDCOM: Configure');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('GEDCOM: Default');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Hello...<br /><br />An email with your new password was sent to the address we have on file for <b>%s</b>.<br /><br />Please check your email account; you should receive our message soon.<br /><br />Recommendation:<br />You should login to this site with your new password as soon as possible, and you should change your password to maintain your data\'s security.', '');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Help contents');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Help items');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Here you can change the box width from 50 percent to 300 percent.  At 100 percent each box is about 270 pixels wide.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Hide contextual help');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('If you are a visitor to this site and wish to request a user account, you can click this link.<br /><br />You will be taken to the "Register" page.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('If you have forgotten your password, you can click this link to request a new password.<br /><br />You will be taken to the "Lost Password Request" page.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Lost your password?');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('No account?');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Number of levels');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Other records');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Place levels to show in person boxes');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Place levels to show on lists');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Please click on the link below to login to your site.  You must Edit the user to activate the account so that he can login to your site.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Request user account');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Show contextual help');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Site Default');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Support contact');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Technical help contact');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('The requested GEDCOM record could not be found.  This could be caused by a link to an invalid person or by a corrupt GEDCOM file.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('This block displays the most frequently occurring surnames in the database.  The configuration settings for this block allow changes to the number of names displayed, the presentation style, and the use of scroll bars with long lists.  By using the GEDCOM administration function, an administrator can also configure names to remove from this list.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('This block shows a table of the 10 most frequently occurring given names in the database.  The actual number of given names shown in this block is configurable.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('This determines how much of the Place information is shown in the Place fields on lists.<br /><br />Setting the value to <b>9</b> will ensure that all Place information will be shown.  Setting the value to <b>0</b> (zero) will hide places completely.  Setting the value to <b>1</b> will show the topmost level, which is normally the country.  Setting it to <b>2</b> will show the topmost two levels.  The second topmost level, below the country, is often the state, province, or territory. Etc.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('This field indicates the number of levels in the places-hierarchy that is being used by the Googlemap modules.<br />The default value is 4 (Country, State, County, Place), which is usually good enough. If you want to add an extra level (for example to add specific location like cemeteries or schools) change this value. If you want to remove a level (for example county) you can also change this value, but keep in mind that the files containing the place-locations contain a 4-level structure.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('This field describes the relationship of the child to its family.  The possibilities are:<ul><li><b>unknown</b>&nbsp;&nbsp;&nbsp;The child\'s relationship to its family cannot be determined.  When this option is selected, the Pedigree field will not be copied into the database.<br /><br /></li><li><b>Birth</b>&nbsp;&nbsp;&nbsp;This option indicates that the child is related to its family by birth.<br /><br /></li><li><b>Adopted</b>&nbsp;&nbsp;&nbsp;This option indicates that the child was adopted by its family.  This does <i>not</i> indicate that there is no blood relationship between the child and its family; it shows that the child was adopted by the family in question sometime after the child\'s birth.<br /><br /></li><li><b>Foster</b>&nbsp;&nbsp;&nbsp;This option indicates that the child is a foster child of the family.  Usually, there is no blood relationship between the child and its family.<br /><br /></li><li><b>Sealing</b>&nbsp;&nbsp;&nbsp;The child was sealed to its family in an LDS <i>sealing</i> ceremony.  A child sealing is performed when the parents were sealed to each other after the birth of the child.  Children born after the parents\' sealing are automatically sealed to the family.<br /><br /></li></ul>');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('This option will enable links, identified by question marks, next to items on many pages.  These links allow users to get information or help about those items.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('This sets how much of the place information is shown in the person boxes on charts.<br /><br />Setting the value to 9 will guarantee to show all place levels.  Setting the value to 0 will hide places completely.  Setting the value to 1 will show the first level, setting it to 2 will show the first two levels, etc.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('To have your password reset, enter your user name.<br /><br />We will respond by sending you an email to the address registered with your account.  The email will contain a URL and confirmation code for your account. When you visit this URL, you can change your password and login to this site. For security reasons, you should not give this confirmation code to anyone.<br /><br />If you require assistance from the site administrator, please use the contact link below.');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Top 10 Given Names');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('Use simple mail headers');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('User %s (%s) has confirmed their request for an account.', '', '');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('View readme.html file');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('grandchildren');
-/* I18N: This message is deleted in webtrees-1.2.7  */ WT_I18N::translate('webtrees user login');
