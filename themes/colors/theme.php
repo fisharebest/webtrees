5c88a3081b4f82275d9815daec35fe6aaecd134d
fisharebest <fisharebest@gmail.com>
2012-06-23 10:28:06 +0000

    HTML validation error in colors theme

--- themes/colors/theme.php
+++ themes/colors/theme.php
@@ -48,7 +48,7 @@ function color_theme_dropdown() {
 	$menu=new WT_Menu(/* I18N: A colour scheme */ WT_I18N::translate('Palette'), '#', 'menu-color');
 	uasort($COLOR_THEME_LIST, 'utf8_strcasecmp');
 	foreach ($COLOR_THEME_LIST as $colorChoice=>$colorName) {
-		$submenu=new WT_Menu($colorName, get_query_url(array('themecolor'=>$colorChoice)), 'menu-color-'.$colorChoice);
+		$submenu=new WT_Menu($colorName, get_query_url(array('themecolor'=>$colorChoice), '&amp;'), 'menu-color-'.$colorChoice);
 		$menu->addSubMenu($submenu);
 	}
 	return $menu->getMenuAsList();
