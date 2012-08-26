030839f423c3b3adc3c1fde95e5b51f55903dbb1
fisharebest <fisharebest@gmail.com>
2012-08-26 07:45:52 +0000

    #886753 - Display death people as alive

--- includes/functions/functions_print_lists.php
+++ includes/functions/functions_print_lists.php
@@ -412,7 +412,7 @@ function format_indi_table($datalist, $option='') {
 		// Died in last 100 years?  Died?  Not dead?
 		if (WT_Date::Compare($death_date, $d100y)>0) {
 			$html .= 'Y100';
-		} elseif ($death_date->minJD()) {
+		} elseif ($death_date->minJD() || $person->isDead()) {
 			$html .= 'YES';
 		} else {
 			$html .= 'N';
