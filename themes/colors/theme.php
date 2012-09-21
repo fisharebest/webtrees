e85265c420a89682b9a7d2a4c06ade13fba61b4f
Rob Peters <makitso@gmail.com>
2012-09-21 23:09:50 +0000

    Highlight current palette in menu dropdown

--- themes/colors/theme.php
+++ themes/colors/theme.php
@@ -47,14 +47,22 @@ function getMenuAsCustomList($menu) {
 
 //-- print color theme sub type change dropdown box
 function color_theme_dropdown() {
-	global $COLOR_THEME_LIST;
-	
+	global $COLOR_THEME_LIST, $WT_SESSION;
+	if (isset($_GET['themecolor'])) { echo 'AAAAA';}
 	$menu=new WT_Menu(/* I18N: A colour scheme */ WT_I18N::translate('Palette'), '#', 'menu-color');
 	uasort($COLOR_THEME_LIST, 'utf8_strcasecmp');
+
 	foreach ($COLOR_THEME_LIST as $colorChoice=>$colorName) {
 		$submenu=new WT_Menu($colorName, get_query_url(array('themecolor'=>$colorChoice), '&amp;'), 'menu-color-'.$colorChoice);
+		if (isset($WT_SESSION->subColor)) {
+			if ($WT_SESSION->subColor == $colorChoice) {  
+				$submenu->addClass('','','theme-active');
+			}
+		} elseif  (WT_Site::preference('DEFAULT_COLOR_PALETTE') == $colorChoice) { 
+			$submenu->addClass('','','theme-active');
+			}
 		$menu->addSubMenu($submenu);
-	}
+		}
 	return $menu->getMenuAsList();
 }
 
