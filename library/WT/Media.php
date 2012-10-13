579e6544605eebd894c1f13b5f121918bcbb0efa
Nigel Osborne <kiwi3685@me.com>
2012-10-13 04:46:34 +0000

    Make random media block and images fixed dimension to stop jumping around in slide show display. webtrees theme only. Can easily be extended to others if required.

--- library/WT/Media.php
+++ library/WT/Media.php
@@ -667,7 +667,7 @@ class WT_Media extends WT_GedcomRecord {
 			}
 
 			$output='';
-			if ($config['oktolink'] && $mainexists) $output .= '<a href="'.$this->getHtmlUrlSnippet($config).'">';
+			if ($config['oktolink'] && $mainexists) $output .= '<a class="media_container" href="'.$this->getHtmlUrlSnippet($config).'">';
 			$output .= '<img '.$idstr.' src="'.$this->getHtmlUrlDirect('thumb').'" '.$sizestr.' class="'.$config['class'].'"';
 			$output .= ' alt="'.$config['img_title'].'" title="'.$config['img_title'].'" '.$stylestr.'>';
 			if ($config['oktolink'] && $mainexists) {
