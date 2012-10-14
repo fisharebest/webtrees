9660b8c23f6d2f80f7b053d1486136d6d2bd2cfb
Nigel Osborne <kiwi3685@me.com>
2012-10-14 21:03:25 +0000

    Revert accidental deletion

---
+++ modules_v3/theme_select/module.php
@@ -0,0 +1,79 @@
+<?php
+// Classes and libraries for module system
+//
+// webtrees: Web based Family History software
+// Copyright (C) 2011 webtrees development team.
+//
+// Derived from PhpGedView
+// Copyright (C) 2010 John Finlay
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// $Id: module.php 13034 2011-12-12 13:10:58Z greg $
+
+if (!defined('WT_WEBTREES')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+class theme_select_WT_Module extends WT_Module implements WT_Module_Block {
+	// Extend class WT_Module
+	public function getTitle() {
+		return /* I18N: Name of a module */ WT_I18N::translate('Theme change');
+	}
+
+	// Extend class WT_Module
+	public function getDescription() {
+		return /* I18N: Description of the "Theme change" module */ WT_I18N::translate('An alternative way to select a new theme.');
+	}
+
+	// Implement class WT_Module_Block
+	public function getBlock($block_id, $template=true, $cfg=null) {
+		$id=$this->getName().$block_id;
+		$class=$this->getName().'_block';
+		$title=$this->getTitle();
+		$menu=WT_MenuBar::getThemeMenu();
+		if ($menu) {
+			$content='<br><div class="center theme_form">'.WT_MenuBar::getThemeMenu()->getMenuAsDropdown().'</div><br>';
+
+			if ($template) {
+				require WT_THEME_DIR.'templates/block_main_temp.php';
+			} else {
+				return $content;
+			}
+		} else {
+			return '';
+		}
+	}
+
+	// Implement class WT_Module_Block
+	public function loadAjax() {
+		return false;
+	}
+
+	// Implement class WT_Module_Block
+	public function isUserBlock() {
+		return true;
+	}
+
+	// Implement class WT_Module_Block
+	public function isGedcomBlock() {
+		return true;
+	}
+
+	// Implement class WT_Module_Block
+	public function configureBlock($block_id) {
+	}
+}
