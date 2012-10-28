e8c8f133f778abde9375eb9a3c948ad0e7297650
Nigel Osborne <kiwi3685@me.com>
2012-10-28 03:46:53 +0000

    Revert 14470/71 - didn't work as expected.

--- modules_v3/googlemap/places_edit.php
+++ modules_v3/googlemap/places_edit.php
@@ -339,6 +339,7 @@ $api='v3';
 			</div></td>
 	</tr>
 	</table>
-	<input name="save2" type="submit" value="<?php echo WT_I18N::translate('Save'); ?>" onclick="closePopupAndReloadParent();"><br>
+	<input name="save2" type="submit" value="<?php echo WT_I18N::translate('Save'); ?>"><br>
 </form>
 <?php
+echo '<p class="center"><a href="#" onclick="closePopupAndReloadParent();">', WT_I18N::translate('Close Window'), '</a></p>';
