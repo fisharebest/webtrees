4e39c1e05eb4074f17a97f9a18452f3ed37574f1
fisharebest <fisharebest@gmail.com>
2013-01-15 10:03:56 +0000

    Remove some dead code

--- modules_v3/lightbox/functions/lightbox_print_media.php
+++ modules_v3/lightbox/functions/lightbox_print_media.php
@@ -180,22 +180,6 @@ function lightbox_print_media($pid, $level=1, $related=false, $kind=1, $noedit=f
 				}
 				continue;
 			}
-			// NOTE: Determine the size of the mediafile
-			$imgwidth = 300+40;
-			$imgheight = 300+150;
-			if (isFileExternal($rowm['m_filename'])) {
-				if (in_array($rowm['m_ext'], $MEDIATYPE)) {
-					$imgwidth = 400+40;
-					$imgheight = 500+150;
-				} else {
-					$imgwidth = 800+40;
-					$imgheight = 400+150;
-				}
-			} else if (media_exists(check_media_depth($rowm['m_filename'], 'NOTRUNC'))) {
-				$imgsize = findImageSize(check_media_depth($rowm['m_filename'], 'NOTRUNC'));
-				$imgwidth = $imgsize[0]+40;
-				$imgheight = $imgsize[1]+150;
-			}
 			$rows=array();
 
 			//-- if there is a change to this media item then get the
