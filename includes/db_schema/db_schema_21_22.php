d9ff06e847757ad0974b047ec190c6df8c0e85af
fisharebest <fisharebest@gmail.com>
2013-01-16 08:58:37 +0000

    More updates for db-changes (note - this script is not yet activated via session.php)

--- includes/db_schema/db_schema_21_22.php
+++ includes/db_schema/db_schema_21_22.php
@@ -37,7 +37,7 @@ if (!defined('WT_WEBTREES')) {
 self::exec("DELETE FROM `##gedcom_setting` WHERE setting_name IN ('AUTO_GENERATE_THUMBS', 'POSTAL_CODE', 'MEDIA_DIRECTORY_LEVELS')");
 
 // Previous versions of webtrees included the MEDIA_DIRECTORY setting in the
-// FILE tag of the OBJE records.  Remove it.
+// FILE tag of the OBJE records.  Remove it…
 self::exec(
 	"UPDATE `##media` m".
 	" JOIN `##gedcom_setting` gs ON (m.m_file = gs.gedcom_id AND gs.setting_name = 'MEDIA_DIRECTORY')".
@@ -45,13 +45,34 @@ self::exec(
 	"  m_filename = TRIM(LEADING gs.setting_value FROM m_filename),".
 	"  m_gedcom   = REPLACE(m_gedcom, CONCAT('\n1 FILE ', gs.setting_value), '\n1 FILE ')"
 );
-
+// …don’t forget pending changes
 self::exec(
 	"UPDATE `##change` c".
 	" JOIN `##gedcom_setting` gs ON (c.gedcom_id = gs.gedcom_id AND gs.setting_name = 'MEDIA_DIRECTORY')".
 	" SET new_gedcom = REPLACE(new_gedcom, CONCAT('\n1 FILE ', gs.setting_value), '\n1 FILE ')".
 	" WHERE status = 'pending'"
 );
+
+// The MEDIA_FIREWALL_ROOTDIR is now fixed as WT_DATA_DIR - filter these out
+self::prepare(
+	"DELETE FROM `##gedcom_setting`".
+	" WHERE setting_name='MEDIA_FIREWALL_ROOTDIR' AND setting_value IN ('', ?)'"
+)->execute(array(WT_DATA_DIR));
+
+// We cannot easily move media folders.  There may be all sorts of complicated configurations.
+// For example these two 
+// GEDCOM_ID=1 / MEDIA_FIREWALL_ROOTDIR=firewall-dir1 / MEDIA_DIRECTORY=media
+// GEDCOM_ID=2 / MEDIA_FIREWALL_ROOTDIR=firewall-dir2 / MEDIA_DIRECTORY=media
+//
+// Just move the simple/obvious/safe one
+if (is_dir('media') && !is_file(WT_DATA_DIR . 'media')) {
+	@unlink('media/.htaccess');
+	@unlink('media/index.php');
+	@unlink('media/MediaInfo.txt');
+	@unlink('media/thumbs/index.php');
+	@unlink('media/thumbs/ThumbsInfo.txt');
+	@rename('media', WT_DATA_DIR . 'media');
+}
 
 // Update the version to indicate success
 WT_Site::preference($schema_name, $next_version);
