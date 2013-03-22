a000b76a4685f690717f11ae2c77483f2f4f1d16
Łukasz Wilenski <wooc@gazeta.pl>
2013-03-22 16:53:26 +0000

    Fatal error: Call to a member function getClientIp() when downloading geographic data from GM

--- modules_v3/googlemap/admin_places.php
+++ modules_v3/googlemap/admin_places.php
@@ -143,6 +143,7 @@ $controller=new WT_Controller_Page();
 $controller->requireAdminLogin();
 	
 if ($action=='ExportFile' && WT_USER_IS_ADMIN) {
+	Zend_Session::writeClose();
 	$tmp = place_id_to_hierarchy($parent);
 	$maxLevel = getHighestLevel();
 	if ($maxLevel>8) $maxLevel=8;
