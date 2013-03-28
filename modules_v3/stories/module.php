ff5e28319d9f663d3fbf3084b5a58c32db208dd1
Nigel Osborne <kiwi3685@me.com>
2013-03-28 21:01:43 +0000

    Bug #1161319 - Missing I18N in stories module Edit

--- modules_v3/stories/module.php
+++ modules_v3/stories/module.php
@@ -307,6 +307,7 @@ class stories_WT_Module extends WT_Module implements WT_Module_Block, WT_Module_
 				->addInlineJavascript('
 					jQuery("#story_table").dataTable({
 						"sDom": \'<"H"pf<"dt-clear">irl>t<"F"pl>\',
+						'.WT_I18N::datatablesI18N().',
 						"bAutoWidth":false,
 						"bPaginate": true,
 						"sPaginationType": "full_numbers",
@@ -383,6 +384,7 @@ class stories_WT_Module extends WT_Module implements WT_Module_Block, WT_Module_
 			->addInlineJavascript('
 				jQuery("#story_table").dataTable({
 					"sDom": \'<"H"pf<"dt-clear">irl>t<"F"pl>\',
+					'.WT_I18N::datatablesI18N().',
 					"bAutoWidth":false,
 					"bPaginate": true,
 					"sPaginationType": "full_numbers",
