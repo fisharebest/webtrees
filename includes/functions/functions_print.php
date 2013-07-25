a5ea671ad7f22f91b4c5c0f216a4cb7fca35fe17
fisharebest <fisharebest@gmail.com>
2013-07-25 20:41:16 +0000

    #1080013 - different notes are not shown if first line is the same

--- includes/functions/functions_print.php
+++ includes/functions/functions_print.php
@@ -523,13 +523,15 @@ function print_fact_notes($factrec, $level, $textOnly=false, $return=false) {
 	$ged_id=get_id_from_gedcom($GEDCOM);
 
 	$data = "";
+	$previous_spos = 0;
 	$nlevel = $level+1;
 	$ct = preg_match_all("/$level NOTE(.*)/", $factrec, $match, PREG_SET_ORDER);
 	for ($j=0; $j<$ct; $j++) {
 		$nid = str_replace("@","",$match[$j][1]);
-		$spos1 = strpos($factrec, $match[$j][0]);
+		$spos1 = strpos($factrec, $match[$j][0], $previous_spos);
 		$spos2 = strpos($factrec."\n$level", "\n$level", $spos1+1);
 		if (!$spos2) $spos2 = strlen($factrec);
+		$previous_spos = $spos2;
 		$nrec = substr($factrec, $spos1, $spos2-$spos1);
 		if (!isset($match[$j][1])) $match[$j][1]="";
 		$nt = preg_match("/@(.*)@/", $match[$j][1], $nmatch);
