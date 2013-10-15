544203a25fe4d3c0c53d85e8b1a94e73a33014e6
JustCarmen <carmen@justcarmen.nl>
2013-10-15 21:40:05 +0200

    Added missing language file to the Fancy Search Block module

---
+++ modules_v3/fancy_search_block/language/nl.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined('WT_WEBTREES')) {
+header('HTTP/1.0 403 Forbidden');
+exit;
+}
+
+return array(
+	'A (general) search block on home page or individual page'=>'Een (algemeen) zoekblok op de startpagina of \'mijn pagina\''
+);
