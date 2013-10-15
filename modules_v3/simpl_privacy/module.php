ad426ff55aab810a77a3c5c845c818b3879e6682
JustCarmen <carmen@justcarmen.nl>
2013-10-15 21:14:15 +0200

    Update Simpl Privacy module for webtrees 1.5

--- modules_v3/simpl_privacy/module.php
+++ modules_v3/simpl_privacy/module.php
@@ -91,82 +91,88 @@ class simpl_privacy_WT_Module extends WT_Module implements WT_Module_Sidebar {
 		return '';
 	}
 	
-	// This is a copy, with modifications, of the function isDead() in /library/WT/Person.php
+	// This is a copy, with modifications, of the function isDead() in /library/WT/Individual.php
 	// It is VERY important that the parameters used in both are identical.
 	private function simpl_isDead() {
 		global $MAX_ALIVE_AGE, $SEARCH_SPIDER;
 		$controller=new WT_Controller_Individual();
-
+		
 		// "1 DEAT Y" or "1 DEAT/2 DATE" or "1 DEAT/2 PLAC"
-		if (preg_match('/\n1 (?:'.WT_EVENTS_DEAT.')(?: Y|(?:\n[2-9].+)*\n2 (DATE|PLAC) )/', $controller->record->getGedcomRecord())) {
+		if (preg_match('/\n1 (?:'.WT_EVENTS_DEAT.')(?: Y|(?:\n[2-9].+)*\n2 (DATE|PLAC) )/', $controller->record->getGedcom())) {
 			return WT_I18N::translate('Death is recorded with an unknown date.');
 		}
-
-		// If any event occured more than $MAX_ALIVE_AGE years ago, then assume the person is dead
-		preg_match_all('/\n2 DATE (.+)/', $controller->record->getGedcomRecord(), $date_matches);
-		foreach ($date_matches[1] as $date_match) {
-			$date=new WT_Date($date_match);
-			if ($date->isOK() && $date->MaxJD() <= WT_SERVER_JD - 365*$MAX_ALIVE_AGE) {
-				return WT_I18N::translate('An event occurred in this person\'s life more than %s years ago<br> %s', $MAX_ALIVE_AGE, $date->Display(!$SEARCH_SPIDER));
+		
+		// If any event occured more than $MAX_ALIVE_AGE years ago, then assume the individual is dead
+		if (preg_match_all('/\n2 DATE (.+)/', $controller->record->getGedcom(), $date_matches)) {
+			foreach ($date_matches[1] as $date_match) {
+				$date=new WT_Date($date_match);
+				if ($date->isOK() && $date->MaxJD() <= WT_CLIENT_JD - 365*$MAX_ALIVE_AGE) {
+					return WT_I18N::translate('An event occurred in this person\'s life more than %s years ago<br> %s', $MAX_ALIVE_AGE, $date->Display(!$SEARCH_SPIDER));
+				}
+			}
+			// The individual has one or more dated events.  All are less than $MAX_ALIVE_AGE years ago.
+			// If one of these is a birth, the individual must be alive.
+			if (preg_match('/\n1 BIRT(?:\n[2-9].+)*\n2 DATE /', $controller->record->getGedcom())) {
+				return false;
 			}
 		}
-
-		// If we found no dates then check the dates of close relatives.
+		
+		// If we found no conclusive dates then check the dates of close relatives.
 
 		// Check parents (birth and adopted)
 		foreach ($controller->record->getChildFamilies(WT_PRIV_HIDE) as $family) {
 			foreach ($family->getSpouses(WT_PRIV_HIDE) as $parent) {
 				// Assume parents are no more than 45 years older than their children
-				preg_match_all('/\n2 DATE (.+)/', $parent->getGedcomRecord(), $date_matches);
+				preg_match_all('/\n2 DATE (.+)/', $parent->getGedcom(), $date_matches);
 				foreach ($date_matches[1] as $date_match) {
 					$date=new WT_Date($date_match);
-					if ($date->isOK() && $date->MaxJD() <= WT_SERVER_JD - 365*($MAX_ALIVE_AGE+45)) {
+					if ($date->isOK() && $date->MaxJD() <= WT_CLIENT_JD - 365*($MAX_ALIVE_AGE+45)) {
 						return WT_I18N::translate('A parent with a birth date of %s is more than 45 years older than this person', $date->Display(!$SEARCH_SPIDER));
 					}
 				}
 			}
 		}
-
+		
 		// Check spouses
 		foreach ($controller->record->getSpouseFamilies(WT_PRIV_HIDE) as $family) {
-			preg_match_all('/\n2 DATE (.+)/', $family->getGedcomRecord(), $date_matches);
+			preg_match_all('/\n2 DATE (.+)/', $family->getGedcom(), $date_matches);
 			foreach ($date_matches[1] as $date_match) {
 				$date=new WT_Date($date_match);
 				// Assume marriage occurs after age of 10
-				if ($date->isOK() && $date->MaxJD() <= WT_SERVER_JD - 365*($MAX_ALIVE_AGE-10)) {
-					return WT_I18N::translate('A marriage with a date of %s suggests they were born at least 10 years earlier than that.', $date->Display(!$SEARCH_SPIDER));
+				if ($date->isOK() && $date->MaxJD() <= WT_CLIENT_JD - 365*($MAX_ALIVE_AGE-10)) {
+					WT_I18N::translate('A marriage with a date of %s suggests they were born at least 10 years earlier than that.', $date->Display(!$SEARCH_SPIDER));
 				}
 			}
 			// Check spouse dates
-			$spouse=$family->getSpouse($this, WT_PRIV_HIDE);
+			$spouse = $family->getSpouse(WT_Individual::getInstance($controller->record->getXref()), WT_PRIV_HIDE);
 			if ($spouse) {
-				preg_match_all('/\n2 DATE (.+)/', $spouse->getGedcomRecord(), $date_matches);
+				preg_match_all('/\n2 DATE (.+)/', $spouse->getGedcom(), $date_matches);
 				foreach ($date_matches[1] as $date_match) {
-					$date=new WT_Date($date_match);
+					$date = new WT_Date($date_match);
 					// Assume max age difference between spouses of 40 years
-					if ($date->isOK() && $date->MaxJD() <= WT_SERVER_JD - 365*($MAX_ALIVE_AGE+40)) {
+					if ($date->isOK() && $date->MaxJD() <= WT_CLIENT_JD - 365*($MAX_ALIVE_AGE+40)) {
 						return WT_I18N::translate('A spouse with a date of %s is more than 40 years older than this person', $date->Display(!$SEARCH_SPIDER));
 					}
 				}
 			}
 			// Check child dates
 			foreach ($family->getChildren(WT_PRIV_HIDE) as $child) {
-				preg_match_all('/\n2 DATE (.+)/', $child->getGedcomRecord(), $date_matches);
+				preg_match_all('/\n2 DATE (.+)/', $child->getGedcom(), $date_matches);
 				// Assume children born after age of 15
 				foreach ($date_matches[1] as $date_match) {
 					$date=new WT_Date($date_match);
-					if ($date->isOK() && $date->MaxJD() <= WT_SERVER_JD - 365*($MAX_ALIVE_AGE-15)) {
+					if ($date->isOK() && $date->MaxJD() <= WT_CLIENT_JD - 365*($MAX_ALIVE_AGE-15)) {
 						return WT_I18N::translate('A child with a birth date of %s suggests this person was born at least 15 years earlier than that.', $date->Display(!$SEARCH_SPIDER));
 					}
 				}
 				// Check grandchildren
 				foreach ($child->getSpouseFamilies(WT_PRIV_HIDE) as $child_family) {
 					foreach ($child_family->getChildren(WT_PRIV_HIDE) as $grandchild) {
-						preg_match_all('/\n2 DATE (.+)/', $grandchild->getGedcomRecord(), $date_matches);
+						preg_match_all('/\n2 DATE (.+)/', $grandchild->getGedcom(), $date_matches);
 						// Assume grandchildren born after age of 30
 						foreach ($date_matches[1] as $date_match) {
 							$date=new WT_Date($date_match);
-							if ($date->isOK() && $date->MaxJD() <= WT_SERVER_JD - 365*($MAX_ALIVE_AGE-30)) {
+							if ($date->isOK() && $date->MaxJD() <= WT_CLIENT_JD - 365*($MAX_ALIVE_AGE-30)) {
 								return WT_I18N::translate('A grandchild with a birth date of %s suggests this person was born at least 30 years earlier than that.', $date->Display(!$SEARCH_SPIDER));
 							}
 						}
