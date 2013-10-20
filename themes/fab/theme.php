a03e267fefa982862bdff8a7a0723a1ce62a7366
Greg Roach <fisharebest@gmail.com>
2013-10-20 23:01:14 +0100

    Modernise the F.A.B. theme

--- themes/fab/theme.php
+++ themes/fab/theme.php
@@ -67,7 +67,7 @@ $fanChart = array(
 );
 
 // variables for the pedigree chart
-$bwidth        = 275; // width of boxes on pedigree chart
+$bwidth        = 260; // width of boxes on pedigree chart
 $bheight       = 80;  // height of boxes on pedigree chart
 $baseyoffset   = 10;  // position the entire pedigree tree relative to the top of the page
 $basexoffset   = 10;  // position the entire pedigree tree relative to the left of the page
