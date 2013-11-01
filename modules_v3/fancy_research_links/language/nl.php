b7e864e575095d1cba1f750e2fa4af478e9b13b9
JustCarmen <carmen@justcarmen.nl>
2013-11-01 23:43:47 +0100

    Fancy Research Links - initial commit

---
+++ modules_v3/fancy_research_links/language/nl.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('WT_WEBTREES')) {
+header('HTTP/1.0 403 Forbidden');
+exit;
+}
+
+return array(
+	'Research links' => 'Onderzoekskoppelingen',
+	'Een zijpaneel met snelle links naar populaire onderzoeksites.' => 'A sidebar tool to provide quick links to popular research web sites.'
+);
---
+++ modules_v3/fancy_research_links/plugins/familysearch.php
@@ -0,0 +1,37 @@
+<?php
+
+if (!defined('WT_WEBTREES')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+class familysearch_plugin extends research_base_plugin {
+	static function getName() {
+		return 'Family Search';
+	}
+	
+	/**
+	* Based on function print_name_record() in /library/WT/Controller/Individual.php
+	*/
+	static function create_link(WT_Fact $event) {
+		if (!$event->canShow()) {
+			return false;
+		}
+		$factrec = $event->getGedCom();
+		// Create a dummy record, so we can extract the formatted NAME value from the event.
+		$dummy=new WT_Individual(
+			'xref',
+			"0 @xref@ INDI\n1 DEAT Y\n".$factrec,
+			null,
+			WT_GED_ID
+		);
+		$all_names=$dummy->getAllNames();
+		$primary_name=$all_names[0];
+		
+		return $link = 'https://familysearch.org/search/record/results#count=20&query=%2Bgivenname%3A%22'
+						.rawurlencode($primary_name['givn'])
+						.'%22~%20%2Bsurname%3A%22'
+						.rawurlencode($primary_name['surname'])
+						.'%22~';
+	}
+}
