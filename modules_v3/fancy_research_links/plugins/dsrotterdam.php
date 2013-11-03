5df0de71a0c860e340b29c4ad6bfbf890d336238
JustCarmen <carmen@justcarmen.nl>
2013-11-03 08:12:27 +0100

    Plugin updated.

--- modules_v3/fancy_research_links/plugins/dsrotterdam.php
+++ modules_v3/fancy_research_links/plugins/dsrotterdam.php
@@ -16,7 +16,27 @@ class dsrotterdam_plugin extends research_base_plugin {
 	static function create_link(WT_Fact $event) {
 		if (!$event->canShow()) {
 			return false;
-		}		
-		return $link = 'http://rotterdam.digitalestamboom.nl/search.aspx?lang=nl';
+		}
+		$factrec = $event->getGedCom();
+		// Create a dummy record, so we can extract the formatted NAME value from the event.
+		$dummy=new WT_Individual(
+			'xref',
+			"0 @xref@ INDI\n1 DEAT Y\n".$factrec,
+			null,
+			WT_GED_ID
+		);
+		$all_names=$dummy->getAllNames();
+		$primary_name=$all_names[0];
+		
+		$givn   = $primary_name['givn'];
+		$surn   = $primary_name['surn'];
+		if($surn != $primary_name['surname']) {
+			$prefix = substr($primary_name['surname'], 0, strpos($primary_name['surname'], $surn) - 1);
+		}
+		else {
+			$prefix = "";
+		}
+				
+		return $link = 'http://rotterdam.digitalestamboom.nl/search.aspx?lang=nl&verder='.$givn.'||'.$prefix.'|'.$surn;;
 	}
 }
