a3ebd64ce43b4f9d80cda761179c5faa24fffd3b
Greg Roach <fisharebest@gmail.com>
2013-11-09 18:18:22 +0000

    Next release will be 1.5.1

--- includes/session.php
+++ includes/session.php
@@ -29,8 +29,8 @@ if (!defined('WT_SCRIPT_NAME')) {
 
 // Identify ourself
 define('WT_WEBTREES',        'webtrees');
-define('WT_VERSION',         '1.5.0');
-define('WT_VERSION_RELEASE', ''); // “dev”, “beta”, “rc1”, “”, etc.
+define('WT_VERSION',         '1.5.1');
+define('WT_VERSION_RELEASE', 'dev'); // “dev”, “beta”, “rc1”, “”, etc.
 define('WT_VERSION_TEXT',    trim(WT_VERSION.' '.WT_VERSION_RELEASE));
 
 // External URLs
