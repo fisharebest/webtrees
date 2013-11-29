ee571c501532a1d8196119d13f5103bb03721de1
Vytautas Krivickas <vytautas.krivickas@gmail.com>
2013-11-29 11:33:20 +0200

    Fix Csrf checks won't allow saving of the form Conflicts: 	modules_v3/faq/module.php

--- modules_v3/faq/module.php
+++ modules_v3/faq/module.php
@@ -159,7 +159,8 @@ class faq_WT_Module extends WT_Module implements WT_Module_Menu, WT_Module_Block
 
 			// "Help for this page" link
 			echo '<div id="page_help">', help_link('add_faq_item', $this->getName()), '</div>';
-			echo '<form name="faq" method="post" action="#">';
+			echo '<form name="faq" method="post" action="module.php?mod=', $this->getName(), '&amp;mod_action=admin_edit">';
+			echo WT_Filter::getCsrf();
 			echo '<input type="hidden" name="save" value="1">';
 			echo '<input type="hidden" name="block_id" value="', $block_id, '">';
 			echo '<table id="faq_module">';
