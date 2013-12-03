db52ebd4c05184a8a7b621eff5c07ecf2fb9c9dd
David Drury <david@drury.me.uk>
2013-12-03 13:23:14 +0000

    Combine gedcom_news & user_blog modules
    Uses modal dialogs for add & edit functions, editnews.php is now
    redundant. Each user has a set of configuration parameters (previously
    config was hard coded)

---
+++ modules_v3/blog/db_schema/db_schema_0_1.php
@@ -0,0 +1,52 @@
+<?php
+// Update the news/blog module database schema from version 0 to version 1
+//
+// Version 0: empty database
+// Version 1: create the tables, as per PGV 4.2.1
+//
+// The script should assume that it can be interrupted at
+// any point, and be able to continue by re-running the script.
+// Fatal errors, however, should be allowed to throw exceptions,
+// which will be caught by the framework.
+// It shouldn't do anything that might take more than a few
+// seconds, for systems with low timeout values.
+//
+// webtrees: Web based Family History software
+// Copyright (C) 2013 webtrees development team.
+//
+// Derived from PhpGedView
+// Copyright (C) 2009 Greg Roach
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+if (!defined('WT_WEBTREES')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+WT_DB::exec(
+	"CREATE TABLE IF NOT EXISTS `##news` (".
+	" n_id       INTEGER AUTO_INCREMENT NOT NULL,".
+	" n_username VARCHAR(100)           NOT NULL,".
+	" n_date     INTEGER                NOT NULL,".
+	" n_title    VARCHAR(255)           NOT NULL,".
+	" n_text     TEXT                   NOT NULL,".
+	" PRIMARY KEY     (n_id),".
+	"         KEY ix1 (n_username)".
+	") COLLATE utf8_unicode_ci ENGINE=InnoDB"
+);
+
+// Update the version to indicate success
+WT_Site::preference($schema_name, $next_version);
