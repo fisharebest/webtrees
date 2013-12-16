ff66886c6108459448a7b9ceb901a1aadf71a2bd
Greg Roach <fisharebest@gmail.com>
2013-12-16 22:59:13 +0000

    #1259086 - branches sosa of spouse

--- library/WT/Controller/Branches.php
+++ library/WT/Controller/Branches.php
@@ -188,10 +188,10 @@ class WT_Controller_Branches extends WT_Controller_Page {
 
 				$spouse = $family->getSpouse($individual);
 				if ($spouse) {
-					$sosa = array_search($spouse->getXref(), $this->ancestors);
+					$sosa = array_search($spouse, $this->ancestors);
 					if ($sosa) {
 						$sosa_class = 'search_hit';
-						$sosa_html  = ' <a class="details1 ' . $spouse->getBoxStyle() . '" title="' . WT_I18N::translate('Sosa') . '" href="relationship.php?pid2=' . WT_USER_ROOT_ID . '&amp;pid1=' . $spouse->getXref() . '"> '.$sosa.' </a>' . self::sosaGeneration($sosa2);
+						$sosa_html  = ' <a class="details1 ' . $spouse->getBoxStyle() . '" title="' . WT_I18N::translate('Sosa') . '" href="relationship.php?pid2=' . WT_USER_ROOT_ID . '&amp;pid1=' . $spouse->getXref() . '"> '.$sosa.' </a>' . self::sosaGeneration($sosa);
 					} else {
 						$sosa_class = '';
 						$sosa_html  = '';
