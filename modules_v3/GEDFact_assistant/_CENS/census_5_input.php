af19214b460eecbd7a5604c7ec7a53e4d1698c93
weberho <jweberhofer@weberhofer.at>
2014-01-04 11:24:46 +0100

    Fix address of FSF, replace licence of GPLv2 by latest version

--- modules_v3/GEDFact_assistant/_CENS/census_5_input.php
+++ modules_v3/GEDFact_assistant/_CENS/census_5_input.php
@@ -21,7 +21,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 require WT_ROOT.WT_MODULES_DIR.'GEDFact_assistant/_CENS/js/census_5_input.js.php';
 
--- modules_v3/GEDFact_assistant/_CENS/js/date.js
+++ modules_v3/GEDFact_assistant/_CENS/js/date.js
@@ -40,7 +40,7 @@ Date.$VERSION = 1.02;
 //
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 // Utility function to append a 0 to single-digit numbers
 Date.LZ = function(x) {return(x<0||x>9?"":"0")+x};
