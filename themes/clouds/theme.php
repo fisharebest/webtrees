4c8687e8900992f025f6002c4a7de5d3e1dfa8b2
Greg Roach <fisharebest@gmail.com>
2014-01-24 23:08:40 +0000

    Rename CSS/JS files with new version number

--- themes/clouds/theme.php
+++ themes/clouds/theme.php
@@ -48,7 +48,7 @@ function getMenuAsCustomList($menu) {
 $theme_name = "clouds";
 
 // A version number in the path prevents browser-cache problems during upgrade
-define('WT_CSS_URL', WT_THEME_URL . 'css-1.5.1/');
+define('WT_CSS_URL', WT_THEME_URL . 'css-1.5.2/');
 
 $headerfile = WT_THEME_DIR . 'header.php';
 $footerfile = WT_THEME_DIR . 'footer.php';
