823af7974a11a173407247a1b11dbc6ea50c8e7b
Greg Roach <fisharebest@gmail.com>
2014-01-24 23:09:46 +0000

    Update list of old files

--- admin.php
+++ admin.php
@@ -583,6 +583,15 @@ function old_paths() {
 		WT_ROOT.'themes/minimal/css-1.5.0',
 		WT_ROOT.'themes/webtrees/css-1.5.0',
 		WT_ROOT.'themes/xenea/css-1.5.0',
+		// Removed in 1.5.2
+		WT_ROOT.'js/webtrees-1.5.1.js',
+		WT_ROOT.'themes/_administration/css-1.5.1',
+		WT_ROOT.'themes/clouds/css-1.5.1',
+		WT_ROOT.'themes/colors/css-1.5.1',
+		WT_ROOT.'themes/fab/css-1.5.1',
+		WT_ROOT.'themes/minimal/css-1.5.1',
+		WT_ROOT.'themes/webtrees/css-1.5.1',
+		WT_ROOT.'themes/xenea/css-1.5.1',
 	);
 }
 
