1df7e17260acd3634417b6f7d7bcb127235408a7
Rico Sonntag <magic_sunday@gmx.de>
2014-02-20 22:06:52 +0100

    Added basic view class.

---
+++ library/WT/View.php
@@ -0,0 +1,142 @@
+<?php
+//declare(encoding = 'UTF-8');
+
+/**
+ * Simple view class.
+ *
+ * @category Library
+ * @author   Rico Sonntag <mail@ricosonntag.de>
+ */
+class WT_View
+{
+	/**
+	 * File name of view to render.
+	 *
+	 * @var string
+	 */
+	protected $fileName;
+
+	/**
+	 * Data assigned to the view.
+	 *
+	 * @var array
+	 */
+	protected $data;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $fileName File name of view
+	 *
+	 * @return void
+	 */
+	public function __construct($fileName)
+	{
+		$this->fileName = $fileName;
+	}
+
+	/**
+	 * Escape given string.
+	 *
+	 * @param string $str String to escape
+	 *
+	 * @return string
+	 */
+	public function escape($str)
+	{
+		if (defined('ENT_SUBSTITUTE')) {
+			// PHP5.4 allows us to substitute invalid UTF8 sequences
+			return htmlspecialchars($str, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+		}
+
+		return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+	}
+
+	/**
+	 * Translate given string.
+	 *
+	 * @param string $str String to translate
+	 *
+	 * @return string
+	 */
+	public function translate($str)
+	{
+		return $this->escape(WT_I18N::translate($str));
+	}
+
+	/**
+	 * Magic getter method to get value of an assigned variable.
+	 *
+	 * @param string $name Name of variable to get value
+	 *
+	 * @return mixed|false FALSE if variable is not assigned
+	 */
+	public function __get($name)
+	{
+		if (isset($this->data[$name])) {
+			return $this->data[$name];
+		}
+
+		return false;
+	}
+
+	/**
+	 * Magic setter method to assign data to the view.
+	 *
+	 * @param string $name  Name of variable to assign value to
+	 * @param mixed  $value Value of variable
+	 *
+	 * @return void
+	 */
+	public function __set($name, $value)
+	{
+		$this->data[$name] = $value;
+	}
+
+	/**
+	 * Render view.
+	 *
+	 * @return void
+	 */
+	public function render()
+	{
+		ob_start();
+
+		include $this->fileName;
+
+		$rendered = ob_get_clean();
+
+		echo $rendered;
+	}
+
+	/**
+	 * Render a view partial.
+	 *
+	 * @param string $fileName Name of partial to render
+	 * @param array  $data     Data to assign to the view partial
+	 *
+	 * @return void
+	 */
+	public function partial($fileName, array $data = array())
+	{
+		$partialView = new View($fileName);
+
+		foreach ($data as $key => $value) {
+			$partialView->$key = $value;
+		}
+
+		$partialView->render();
+	}
+
+	/**
+	 * Return class representation as string. Auto renders an
+	 * assigned child view.
+	 *
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return $this->render();
+	}
+}
+?>
