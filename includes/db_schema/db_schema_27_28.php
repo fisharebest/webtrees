eca1e5174b7fa9d0d31ea4477c151bf10c5a4545
Greg Roach <fisharebest@gmail.com>
2014-02-22 22:56:23 +0000

    #1280687 Change in Geonames API interface

---
+++ includes/db_schema/db_schema_27_28.php
@@ -0,0 +1,40 @@
+<?php
+// Update the database schema from version 27-28
+// - delete unused settings and update indexes
+//
+// The script should assume that it can be interrupted at
+// any point, and be able to continue by re-running the script.
+// Fatal errors, however, should be allowed to throw exceptions,
+// which will be caught by the framework.
+// It shouldn't do anything that might take more than a few
+// seconds, for systems with low timeout values.
+//
+// webtrees: Web based Family History software
+// Copyright (C) 2014 Greg Roach
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+if (!defined('WT_WEBTREES')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+// Delete old/unused settings
+self::exec(
+	"DELETE FROM `##gedcom_setting` WHERE setting_name IN ('USE_GEONAMES')"
+);
+
+// Update the version to indicate success
+WT_Site::preference($schema_name, $next_version);
