c2a1d2c58c37a1283f127b494680f4b5dc148544
Greg Roach <fisharebest@gmail.com>
2014-03-31 16:33:15 +0100

    Type - invalid function name

--- save.php
+++ save.php
@@ -240,7 +240,7 @@ case 'user_setting':
 	//////////////////////////////////////////////////////////////////////////////
 
 	// Authorisation
-	if (!(WT_USER_IS_ADMIN || WT_USER_ID && get_user_setting($id1, 'editaccount') && _array($id2, array('language','visible_online','contact_method')))) {
+	if (!(WT_USER_IS_ADMIN || WT_USER_ID && get_user_setting($id1, 'editaccount') && in_array($id2, array('language','visible_online','contact_method')))) {
 		fail();
 	}
 
