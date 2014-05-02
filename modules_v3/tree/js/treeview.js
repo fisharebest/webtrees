0b01d56876149f854497566d6bc5574893bd9475
Greg Roach <fisharebest@gmail.com>
2014-05-02 23:12:36 +0100

    Interactive tree - code cleanup

--- modules_v3/tree/js/treeview.js
+++ modules_v3/tree/js/treeview.js
@@ -17,92 +17,92 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-function TreeViewHandler(treeviewInstance) {
-	this.treeview = jQuery("#" + treeviewInstance + "_in");
-	this.loadingImage = jQuery("#" + treeviewInstance + "_loading");
+
+function TreeViewHandler(treeview_instance) {
+	var tv = this; // Store "this" for usage within jQuery functions where "this" is not this ;-)
+
+	this.treeview = jQuery("#" + treeview_instance + "_in");
+	this.loadingImage = jQuery("#" + treeview_instance + "_loading");
 	this.toolbox = jQuery("#tv_tools");
 	this.buttons = jQuery(".tv_button:first", this.toolbox);
 	this.zoom = 100; // in percent
 	this.boxWidth = 180; // default family box width
 	this.boxExpandedWidth = 250; // default expanded family box width
 	this.cookieDays = 3; // lifetime of preferences memory, in days
-	this.ajaxUrl = "module.php?mod=tree&instance=" + treeviewInstance + 
+	this.ajaxUrl = "module.php?mod=tree&instance=" + treeview_instance +
 		"&mod_action=";
 
-	this.container = this.treeview.parent(); // Store the container element ("#" + treeviewInstance + "_out")
+	this.container = this.treeview.parent(); // Store the container element ("#" + treeview_instance + "_out")
 	this.auto_box_width = false;
 	this.updating = false;
-	this.overLevel = 0; // internal var for handling submenus
-	
-	var tv = this; // Store "this" for usage within jQuery functions where "this" is not this ;-)
 
 	// Restore user preferences
-	if (readCookie("compact") == "true") {
+	if (readCookie("compact") === "true") {
 		tv.compact();
 	}
 
 	// Define the draggables
 	tv.treeview.draggable({
 		cursor: "move",
-		stop: function(event, ui) {
+		stop: function () {
 			tv.updateTree();
 		}
 	});
-	
+
 	// Add click handlers to buttons
-	tv.toolbox.find("#tvbCompact").each(function(index, tvCompact) {
-		tvCompact.onclick = function() {
+	tv.toolbox.find("#tvbCompact").each(function (index, tvCompact) {
+		tvCompact.onclick = function () {
 			tv.compact();
-		}
+		};
 	});
 	// If we click the "hide/show all partners" button, toggle the setting before reloading the page
-	tv.toolbox.find("#tvbAllPartners").each(function(index, tvAllPartners) {
-		tvAllPartners.onclick = function() {
+	tv.toolbox.find("#tvbAllPartners").each(function (index, tvAllPartners) {
+		tvAllPartners.onclick = function () {
 			createCookie("allPartners", readCookie("allPartners") === "true" ? "false" : "true", tv.cookieDays);
-		}
+		};
 	});
-	tv.toolbox.find("#tvbOpen").each(function(index, tvbOpen) {
+	tv.toolbox.find("#tvbOpen").each(function (index, tvbOpen) {
 		var b = jQuery(tvbOpen, tv.toolbox);
-		tvbOpen.onclick = function() {
+		tvbOpen.onclick = function () {
 			b.addClass("tvPressed");
 			tv.setLoading();
 			var e = jQuery.Event("click");
-			tv.treeview.find(".tv_box:not(.boxExpanded)").each(function(index, box){
+			tv.treeview.find(".tv_box:not(.boxExpanded)").each(function (index, box) {
 				var pos = jQuery(box, tv.treeview).offset();
-				if ((pos.left >= tv.leftMin) && (pos.left <= tv.leftMax) && (pos.top >= tv.topMin) && (pos.top <= tv.topMax)) {
-					tv.expandBox(box,e);
+				if (pos.left >= tv.leftMin && pos.left <= tv.leftMax && pos.top >= tv.topMin && pos.top <= tv.topMax) {
+					tv.expandBox(box, e);
 				}
 			});
 			b.removeClass("tvPressed");
 			tv.setComplete();
-		}
+		};
 	});
-	tv.toolbox.find("#tvbClose").each(function(index, tvbClose) {
-		var b = jQuery(tvbClose,tv.toolbox );
-		tvbClose.onclick = function() {
+	tv.toolbox.find("#tvbClose").each(function (index, tvbClose) {
+		var b = jQuery(tvbClose, tv.toolbox);
+		tvbClose.onclick = function () {
 			b.addClass("tvPressed");
 			tv.setLoading();
-			tv.treeview.find(".tv_box.boxExpanded").each(function(index, box){
+			tv.treeview.find(".tv_box.boxExpanded").each(function (index, box) {
 				jQuery(box).css("display", "none").removeClass("boxExpanded").parent().find(".tv_box.collapsedContent").css("display", "block");
 			});
 			b.removeClass("tvPressed");
 			tv.setComplete();
-		}
+		};
 	});
-	
+
 	tv.centerOnRoot(); // fire ajax update if needed, which call setComplete() when all is loaded
 }
 /**
  * Class TreeView setLoading method
  */
-TreeViewHandler.prototype.setLoading = function() {
+TreeViewHandler.prototype.setLoading = function () {
 	this.treeview.css("cursor", "wait");
 	this.loadingImage.css("display", "block");
 };
 /**
  * Class TreeView setComplete  method
  */
-TreeViewHandler.prototype.setComplete = function() {
+TreeViewHandler.prototype.setComplete = function () {
 	this.treeview.css("cursor", "move");
 	this.loadingImage.css("display", "none");
 };
@@ -111,7 +111,7 @@ TreeViewHandler.prototype.setComplete = function() {
  * Class TreeView getSize  method
  * Store the viewport current size
  */
-TreeViewHandler.prototype.getSize = function() {
+TreeViewHandler.prototype.getSize = function () {
 	var tv = this;
 	// retrieve the current container bounding box
 	var container = tv.container.parent();
@@ -121,9 +121,9 @@ TreeViewHandler.prototype.getSize = function() {
 	tv.topMin = offset.top;
 	tv.topMax = tv.topMin + container.innerHeight();
 	/*
-	var frm = jQuery("#tvTreeBorder");
-	tv.treeview.css("width", frm.width());
-	tv.treeview.css("height", frm.height());*/
+	 var frm = jQuery("#tvTreeBorder");
+	 tv.treeview.css("width", frm.width());
+	 tv.treeview.css("height", frm.height());*/
 };
 
 /**
@@ -131,43 +131,43 @@ TreeViewHandler.prototype.getSize = function() {
  * Perform ajax requests to complete the tree after drag
  * param boolean @center center on root person when done
  */
-TreeViewHandler.prototype.updateTree = function(center, button) {
+TreeViewHandler.prototype.updateTree = function (center, button) {
 	var tv = this; // Store "this" for usage within jQuery functions where "this" is not this ;-)
-	var toLoad = new Array();
-	var elts = new Array();
+	var to_load = [];
+	var elts = [];
 	this.getSize();
 
 	// check which td with datafld attribute are within the container bounding box
 	// and therefore need to be dynamically loaded
-	tv.treeview.find("td[abbr]").each(function(index, el) {
+	tv.treeview.find("td[abbr]").each(function (index, el) {
 		el = jQuery(el, tv.treeview);
 		var pos = el.offset();
-		if ((pos.left >= tv.leftMin) && (pos.left <= tv.leftMax) && (pos.top >= tv.topMin) && (pos.top <= tv.topMax)) {
-			toLoad.push(el.attr("abbr"));
+		if (pos.left >= tv.leftMin && pos.left <= tv.leftMax && pos.top >= tv.topMin && pos.top <= tv.topMax) {
+			to_load.push(el.attr("abbr"));
 			elts.push(el);
 		}
 	});
 	// if some boxes need update, we perform an ajax request
-	if (toLoad.length > 0) {
+	if (to_load.length > 0) {
 		tv.updating = true;
 		tv.setLoading();
 		jQuery.ajax({
 			url: tv.ajaxUrl + "getPersons",
 			dataType: "json",
-			data: "q=" + toLoad.join(";"),
-			success: function(ret) {
+			data: "q=" + to_load.join(";"),
+			success: function (ret) {
 				var nb = elts.length;
-				var rootEl = jQuery(".rootPerson", this.treeview);
-				var l = rootEl.offset().left;
-				for (var i=0;i<nb;i++) {
+				var root_element = jQuery(".rootPerson", this.treeview);
+				var l = root_element.offset().left;
+				for (var i = 0; i < nb; i++) {
 					elts[i].removeAttr("abbr").html(ret[i]);
 				}
 				// repositionning
-				tv.treeview.offset({left: tv.treeview.offset().left - rootEl.offset().left +l});
+				tv.treeview.offset({left: tv.treeview.offset().left - root_element.offset().left + l});
 				// we now ajust the draggable treeview size to its content size
 				tv.getSize();
 			},
-			complete: function() {
+			complete: function () {
 				if (tv.treeview.find("td[abbr]").length) {
 					tv.updateTree(center, button); // recursive call
 				}
@@ -185,7 +185,7 @@ TreeViewHandler.prototype.updateTree = function(center, button) {
 				tv.setComplete();
 				tv.updating = false;
 			},
-			timeout: function() {
+			timeout: function () {
 				if (button) {
 					button.removeClass("tvPressed");
 				}
@@ -205,21 +205,11 @@ TreeViewHandler.prototype.updateTree = function(center, button) {
 /**
  * Class TreeView compact method
  */
-TreeViewHandler.prototype.compact = function() {
+TreeViewHandler.prototype.compact = function () {
 	var tv = this;
 	var b = jQuery("#tvbCompact", tv.toolbox);
 	tv.setLoading();
-	if (!tv.auto_box_width) {
-		tv.treeview.find(".tv_box").css("width", "auto");
-		tv.auto_box_width = true;
-		if (!readCookie("compact")) {
-			createCookie("compact", true, tv.cookieDays);
-		}
-		if (!tv.updating) {
-			tv.updateTree();
-		}
-		b.addClass("tvPressed");
-	} else {
+	if (tv.auto_box_width) {
 		var w = tv.boxWidth * (tv.zoom / 100) + "px";
 		var ew = tv.boxExpandedWidth * (tv.zoom / 100) + "px";
 		tv.treeview.find(".tv_box:not(boxExpanded)", tv.treeview).css("width", w);
@@ -229,6 +219,16 @@ TreeViewHandler.prototype.compact = function() {
 			createCookie("compact", false, tv.cookieDays);
 		}
 		b.removeClass("tvPressed");
+	} else {
+		tv.treeview.find(".tv_box").css("width", "auto");
+		tv.auto_box_width = true;
+		if (!readCookie("compact")) {
+			createCookie("compact", true, tv.cookieDays);
+		}
+		if (!tv.updating) {
+			tv.updateTree();
+		}
+		b.addClass("tvPressed");
 	}
 	tv.setComplete();
 	return false;
@@ -237,18 +237,19 @@ TreeViewHandler.prototype.compact = function() {
 /**
  * Class TreeView centerOnRoot method
  */
-TreeViewHandler.prototype.centerOnRoot = function() {
+TreeViewHandler.prototype.centerOnRoot = function () {
 	this.loadingImage.css("display", "block");
 	var tvc = this.container;
-	var tvcW = tvc.innerWidth() / 2;
-	if(isNaN(tvcW)) {
+	var tvc_width = tvc.innerWidth() / 2;
+	if (isNaN(tvc_width)) {
 		return false;
 	}
-	var tvcH = tvc.innerHeight() / 2;
-	var el = jQuery(".rootPerson", this.treeview);
-	var dLeft = tvc.offset().left + this.treeview.offset().left + tvcW - el.offset().left - el.outerWidth()/2;
-	var dTop = tvc.offset().top + this.treeview.offset().top + tvcH - el.offset().top - el.outerHeight()/2;
-	this.treeview.offset({left: dLeft, top: dTop});
+	var tvc_height = tvc.innerHeight() / 2;
+	var root_person = jQuery(".rootPerson", this.treeview);
+	this.treeview.offset({
+		left: tvc.offset().left + this.treeview.offset().left + tvc_width - root_person.offset().left - root_person.outerWidth() / 2,
+		top: tvc.offset().top + this.treeview.offset().top + tvc_height - root_person.offset().top - root_person.outerHeight() / 2
+	});
 
 	if (!this.updating) {
 		this.updateTree(true);
@@ -261,10 +262,10 @@ TreeViewHandler.prototype.centerOnRoot = function() {
  * param string @box the person box element
  * param string @event the call event
  * param string @pid the person id
- * 
+ *
  * called ONLY for elements which have NOT the class tv_link to avoid unuseful requests to the server
  */
-TreeViewHandler.prototype.expandBox = function(box, event) {
+TreeViewHandler.prototype.expandBox = function (box, event) {
 	var t = jQuery(event.target);
 	if (t.hasClass("tv_link")) {
 		return false;
@@ -276,7 +277,7 @@ TreeViewHandler.prototype.expandBox = function(box, event) {
 	var tv = this; // Store "this" for usage within jQuery functions where "this" is not this ;-)
 	var expanded;
 	var collapsed;
-	
+
 	if (bc.hasClass("detailsLoaded")) {
 		collapsed = bc.find(".collapsedContent");
 		expanded = bc.find(".tv_box:not(.collapsedContent)");
@@ -287,18 +288,18 @@ TreeViewHandler.prototype.expandBox = function(box, event) {
 		bc.append(collapsed.addClass("collapsedContent").css("display", "none"));
 		// we add a waiting image at the right side of the box
 		// TODO : manage rtl (left side instead of right in rtl mode)
-		var loadingImage = this.loadingImage.find("img").clone().addClass("tv_box_loading").css("display", "block");
-		box.prepend(loadingImage);
+		var loading_image = this.loadingImage.find("img").clone().addClass("tv_box_loading").css("display", "block");
+		box.prepend(loading_image);
 		tv.updating = true;
 		tv.setLoading();
 		// perform the Ajax request and load the result in the box
-		box.load(tv.ajaxUrl + "getDetails&pid=" + pid, function() {
+		box.load(tv.ajaxUrl + "getDetails&pid=" + pid, function () {
 			// If Lightbox module is active, we reinitialize it for the new links
-			if (typeof CB_Init == "function") {
+			if (typeof CB_Init === "function") {
 				CB_Init();
 			}
 			box.css("width", tv.boxExpandedWidth * (tv.zoom / 100) + "px");
-			loadingImage.remove();
+			loading_image.remove();
 			bc.addClass("detailsLoaded");
 			tv.setComplete();
 			tv.updating = false;
@@ -318,26 +319,26 @@ TreeViewHandler.prototype.expandBox = function(box, event) {
 	return false;
 };
 
-function createCookie(name,value,days) {
+function createCookie(name, value, days) {
 	if (days) {
 		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		document.cookie = name + "=" + value + "; expires=" + date.toGMTString() + "; path=/";
 	} else {
-		var expires = "";
+		document.cookie = name + "=" + value + "; path=/";
 	}
-	document.cookie = name+"="+value+expires+"; path=/";
 }
+
 function readCookie(name) {
-	var nameEQ = name + "=";
+	var name_equals = name + "=";
 	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
+	for (var i = 0; i < ca.length; i++) {
 		var c = ca[i];
-		while (c.charAt(0)==' ') {
-			c = c.substring(1,c.length);
+		while (c.charAt(0) === ' ') {
+			c = c.substring(1, c.length);
 		}
-		if (c.indexOf(nameEQ) == 0) {
-			return c.substring(nameEQ.length,c.length);
+		if (c.indexOf(name_equals) === 0) {
+			return c.substring(name_equals.length, c.length);
 		}
 	}
 	return null;
