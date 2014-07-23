085be403cf41d92c467791ef6eb8c80b0c69504b
Greg Roach <fisharebest@gmail.com>
2014-07-23 20:30:13 +0100

    Use external library to generate UUIDs

--- library/composer/autoload_namespaces.php
+++ library/composer/autoload_namespaces.php
@@ -8,6 +8,7 @@ $baseDir = dirname($vendorDir);
 return array(
     'Zend_' => array($vendorDir . '/bombayworks/zendframework1/library'),
     'WT_' => array($vendorDir . '/WT'),
+    'Rhumsaa\\Uuid' => array($vendorDir . '/rhumsaa/uuid/src'),
     'Patchwork' => array($vendorDir . '/patchwork/utf8/class'),
     'Normalizer' => array($vendorDir . '/patchwork/utf8/class'),
     'Michelf' => array($vendorDir . '/michelf/php-markdown'),
