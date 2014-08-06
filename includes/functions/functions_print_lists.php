5a1859b18296bf89105e77c2de61132eccb4dd7e
Greg Roach <fisharebest@gmail.com>
2014-08-06 07:15:49 +0100

    Unescaped HTML in shared-note list

--- includes/functions/functions_print_lists.php
+++ includes/functions/functions_print_lists.php
@@ -1230,7 +1230,7 @@ function format_note_table($datalist) {
 		}
 		//-- Delete
 		if (WT_USER_GEDCOM_ADMIN) {
-			$html .= '<td><div title="'. WT_I18N::translate('Delete'). '" class="deleteicon" onclick="return delete_note(\'' . WT_I18N::translate('Are you sure you want to delete “%s”?', strip_tags($note->getFullName())) . "', '" . $note->getXref() . '\');"><span class="link_text">'. WT_I18N::translate('Delete'). '</span></div></td>';
+			$html .= '<td><div title="'. WT_I18N::translate('Delete'). '" class="deleteicon" onclick="return delete_note(\'' . WT_I18N::translate('Are you sure you want to delete “%s”?', WT_Filter::escapeJs(WT_Filter::unescapeHtml($note->getFullName()))) . "', '" . $note->getXref() . '\');"><span class="link_text">'. WT_I18N::translate('Delete'). '</span></div></td>';
 		} else {
 			$html .= '<td></td>';
 		}
