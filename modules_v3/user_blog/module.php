5227e446a0d31a5cffeb27d4e74c04340f1bd715
Greg Roach <fisharebest@gmail.com>
2014-09-18 22:19:28 +0100

    User journal module won't install without news module

--- modules_v3/user_blog/module.php
+++ modules_v3/user_blog/module.php
@@ -28,7 +28,7 @@ if (!defined('WT_WEBTREES')) {
 
 // Create tables, if not already present
 try {
-	WT_DB::updateSchema(WT_MODULES_DIR, 'user_blog/db_schema/', 'NB_SCHEMA_VERSION', 3);
+	WT_DB::updateSchema(WT_MODULES_DIR . 'user_blog/db_schema/', 'NB_SCHEMA_VERSION', 3);
 } catch (PDOException $ex) {
 	// The schema update scripts should never fail.  If they do, there is no clean recovery.
 	die($ex);
