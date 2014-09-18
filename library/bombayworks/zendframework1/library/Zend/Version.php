d41520a343b581ec5e36379e39e1d04685053a99
Greg Roach <fisharebest@gmail.com>
2014-09-18 16:07:18 +0100

    Update vendor code: zend-framework and ext-calendar

--- library/bombayworks/zendframework1/library/Zend/Version.php
+++ library/bombayworks/zendframework1/library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.12.8';
+    const VERSION = '1.12.9';
 
     /**
      * The latest stable version Zend Framework available
