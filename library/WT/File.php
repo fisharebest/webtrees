5434f4dd757c4c354399b52281a0d654870a90b5
Greg Roach <fisharebest@gmail.com>
2014-10-15 17:27:13 +0100

    PHPDoc

--- library/WT/File.php
+++ library/WT/File.php
@@ -19,19 +19,23 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 class WT_File {
-	//////////////////////////////////////////////////////////////////////////////
-	// Fetch a remote file
-	// Note that fopen() and file_get_contents() are often unvailable, as they
-	// can easily be exploited by application bugs, and are therefore disabled.
-	// Hence we use fsockopen().
-	// To allow arbitrarily large downloads with small memory limits, we either
-	// write output to a stream or return it.
-	//////////////////////////////////////////////////////////////////////////////
-
-	public static function fetchUrl($url, $stream=null) {
-		$host  = parse_url($url, PHP_URL_HOST);
-		$port  = parse_url($url, PHP_URL_PORT);
-		$path  = parse_url($url, PHP_URL_PATH);
+	/**
+	 * Fetch a remote file
+	 * Note that fopen() and file_get_contents() are often unvailable, as they
+	 * can easily be exploited by application bugs, and are therefore disabled.
+	 * Hence we use fsockopen().
+	 * To allow arbitrarily large downloads with small memory limits, we either
+	 * write output to a stream or return it.
+	 *
+	 * @param string $url
+	 * @param null   $stream
+	 *
+	 * @return null|string
+	 */
+	public static function fetchUrl($url, $stream = null) {
+		$host = parse_url($url, PHP_URL_HOST);
+		$port = parse_url($url, PHP_URL_PORT);
+		$path = parse_url($url, PHP_URL_PATH);
 		$query = parse_url($url, PHP_URL_QUERY);
 
 		if (!$port) {
@@ -53,6 +57,7 @@ class WT_File {
 		// The file has moved?  Follow it.
 		if (preg_match('/^HTTP\/1.[01] 30[23].+\nLocation: ([^\r\n]+)/s', $response, $match)) {
 			fclose($fp);
+
 			return WT_File::fetchUrl($match[1], $stream);
 		} else {
 			// The response includes headers, a blank line, then the content
@@ -65,20 +70,25 @@ class WT_File {
 				fwrite($stream, $tmp);
 			}
 			fclose($fp);
+
 			return null;
-		} else  {
+		} else {
 			while ($tmp = fread($fp, 8192)) {
 				$response .= $tmp;
 			}
 			fclose($fp);
+
 			return $response;
 		}
 	}
 
-	//////////////////////////////////////////////////////////////////////////////
-	// Recursively delete a folder or file
-	//////////////////////////////////////////////////////////////////////////////
-
+	/**
+	 * Recursively delete a folder or file
+	 *
+	 * @param string $path
+	 *
+	 * @return bool Was the file deleted
+	 */
 	public static function delete($path) {
 		// In case the file is marked read-only
 		@chmod($path, 0777);
@@ -95,13 +105,17 @@ class WT_File {
 		} else {
 			@unlink($path);
 		}
+
 		return !file_exists($path);
 	}
 
-	//////////////////////////////////////////////////////////////////////////////
-	// Create a folder, and sub-folders, if it does not already exist
-	//////////////////////////////////////////////////////////////////////////////
-
+	/**
+	 * Create a folder, and sub-folders, if it does not already exist
+	 *
+	 * @param string $path
+	 *
+	 * @return bool Does the folder now exist
+	 */
 	public static function mkdir($path) {
 		if (is_dir($path)) {
 			return true;
@@ -110,6 +124,7 @@ class WT_File {
 				WT_File::mkdir(dirname($path));
 			}
 			@mkdir($path);
+
 			return is_dir($path);
 		}
 	}
