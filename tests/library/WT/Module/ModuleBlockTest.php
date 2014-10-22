b8b18a48175a555520093aac42954d8fd7b90356
Greg Roach <fisharebest@gmail.com>
2014-10-22 15:08:39 +0100

    Basic tests for classes (simply check that they can be loaded/parsed).

--- tests/library/WT/Module/ModuleBlockTest.php
+++ tests/library/WT/Module/ModuleBlockTest.php
@@ -22,10 +22,11 @@ class ModuleBlockTest extends PHPUnit_Framework_TestCase {
 	}
 
 	/**
-	 * Test FooBar
+	 * Test that the class exists
 	 *
 	 * @return void
 	 */
-	public function testFooBar() {
+	public function testClassExists() {
+		$this->assertTrue(interface_exists('\WT_Module_Block'));
 	}
 }
