1db34b237471242aa36703f190ac01cb6fa87cb6
Greg Roach <fisharebest@gmail.com>
2014-12-12 10:29:26 +0000

    Fix #319 jquery CSS files

--- themes/minimal/header.php
+++ themes/minimal/header.php
@@ -52,7 +52,6 @@ echo
 	'<title>', WT_Filter::escapeHtml($title), '</title>',
 	'<link rel="icon" href="', WT_CSS_URL, 'favicon.png" type="image/png">',
 	'<link rel="stylesheet" type="text/css" href="', WT_THEME_URL, 'jquery-ui-1.11.2/jquery-ui.css">',
-	'<link rel="stylesheet" type="text/css" href="', WT_THEME_URL, 'jquery-ui-1.11.2/jquery-ui.theme.css">',
 	'<link rel="stylesheet" type="text/css" href="', WT_CSS_URL, 'style.css">',
 	'<!--[if IE]>',
 	'<link type="text/css" rel="stylesheet" href="', WT_CSS_URL, 'msie.css">',
