c0f250978363d212adf902e0d3b7006518711fdc
Greg Roach <fisharebest@gmail.com>
2014-12-17 21:30:42 +0000

    Use HTML5 structural markup in xenea theme

--- themes/xenea/footer.php
+++ themes/xenea/footer.php
@@ -26,7 +26,7 @@ if (!defined('WT_WEBTREES')) {
 	exit;
 }
 
-echo '</div>'; // <div id="content">
+echo '</main>';
 if ($view!='simple') {
 	echo '<div id="footer" class="', $TEXT_DIRECTION, ' width99 center">';
 	echo contact_links();
@@ -41,5 +41,5 @@ if ($view!='simple') {
 		echo '<p class="error center">', WT_I18N::translate('There are pending changes for you to moderate.'), '</p>';
 		echo '</a>';
 	}
-	echo '</div>'; // <div id="footer">
+	echo '</footer>';
 }
