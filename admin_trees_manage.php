f03ee39a20d3246b6ab092ccd3c3f9edb699072e
Greg Roach <fisharebest@gmail.com>
2015-02-07 20:55:41 +0000

    Importing tree?  Default to previous GEDCOM filename

--- admin_trees_manage.php
+++ admin_trees_manage.php
@@ -166,7 +166,7 @@ case 'importform':
 	echo '<p>', /* I18N: %s is the name of a family tree */ I18N::translate('This will delete all the genealogy data from “%s” and replace it with data from another GEDCOM file.', $tree->getTitleHtml()), '</p>';
 	// the javascript in the next line strips any path associated with the file before comparing it to the current GEDCOM name (both Chrome and IE8 include c:\fakepath\ in the filename).
 	$previous_gedcom_filename = $tree->getPreference('gedcom_filename');
-	echo '<form name="replaceform" method="post" enctype="multipart/form-data" onsubmit="var newfile = document.replaceform.ged_name.value; newfile = newfile.substr(newfile.lastIndexOf(\'\\\\\')+1); if (newfile!=\'', Filter::escapeHtml($previous_gedcom_filename), '\' && \'\' != \'', Filter::escapeHtml($previous_gedcom_filename), '\') return confirm(\'', Filter::escapeHtml(I18N::translate('You have selected a GEDCOM file with a different name.  Is this correct?')), '\'); else return true;">';
+	echo '<form name="replaceform" method="post" enctype="multipart/form-data" onsubmit="var newfile = document.replaceform.tree_name.value; newfile = newfile.substr(newfile.lastIndexOf(\'\\\\\')+1); if (newfile!=\'', Filter::escapeHtml($previous_gedcom_filename), '\' && \'\' != \'', Filter::escapeHtml($previous_gedcom_filename), '\') return confirm(\'', Filter::escapeHtml(I18N::translate('You have selected a GEDCOM file with a different name.  Is this correct?')), '\'); else return true;">';
 	echo '<input type="hidden" name="gedcom_id" value="', $tree->getTreeId(), '">';
 	echo Filter::getCsrf();
 	if (Filter::get('action') == 'uploadform') {
@@ -191,8 +191,8 @@ case 'importform':
 			echo WT_DATA_DIR, '<select name="tree_name">';
 			foreach ($files as $gedcom_file) {
 				echo '<option value="', Filter::escapeHtml($gedcom_file), '" ';
-				if ($gedcom_file == $previous_gedcom_filename) {
-					echo '';
+				if ($gedcom_file === $previous_gedcom_filename) {
+					echo ' selected';
 				}
 				echo'>', Filter::escapeHtml($gedcom_file), '</option>';
 			}
