66d53760f8a122156231acc79e16eb75f8af68c6
Greg Roach <fisharebest@gmail.com>
2015-02-16 11:38:31 +0000

    Rewrite relationship calculations.  Fixes #256 and 899785.

--- app/Controller/RelationshipController.php
+++ app/Controller/RelationshipController.php
@@ -16,264 +16,107 @@ namespace Fisharebest\Webtrees;
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Fisharebest\Algorithm\Dijkstra;
+
 /**
- * Class BranchesController - Controller for the branches list
+ * Class RelationshipController - Controller for the relationships calculations
  */
-class BranchesController extends PageController {
-	/** @var string Generate the branches for this surname */
-	private $surname;
-
-	/** @var bool Whether to use Standard phonetic matching */
-	private $soundex_std;
-
-	/** @var bool Whether to use Daitch-Mokotov phonetic matching */
-	private $soundex_dm;
-
-	/** @var Individual[] Everyone with the selected surname */
-	private $individuals = array();
-
-	/** @var Individual[] Ancestors of the root person - for SOSA numbers */
-	private $ancestors = array();
-
+class RelationshipController extends PageController {
 	/**
-	 * Create a branches list controller
-	 */
-	public function __construct() {
-		parent::__construct();
-
-		$this->surname     = Filter::get('surname');
-		$this->soundex_std = Filter::getBool('soundex_std');
-		$this->soundex_dm  = Filter::getBool('soundex_dm');
-
-		if ($this->surname) {
-			$this->setPageTitle(/* I18N: %s is a surname */
-				I18N::translate('Branches of the %s family', Filter::escapeHtml($this->surname)));
-			$this->loadIndividuals();
-			$self = Individual::getInstance(WT_USER_GEDCOM_ID);
-			if ($self) {
-				$this->loadAncestors($self, 1);
-			}
-		} else {
-			$this->setPageTitle(/* I18N: Branches of a family tree */ I18N::translate('Branches'));
-		}
-	}
-
-	/**
-	 * The surname to be used on this page.
+	 * Calculate the shortest paths - or all paths - between two individuals.
 	 *
-	 * @return null|string
-	 */
-	public function getSurname() {
-		return $this->surname;
-	}
-
-	/**
-	 * Should we use Standard phonetic matching
+	 * @param Individual $indi1
+	 * @param Individual $indi2
+	 * @param boolean    $all;
 	 *
-	 * @return boolean
+	 * @return string[][];
 	 */
-	public function getSoundexStd() {
-		return $this->soundex_std;
-	}
+	public function calculateRelationships(Individual $individual1, Individual $individual2, $all) {
+		$rows = Database::prepare(
+			"SELECT l_from, l_to FROM `##link` WHERE l_file = :tree_id AND l_type IN ('FAMS', 'FAMC', 'CHIL', 'HUSB', 'WIFE')"
+		)->execute(array(
+			'tree_id' => $individual1->getTree()->getTreeId()
+		))->fetchAll();
 
-	/**
-	 * Should we use Daitch-Mokotov phonetic matching
-	 *
-	 * @return boolean
-	 */
-	public function getSoundexDm() {
-		return $this->soundex_dm;
-	}
-
-	/**
-	 * Fetch all individuals with a matching surname
-	 */
-	private function loadIndividuals() {
-		$sql =
-			"SELECT DISTINCT i_id AS xref, i_file AS gedcom_id, i_gedcom AS gedcom" .
-			" FROM `##individuals`" .
-			" JOIN `##name` ON (i_id=n_id AND i_file=n_file)" .
-			" WHERE n_file = ?" .
-			" AND n_type != ?" .
-			" AND (n_surn = ? OR n_surname = ?";
-		$args = array(WT_GED_ID, '_MARNM', $this->surname, $this->surname);
-		if ($this->soundex_std) {
-			$sdx = Soundex::russell($this->surname);
-			if ($sdx) {
-				foreach (explode(':', $sdx) as $value) {
-					$sql .= " OR n_soundex_surn_std LIKE CONCAT('%', ?, '%')";
-					$args[] = $value;
-				}
-			}
-		}
-		if ($this->soundex_dm) {
-			$sdx = Soundex::daitchMokotoff($this->surname);
-			if ($sdx) {
-				foreach (explode(':', $sdx) as $value) {
-					$sql .= " OR n_soundex_surn_dm LIKE CONCAT('%', ?, '%')";
-					$args[] = $value;
-				}
-			}
-		}
-		$sql .= ')';
-		$rows              = Database::prepare($sql)->execute($args)->fetchAll();
-		$this->individuals = array();
+		$graph = array();
 		foreach ($rows as $row) {
-			$this->individuals[] = Individual::getInstance($row->xref, $row->gedcom_id, $row->gedcom);
+			$graph[$row->l_from][$row->l_to] = 1;
 		}
-		// Sort by birth date, oldest first
-		usort($this->individuals, __NAMESPACE__ . '\Individual::compareBirthDate');
-	}
 
-	/**
-	 * Load the ancestors of an individual, so we can highlight them in the list
-	 *
-	 * @param Individual $ancestor
-	 * @param integer       $sosa
-	 */
-	private function loadAncestors(Individual $ancestor, $sosa) {
-		if ($ancestor) {
-			$this->ancestors[$sosa] = $ancestor;
-			foreach ($ancestor->getChildFamilies() as $family) {
-				foreach ($family->getSpouses() as $parent) {
-					$this->loadAncestors($parent, $sosa * 2 + ($parent->getSex() == 'F' ? 1 : 0));
-				}
-			}
-		}
-	}
+		$xref1    = $individual1->getXref();
+		$xref2    = $individual2->getXref();
+		$dijkstra = new Dijkstra($graph);
+		$paths    = $dijkstra->shortestPaths($xref1, $xref2);
 
-	/**
-	 * For each individual with no ancestors, list their descendants.
-	 *
-	 * @return string
-	 */
-	public function getPatriarchsHtml() {
-		$html = '';
-		foreach ($this->individuals as $individual) {
-			foreach ($individual->getChildFamilies() as $family) {
-				foreach ($family->getSpouses() as $parent) {
-					if (in_array($parent, $this->individuals, true)) {
-						continue 3;
+		if ($all) {
+			$queue = array();
+			foreach ($paths as $path) {
+				// Insert the paths into the queue, with an exclusion list.
+				$queue[] = array('path' => $path, 'exclude' => array());
+				// While there are un-extended paths
+				while (list(, $next) = each($queue)) {
+					// For each family on the path
+					for ($n = count($next['path']) - 2; $n >= 1; $n-=2) {
+						$exclude = $next['exclude'];
+						$exclude[] = $next['path'][$n];
+						// Add any new path to the queue
+						foreach ($dijkstra->shortestPaths($xref1, $xref2, $exclude) as $new_path) {
+							$queue[] = array('path' => $new_path, 'exclude' => $exclude);
+						}
 					}
 				}
 			}
-			$html .= $this->getDescendantsHtml($individual);
+			// Extract the paths from the queue, removing duplicates.
+			$paths = array();
+			foreach ($queue as $next) {
+				$paths[implode('-', $next['path'])] = $next['path'];
+			}
 		}
 
-		return $html;
+		return $paths;
 	}
 
 	/**
-	 * Generate a recursive list of descendants of an individual.
-	 * If parents are specified, we can also show the pedigree (adopted, etc.).
+	 * Convert a path (list of XREFs) to an "old-style" string of relationships.
 	 *
-	 * @param Individual  $individual
-	 * @param Family|null $parents
+	 * Return an empty array, if privacy rules prevent us viewing any node.
 	 *
-	 * @return string
+	 * @param string $path
+	 *
+	 * @return array();
 	 */
-	private function getDescendantsHtml(Individual $individual, Family $parents = null) {
-		// A person has many names.  Select the one that matches the searched surname
-		$person_name = '';
-		foreach ($individual->getAllNames() as $name) {
-			list($surn1) = explode(",", $name['sort']);
-			if (// one name is a substring of the other
-				stripos($surn1, $this->surname) !== false ||
-				stripos($this->surname, $surn1) !== false ||
-				// one name sounds like the other
-				$this->soundex_std && Soundex::compare(Soundex::russell($surn1), Soundex::russell($this->surname)) ||
-				$this->soundex_dm && Soundex::compare(Soundex::daitchMokotoff($surn1), Soundex::daitchMokotoff($this->surname))
-			) {
-				$person_name = $name['full'];
-				break;
+	public function oldStyleRelationshipPath(array $path) {
+		$spouse_codes  = array('M' => 'hus', 'F' => 'wif', 'U' => 'spo');
+		$parent_codes  = array('M' => 'fat', 'F' => 'mot', 'U' => 'par');
+		$child_codes   = array('M' => 'son', 'F' => 'dau', 'U' => 'chi');
+		$sibling_codes = array('M' => 'bro', 'F' => 'sis', 'U' => 'sib');
+		$relationships = array();
+
+		for ($i = 1; $i < count($path); $i += 2) {
+			$family = Family::getInstance($path[$i]);
+			$prev   = Individual::getInstance($path[$i - 1]);
+			$next   = Individual::getInstance($path[$i + 1]);
+			if (preg_match('/\n\d (HUSB|WIFE|CHIL) @' . $prev->getXref() . '@/', $family->getGedcom(), $match)) {
+				$rel1 = $match[1];
+			} else {
+				return array();
+			}
+			if (preg_match('/\n\d (HUSB|WIFE|CHIL) @' . $next->getXref() . '@/', $family->getGedcom(), $match)) {
+				$rel2 = $match[1];
+			} else {
+				return array();
+			}
+			if (($rel1 === 'HUSB' || $rel1 === 'WIFE') && ($rel2 === 'HUSB' || $rel2 === 'WIFE')) {
+				$relationships[$i] = $spouse_codes[$next->getSex()];
+			} elseif (($rel1 === 'HUSB' || $rel1 === 'WIFE') && $rel2 === 'CHIL') {
+				$relationships[$i] = $child_codes[$next->getSex()];
+			} elseif ($rel1 === 'CHIL' && ($rel2 === 'HUSB' || $rel2 === 'WIFE')) {
+				$relationships[$i] = $parent_codes[$next->getSex()];
+			} elseif ($rel1 === 'CHIL' && $rel2 === 'CHIL') {
+				$relationships[$i] = $sibling_codes[$next->getSex()];
 			}
 		}
 
-		// No matching name?  Typically children with a different surname.  The branch stops here.
-		if (!$person_name) {
-			return '<li title="' . strip_tags($individual->getFullName()) . '">' . $individual->getSexImage() . '…</li>';
-		}
-
-		// Is this individual one of our ancestors?
-		$sosa = array_search($individual, $this->ancestors, true);
-		if ($sosa) {
-			$sosa_class = 'search_hit';
-			$sosa_html  = ' <a class="details1 ' . $individual->getBoxStyle() . '" title="' . I18N::translate('Sosa') . '" href="relationship.php?pid2=' . WT_USER_ROOT_ID . '&amp;pid1=' . $individual->getXref() . '">' . $sosa . '</a>' . self::sosaGeneration($sosa);
-		} else {
-			$sosa_class = '';
-			$sosa_html  = '';
-		}
-
-		// Generate HTML for this individual, and all their descendants
-		$indi_html = $individual->getSexImage() . '<a class="' . $sosa_class . '" href="' . $individual->getHtmlUrl() . '">' . $person_name . '</a> ' . $individual->getLifeSpan() . $sosa_html;
-
-		// If this is not a birth pedigree (e.g. an adoption), highlight it
-		if ($parents) {
-			$pedi = '';
-			foreach ($individual->getFacts('FAMC') as $fact) {
-				if ($fact->getTarget() === $parents) {
-					$pedi = $fact->getAttribute('PEDI');
-					break;
-				}
-			}
-			if ($pedi && $pedi != 'birth') {
-				$indi_html = '<span class="red">' . WT_Gedcom_Code_Pedi::getValue($pedi, $individual) . '</span> ' . $indi_html;
-			}
-		}
-
-		// spouses and children
-		$spouse_families = $individual->getSpouseFamilies();
-		if ($spouse_families) {
-			usort($spouse_families, __NAMESPACE__ . '\Family::compareMarrDate');
-			$fam_html = '';
-			foreach ($spouse_families as $family) {
-				$fam_html .= $indi_html; // Repeat the individual details for each spouse.
-
-				$spouse = $family->getSpouse($individual);
-				if ($spouse) {
-					$sosa = array_search($spouse, $this->ancestors, true);
-					if ($sosa) {
-						$sosa_class = 'search_hit';
-						$sosa_html  = ' <a class="details1 ' . $spouse->getBoxStyle() . '" title="' . I18N::translate('Sosa') . '" href="relationship.php?pid2=' . WT_USER_ROOT_ID . '&amp;pid1=' . $spouse->getXref() . '"> ' . $sosa . ' </a>' . self::sosaGeneration($sosa);
-					} else {
-						$sosa_class = '';
-						$sosa_html  = '';
-					}
-					$marriage_year = $family->getMarriageYear();
-					if ($marriage_year) {
-						$fam_html .= ' <a href="' . $family->getHtmlUrl() . '" title="' . strip_tags($family->getMarriageDate()->display()) . '"><i class="icon-rings"></i>' . $marriage_year . '</a>';
-					} elseif ($family->getFirstFact('MARR')) {
-						$fam_html .= ' <a href="' . $family->getHtmlUrl() . '" title="' . WT_Gedcom_Tag::getLabel('MARR') . '"><i class="icon-rings"></i></a>';
-					} elseif ($family->getFirstFact('_NMR')) {
-						$fam_html .= ' <a href="' . $family->getHtmlUrl() . '" title="' . WT_Gedcom_Tag::getLabel('_NMR') . '"><i class="icon-rings"></i></a>';
-					}
-					$fam_html .= ' ' . $spouse->getSexImage() . '<a class="' . $sosa_class . '" href="' . $spouse->getHtmlUrl() . '">' . $spouse->getFullName() . '</a> ' . $spouse->getLifeSpan() . ' ' . $sosa_html;
-				}
-
-				$fam_html .= '<ol>';
-				foreach ($family->getChildren() as $child) {
-					$fam_html .= $this->getDescendantsHtml($child, $family);
-				}
-				$fam_html .= '</ol>';
-			}
-
-			return '<li>' . $fam_html . '</li>';
-		} else {
-			// No spouses - just show the individual
-			return '<li>' . $indi_html . '</li>';
-		}
-	}
-
-	/**
-	 * Convert a SOSA number into a generation number.  e.g. 8 = great-grandfather = 3 generations
-	 *
-	 * @param integer $sosa
-	 *
-	 * @return string
-	 */
-	private static function sosaGeneration($sosa) {
-		$generation = (int) log($sosa, 2) + 1;
-
-		return '<sup title="' . I18N::translate('Generation') . '">' . $generation . '</sup>';
+		return $relationships;
 	}
 }
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit207af526399948cc5e2e445108bc9b12::getLoader();
+return ComposerAutoloaderInitbc7ef3c4f9a7f3ece04ccd8101dc6f40::getLoader();
