7126668f8fb92b0cac155adc02efde34bbd890ce
Greg Roach <fisharebest@gmail.com>
2015-03-02 14:57:54 +0000

    PSR-1 function names

--- includes/functions/functions_print.php
+++ includes/functions/functions_print.php
@@ -308,7 +308,7 @@ function format_parents_age(Individual $person, Date $birth_date) {
 				switch ($parent->getSex()) {
 				case 'F':
 					// Highlight mothers who die in childbirth or shortly afterwards
-					if ($deatdate->isOK() && $deatdate->MinJD() < $birth_date->minimumJulianDay() + 90) {
+					if ($deatdate->isOK() && $deatdate->maximumJulianDay() < $birth_date->minimumJulianDay() + 90) {
 						$html .= ' <span title="' . WT_Gedcom_Tag::getLabel('_DEAT_PARE', $parent) . '" class="parentdeath">' . $sex . $age . '</span>';
 					} else {
 						$html .= ' <span title="' . I18N::translate('Mother’s age') . '">' . $sex . $age . '</span>';
@@ -316,7 +316,7 @@ function format_parents_age(Individual $person, Date $birth_date) {
 					break;
 				case 'M':
 					// Highlight fathers who die before the birth
-					if ($deatdate->isOK() && $deatdate->MinJD() < $birth_date->minimumJulianDay()) {
+					if ($deatdate->isOK() && $deatdate->maximumJulianDay() < $birth_date->minimumJulianDay()) {
 						$html .= ' <span title="' . WT_Gedcom_Tag::getLabel('_DEAT_PARE', $parent) . '" class="parentdeath">' . $sex . $age . '</span>';
 					} else {
 						$html .= ' <span title="' . I18N::translate('Father’s age') . '">' . $sex . $age . '</span>';
