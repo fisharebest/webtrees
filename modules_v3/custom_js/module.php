791accbd8ef6a89dd0fa6ddac32fe0c188e27fac
JustCarmen <carmen@justcarmen.nl>
2015-03-30 06:17:49 +0200

    Module API changed.

--- modules_v3/custom_js/module.php
+++ modules_v3/custom_js/module.php
@@ -17,7 +17,7 @@ namespace Fisharebest\Webtrees;
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-class CustomJavaScriptModule extends Module implements ModuleConfigInterface, ModuleMenuInterface {
+class CustomJavaScriptModule extends AbstractModule implements ModuleConfigInterface, ModuleMenuInterface {
 	
 	public function __construct() {
 		parent::__construct('custom_js');		
