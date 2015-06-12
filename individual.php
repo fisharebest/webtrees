903bf7b4979eef38002f19e7b6a32f4c827521a2
Benjamin Marwell <bmarwell@gmail.com>
2015-06-12 22:48:41 +0200

    Revert "Add 403 if person exists but cannot be seen by this viewer. Adjust error messages accordingly. fixes #607."
    This reverts commit 473a64ea10df9f84b982b5935749446532d32ad7.

--- individual.php
+++ individual.php
@@ -80,23 +80,17 @@ if ($controller->record && $controller->record->canShow()) {
 				'</p>';
 		}
 	}
-} else if ($controller->record && $controller->record->canShowName()) {
+} elseif ($controller->record && $controller->record->canShowName()) {
 	// Just show the name.
 	$controller->pageHeader();
 	echo '<h2>', $controller->record->getFullName(), '</h2>';
 	echo '<p class="ui-state-highlight">', I18N::translate('The details of this individual are private.'), '</p>';
 
-	return; 
-} else if ($controller->record && !($controller->record->canShowName())) {
-	http_response_code(403);
-	$controller->pageHeader();
-	echo '<p class="ui-state-error">', I18N::translate('You do not have permission to view this individual.'), '</p>';
-	
 	return;
 } else {
 	http_response_code(404);
 	$controller->pageHeader();
-	echo '<p class="ui-state-error">', I18N::translate('This individual does not exist.'), '</p>';
+	echo '<p class="ui-state-error">', I18N::translate('This individual does not exist or you do not have permission to view it.'), '</p>';
 
 	return;
 }
