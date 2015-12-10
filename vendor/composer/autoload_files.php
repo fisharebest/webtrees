d5e26e1ec9d0863d55dd74f8a1c48aeb30f5d03d
Greg Roach <fisharebest@gmail.com>
2015-12-10 22:45:53 +0000

    Update Zend Framework from 1.12.16 to 1.12.17

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,7 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'fd8a8d96137652b1c2829f2141bcdf77' => $vendorDir . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
-    'e23f4cc203305211f73ba674292b6745' => $vendorDir . '/fisharebest/ext-calendar/src/shims.php',
-    '05669301f06448944ce5eb260126f84e' => $vendorDir . '/ircmaxell/password-compat/lib/password.php',
+    '2cffec82183ee1cea088009cef9a6fc3' => $vendorDir . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
+    'b3134b0e457069f7362d2c9cf91628c7' => $vendorDir . '/fisharebest/ext-calendar/src/shims.php',
+    'e40631d46120a9c38ea139981f8dab26' => $vendorDir . '/ircmaxell/password-compat/lib/password.php',
 );
