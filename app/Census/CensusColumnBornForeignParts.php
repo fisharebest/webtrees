b164ca57552a25e83f2c8aa654e2e6557c1bd0f9
Greg Roach <fisharebest@gmail.com>
2016-02-04 22:12:37 +0000

    Fix #823 - census birthplace

--- app/Census/CensusColumnBornForeignParts.php
+++ app/Census/CensusColumnBornForeignParts.php
@@ -42,7 +42,7 @@ class CensusColumnBornForeignParts extends AbstractCensusColumn implements Censu
 			$census_place = 'England';
 		}
 
-		if ($birth_place === $census_place) {
+		if ($birth_place === $census_place || $birth_place === '') {
 			return '';
 		} elseif ($birth_place === 'England' || $birth_place === 'Scotland' || $birth_place === 'Ireland') {
 			return substr($birth_place, 0, 1);
