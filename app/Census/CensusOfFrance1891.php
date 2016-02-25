97be64a7643f764f5c71b5ef228f92f6f45e9ebe
mikejpr <michael_rapp@gmx.net>
2016-02-25 22:33:54 +0100

    Update translation french

--- app/Census/CensusOfFrance1891.php
+++ app/Census/CensusOfFrance1891.php
@@ -35,11 +35,11 @@ class CensusOfFrance1891 extends CensusOfFrance implements CensusInterface {
 	 */
 	public function columns() {
 		return array(
-			new CensusColumnSurname($this, 'XXXX', 'XXXX'),
-			new CensusColumnGivenNames($this, 'XXXX', 'XXXX'),
-			new CensusColumnOccupation($this, 'XXXX', 'XXXX'),
-			new CensusColumnRelationToHead($this, 'XXXX', 'XXXX'),
-			new CensusColumnNationality($this, 'XXXX', 'XXXX'),
+			new CensusColumnSurname($this, 'Nom', 'Nom de famille'),
+			new CensusColumnGivenNames($this, 'Prénom', 'Prénom'),
+			new CensusColumnOccupation($this, 'Profession', 'Profession'),
+			new CensusColumnRelationToHead($this, 'Position', 'Position dans le ménage'),
+			new CensusColumnNationality($this, 'Nationalité', 'Nationalité'),	
 		);
 	}
 }
