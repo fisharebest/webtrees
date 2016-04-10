2f0e6ce965fb0913e7870089cd0c652928c73f3d
ric2016 <ric@richard-cissee.de>
2016-04-10 09:10:45 +0200

    Migration
    simplified

--- app/Schema/Migration34.php
+++ app/Schema/Migration34.php
@@ -23,39 +23,11 @@ use Fisharebest\Webtrees\I18N;
  * Upgrade the database schema from version 33 to version 34.
  */
 class Migration34 implements MigrationInterface {
+	
 	/**
-	 * Upgrade to to the next version (Charts as modules, should be enabled by default)
+	 * Upgrade to to the next version (for 'Charts as modules', the respective modules should be enabled by default)
 	 */
 	public function upgrade() {
-		$names = array(
-			"ancestors_chart", 
-			"compact_tree_chart", 
-			"descendancy_chart", 
-			"family_book_chart", 
-			"fan_chart", 
-			"hourglass_chart", 
-			"lifespans_chart", 
-			"pedigree_chart", 
-			"relationships_chart", 
-			"statistics_chart", 
-			"timeline_chart");
-		
-		$func = function($value) {
-    	return " ('".$value."', 'enabled', NULL, NULL, NULL)";
-		};
-		$values = array_map($func, $names);
-			
-		Database::exec(
-			"INSERT IGNORE INTO `##module` (module_name, status, tab_order, menu_order, sidebar_order) VALUES" .
-			implode(',',$values)
-		);
-		
-		foreach ($names as $name) {
-			Database::prepare(
-				"INSERT IGNORE INTO `##module_privacy` (module_name, gedcom_id, component, access_level)" .
-				" SELECT ?, gedcom_id, 'chart', 2" .
-				" FROM `##gedcom`"
-			)->execute(array($name));
-		}
+		Module::getInstalledModules('enabled');		
 	}
 }
