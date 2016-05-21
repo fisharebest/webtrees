fisharebest/webtrees

commit 1595d1801af17ae299a9629523c1be5e5f2fd0e4

Can only create new objects

admin_trees_manage.php
@@ -617,8 +617,8 @@ if (count($all_trees) >= $multiple_tree_threshold) {
 		<div class="panel-heading">
 			<h2 class="panel-title">
 				<i class="fa fa-fw fa-plus"></i>
-				<a data-toggle="collapse" data-parent="#accordion" href="#create-a-new-family-tree">
-					<?php echo I18N::translate('Create a new family tree'); ?>
+				<a data-toggle="collapse" data-parent="#accordion" href="#create-a-family-tree">
+					<?php echo I18N::translate('Create a family tree'); ?>
 				</a>
 			</h2>
 		</div>

commit 63197851b19841830a54da4d471d8bb14c1dfb2b

Fix #939 - regression caused by 1595d1801af17ae299a9629523c1be5e5f2fd0e4

admin_trees_manage.php
@@ -625,7 +625,7 @@ if (count($all_trees) >= $multiple_tree_threshold) {
 				</a>
 			</h2>
 		</div>
-		<div id="create-a-new-family-tree" class="panel-collapse collapse<?php echo Tree::getAll() ? '' : ' in'; ?>">
+		<div id="create-a-family-tree" class="panel-collapse collapse<?php echo Tree::getAll() ? '' : ' in'; ?>">
 			<div class="panel-body">
 				<form role="form" class="form-horizontal" method="post">
 					<?php echo Filter::getCsrf(); ?>