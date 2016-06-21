c2edf43a772675eca228773ee5c2c464b4e87289
Greg Roach <fisharebest@gmail.com>
2016-06-21 11:18:54 +0100

    Fix #968 - wrong menu name

--- login.php
+++ login.php
@@ -256,7 +256,7 @@ case 'requestpw':
 			I18N::translate('A new password has been requested for your username.') . Mail::EOL . Mail::EOL .
 			I18N::translate('Username') . ": " . Filter::escapeHtml($user->getUserName()) . Mail::EOL .
 			I18N::translate('Password') . ": " . $user_new_pw . Mail::EOL . Mail::EOL .
-			I18N::translate('After you have signed in, select the “My account” link under the “My page” menu and fill in the password fields to change your password.') . Mail::EOL . Mail::EOL .
+			I18N::translate('After you have signed in, select the “My account” link under the “My pages” menu and fill in the password fields to change your password.') . Mail::EOL . Mail::EOL .
 			'<a href="' . WT_BASE_URL . 'login.php?ged=' . $WT_TREE->getNameUrl() . '">' . WT_BASE_URL . 'login.php?ged=' . $WT_TREE->getNameUrl() . '</a>'
 		);
 
