cc2336a73419395e66ac0115b0e832addc6faa7b
Greg Roach <fisharebest@gmail.com>
2016-06-29 14:42:13 +0100

    Typo

--- app/GedcomCode/GedcomCodeTemp.php
+++ app/GedcomCode/GedcomCodeTemp.php
@@ -120,7 +120,7 @@ class GedcomCodeTemp {
 		case 'BROUG':
 			return /* I18N: Location of an LDS church temple */ I18N::translate('Baton Rouge, Louisiana, United States');
 		case 'CALGA':
-			return /* I18N: Location of an LDS church temple */ I18N::translate('Calgary, Alberata, Canada');
+			return /* I18N: Location of an LDS church temple */ I18N::translate('Calgary, Alberta, Canada');
 		case 'CAMPI':
 			return /* I18N: Location of an LDS church temple */ I18N::translate('Campinas, Brazil');
 		case 'CARAC':
