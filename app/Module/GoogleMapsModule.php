41ed1f46244abe691ec0b5a220904c5712261577
Greg Roach <fisharebest@gmail.com>
2017-06-24 18:38:32 +0100

    Fix #1099 - error if no file selected

--- app/Module/GoogleMapsModule.php
+++ app/Module/GoogleMapsModule.php
@@ -2265,6 +2265,9 @@ class GoogleMapsModule extends AbstractModule implements ModuleConfigInterface, 
 			$lines = file($_FILES['placesfile']['tmp_name']);
 		} elseif (!empty($_REQUEST['localfile'])) {
 			$lines = file(WT_MODULES_DIR . 'googlemap/extra' . $_REQUEST['localfile']);
+		} else {
+			FlashMessages::addMessage(I18N::translate('No file was received. Please try again.'), 'danger');
+			$lines = [];
 		}
 		// Strip BYTE-ORDER-MARK, if present
 		if (!empty($lines[0]) && substr($lines[0], 0, 3) === WT_UTF8_BOM) {
