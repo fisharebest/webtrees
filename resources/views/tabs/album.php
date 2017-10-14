a9862c2be07e49a206e0c6894a981a414ad4051f
Greg Roach <fisharebest@gmail.com>
2017-10-14 21:27:01 +0100

    Add links in album tab, remove old styling

--- resources/views/tabs/album.php
+++ resources/views/tabs/album.php
@@ -1,9 +1,13 @@
+<?php use Fisharebest\Webtrees\Html; ?>
+
 <div class="wt-album-tab row mt-4">
 	<?php foreach ($media_list as $media): ?>
 		<figure class="figure text-center col-sm=6 col-md-4 col-lg-3 col-xl-2 wt-album-tab-figure">
 			<?= $media->displayImage(100, 100, 'contain', ['class' => 'img-thumbnail wt-album-tab-image']) ?>
 			<figcaption class="figure-caption wt-album-tab-caption">
-				<?= $media->getFullName() ?>
+				<a href="<?= Html::escape($media->getRawUrl()) ?>">
+					<?= $media->getFullName() ?>
+				</a>
 			</figcaption>
 		</figure>
 	<?php endforeach ?>
