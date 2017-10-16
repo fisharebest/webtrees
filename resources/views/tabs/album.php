dae2f6291fd6eb3a60052c0a9d31ecb48dd711d0
JustCarmen <carmen@justcarmen.nl>
2017-10-16 19:52:45 +0200

    Typo in classname

--- resources/views/tabs/album.php
+++ resources/views/tabs/album.php
@@ -2,7 +2,7 @@
 
 <div class="wt-album-tab row py-4">
 	<?php foreach ($media_list as $media): ?>
-		<figure class="figure text-center col-sm=6 col-md-4 col-lg-3 col-xl-2 wt-album-tab-figure">
+		<figure class="figure text-center col-sm-6 col-md-4 col-lg-3 col-xl-2 wt-album-tab-figure">
 			<?= $media->displayImage(100, 100, 'contain', ['class' => 'img-thumbnail wt-album-tab-image']) ?>
 			<figcaption class="figure-caption wt-album-tab-caption">
 				<a href="<?= Html::escape($media->getRawUrl()) ?>">
