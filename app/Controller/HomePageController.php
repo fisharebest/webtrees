918ee52f68d90e36d6ceb8eefe481cc1969e8ab3
Greg Roach <fisharebest@gmail.com>
2017-11-14 17:13:12 +0000

    Working on front controller

--- app/Controller/HomePageController.php
+++ app/Controller/HomePageController.php
@@ -16,10 +16,13 @@
 namespace Fisharebest\Webtrees\Controller;
 
 use Fisharebest\Webtrees\Auth;
-use Fisharebest\Webtrees\Functions\FunctionsDb;
+use Fisharebest\Webtrees\Database;
 use Fisharebest\Webtrees\Html;
+use Fisharebest\Webtrees\I18N;
 use Fisharebest\Webtrees\Module;
-use Fisharebest\Webtrees\View;
+use Fisharebest\Webtrees\Module\ModuleBlockInterface;
+use Fisharebest\Webtrees\Tree;
+use Fisharebest\Webtrees\User;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -43,28 +46,14 @@ class HomePageController extends PageController {
 	 * Show a tree's home page.
 	 */
 	public function treePage() {
-		$blocks = FunctionsDb::getTreeBlocks($this->tree()->getTreeId());
-		$active_blocks = Module::getActiveBlocks($this->tree());
-
-		$main_blocks = array_filter($blocks['main'], function(string $block_name) use ($active_blocks) {
-			return array_key_exists($block_name, $active_blocks);
-		});
-		$side_blocks = array_filter($blocks['side'], function(string $block_name) use ($active_blocks) {
-			return array_key_exists($block_name, $active_blocks);
-		});
-
-		$main_blocks = array_map(function($module_name) use ($active_blocks) {
-			return $active_blocks[$module_name];
-		}, $main_blocks);
-
-		$side_blocks = array_map(function($module_name) use ($active_blocks) {
-			return $active_blocks[$module_name];
-		}, $side_blocks);
+		$tree        = $this->tree();
+		$main_blocks = $this->getTreeBlocks($tree, 'main');
+		$side_blocks = $this->getTreeBlocks($tree, 'side');
 
 		return $this->viewResponse('home-page', [
-			'title'       => Html::escape($this->tree()->getTitle()),
+			'title'       => Html::escape($tree->getTitle()),
 			'main_blocks' => $main_blocks,
-			'side_blocks' => $side_blocks
+			'side_blocks' => $side_blocks,
 		]);
 	}
 
@@ -94,27 +83,14 @@ class HomePageController extends PageController {
 	 * Show a users's home page.
 	 */
 	public function userPage() {
-		$blocks = FunctionsDb::getUserBlocks(Auth::id());
-		$active_blocks = Module::getActiveBlocks($this->tree());
+		$tree        = $this->tree();
+		$main_blocks = $this->getUserBlocks($tree, Auth::user(), 'main');
+		$side_blocks = $this->getUserBlocks($tree, Auth::user(), 'side');
 
-		$main_blocks = array_filter($blocks['main'], function(string $block_name) use ($active_blocks) {
-			return array_key_exists($block_name, $active_blocks);
-		});
-		$side_blocks = array_filter($blocks['side'], function(string $block_name) use ($active_blocks) {
-			return array_key_exists($block_name, $active_blocks);
-		});
-
-		$main_blocks = array_map(function($module_name) use ($active_blocks) {
-			return $active_blocks[$module_name];
-		}, $main_blocks);
-
-		$side_blocks = array_map(function($module_name) use ($active_blocks) {
-			return $active_blocks[$module_name];
-		}, $side_blocks);
-
-		$content = View::make('my-page', [
+		return $this->viewResponse('my-page', [
+			'title'       => I18N::translate('My page'),
 			'main_blocks' => $main_blocks,
-			'side_blocks' => $side_blocks
+			'side_blocks' => $side_blocks,
 		]);
 	}
 
@@ -136,5 +112,80 @@ class HomePageController extends PageController {
 	 */
 	public function userPageUpdate(Request $request): RedirectResponse {
 
+	}
+
+	/**
+	 * Get the blocks for a specified user.
+	 *
+	 * @param Tree   $tree
+	 * @param string $location "main" or "side"
+	 *
+	 * @return ModuleBlockInterface[]
+	 */
+	private static function getTreeBlocks(Tree $tree, string $location): array {
+		$active_blocks = Module::getActiveBlocks($tree);
+
+		if ($tree->getTreeId() < 0) {
+			$access_level = Auth::PRIV_NONE;
+		} else {
+			$access_level = Auth::accessLevel($tree);
+		}
+
+		$rows = Database::prepare(
+			"SELECT SQL_CACHE block_id, module_name" .
+			" FROM  `##block`" .
+			" JOIN  `##module` USING (module_name)" .
+			" JOIN  `##module_privacy` USING (module_name, gedcom_id)" .
+			" WHERE gedcom_id = :tree_id" .
+			" AND   status='enabled'" .
+			" AND   location = :location" .
+			" AND   access_level >= :access_level" .
+			" ORDER BY location, block_order"
+		)->execute([
+			'access_level' => $access_level,
+			'location'     => $location,
+			'tree_id'      => $tree->getTreeId(),
+		])->fetchAssoc();
+
+		// Modules may have been deleted on disk, or may no longer provide "block" functions.
+		return array_filter(array_map(function(string $module_name) use ($active_blocks) {
+			return $active_blocks[$module_name] ?? false;
+		}, $rows));
+	}
+
+	/**
+	 * Get the blocks for a specified user.
+	 *
+	 * @param Tree   $tree
+	 * @param User   $user
+	 * @param string $location "main" or "side"
+	 *
+	 * @return ModuleBlockInterface[]
+	 */
+	private static function getUserBlocks(Tree $tree, User $user, string $location): array {
+		$active_blocks = Module::getActiveBlocks($tree);
+
+		$rows = Database::prepare(
+			"SELECT SQL_CACHE block_id, module_name" .
+			" FROM  `##block`" .
+			" JOIN  `##module` USING (module_name)" .
+			" JOIN  `##module_privacy` USING (module_name)" .
+			" WHERE user_id = :user_id" .
+			" AND   status = 'enabled'" .
+			" AND   location = :location" .
+			" AND   `##module_privacy`.gedcom_id = :tree_id" .
+			" AND   access_level >= :access_level" .
+			" ORDER BY block_order"
+		)->execute([
+			'access_level' => Auth::accessLevel($tree),
+			'location'     => $location,
+			'user_id'      => $user->getUserId(),
+			'tree_id'      => $tree->getTreeId(),
+		])->fetchAssoc();
+
+		// Modules may have been deleted on disk, or may no longer provide "block" functions.
+		return array_filter(array_map(function(string $module_name) use ($active_blocks) {
+			return $active_blocks[$module_name] ?? false;
+		}, $rows));
 	}
 }
---
+++ resources/views/my-page.php
@@ -0,0 +1,41 @@
+<?php use Fisharebest\Webtrees\Html; ?>
+
+<h2 class="text-center">
+	<?= $title ?>
+</h2>
+
+<div class="row">
+	<?php if (empty($main_blocks) || empty($side_blocks)): ?>
+		<div class="col-sm-12 wt-main-blocks">
+			<?php foreach ($main_blocks + $side_blocks as $block_id => $block): ?>
+				<?php if ($block->loadAjax()): ?>
+					<div class="wt-ajax-load" data-ajax-url="<?= Html::escape(Html::url('index.php', ['ctype' => 'gedcom', 'block_id' => $block_id, 'ajax' => 1])) ?>">
+					</div>
+				<?php else: ?>
+					<?= $block->getBlock($block_id) ?>
+				<?php endif ?>
+			<?php endforeach ?>
+		</div>
+	<?php else: ?>
+		<div class="col-sm-8 wt-main-blocks">
+			<?php foreach ($main_blocks as $block_id => $block): ?>
+				<?php if ($block->loadAjax()): ?>
+					<div class="wt-ajax-load" data-ajax-url="<?= Html::escape(Html::url('index.php', ['ctype' => 'gedcom', 'block_id' => $block_id, 'ajax' => 1])) ?>">
+					</div>
+				<?php else: ?>
+					<?= $block->getBlock($block_id) ?>
+				<?php endif ?>
+			<?php endforeach ?>
+		</div>
+		<div class="col-sm-4 wt-side-blocks">
+			<?php foreach ($side_blocks as $block_id => $block): ?>
+				<?php if ($block->loadAjax()): ?>
+					<div class="wt-ajax-load" data-ajax-url="<?= Html::escape(Html::url('index.php', ['ctype' => 'gedcom', 'block_id' => $block_id, 'ajax' => 1])) ?>">
+					</div>
+				<?php else: ?>
+					<?= $block->getBlock($block_id) ?>
+				<?php endif ?>
+			<?php endforeach ?>
+		</div>
+	<?php endif ?>
+</div>
