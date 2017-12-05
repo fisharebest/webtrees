46ff6bc2c966ffe53f0ef021e6f4c337d7be544b
Greg Roach <fisharebest@gmail.com>
2017-12-05 18:04:16 +0000

    Media object / media file

--- resources/views/tabs/album.php
+++ resources/views/tabs/album.php
@@ -1,7 +1,11 @@
 <div class="wt-album-tab row py-4">
 	<?php foreach ($media_list as $media): ?>
 		<figure class="figure text-center col-sm-6 col-md-4 col-lg-3 col-xl-2 wt-album-tab-figure">
-			<?= $media->displayImage(100, 100, 'contain', ['class' => 'img-thumbnail wt-album-tab-image']) ?>
+			<?php foreach ($media->mediaFiles() as $media_file): ?>
+				<?php if ($media_file->isImage()): ?>
+					<?= $media_file->displayImage(100, 100, 'contain', ['class' => 'img-thumbnail wt-album-tab-image']) ?>
+				<?php endif ?>
+			<?php endforeach ?>
 			<figcaption class="figure-caption wt-album-tab-caption">
 				<a href="<?= e($media->getRawUrl()) ?>">
 					<?= $media->getFullName() ?>
