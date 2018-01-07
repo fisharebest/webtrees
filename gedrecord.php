83d3bf722a2e615dcb84f23b44b2219c6a77f0d5
Greg Roach <fisharebest@gmail.com>
2018-01-07 13:54:35 +0000

    Use view for gedcom-record page

--- gedrecord.php
+++ gedrecord.php
@@ -78,77 +78,12 @@ $sources       = $controller->record->linkedSources('SUBM');
 $notes         = $controller->record->linkedNotes('SUBM');
 $facts         = $controller->record->getFacts();
 
-?>
-<h2 class="wt-page-title">
-	<?= $controller->record->getFullName() ?>
-</h2>
-
-<div class="wt-page-content">
-	<ul class="nav nav-tabs" role="tablist">
-		<li class="nav-item">
-				<a class="nav-link active" data-toggle="tab" role="tab" href="#details">
-				<?= I18N::translate('Details') ?>
-			</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link<?= empty($individuals) ? ' text-muted' : '' ?>" data-toggle="tab" role="tab" href="#individuals">
-				<?= I18N::translate('Individuals') ?>
-				<?= Bootstrap4::badgeCount($individuals) ?>
-			</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link<?= empty($families) ? ' text-muted' : '' ?>" data-toggle="tab" role="tab" href="#families">
-				<?= I18N::translate('Families') ?>
-				<?= Bootstrap4::badgeCount($families) ?>
-			</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link<?= empty($media_objects) ? ' text-muted' : '' ?>" data-toggle="tab" role="tab" href="#media">
-				<?= I18N::translate('Media objects') ?>
-				<?= Bootstrap4::badgeCount($media_objects) ?>
-			</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link<?= empty($sources) ? ' text-muted' : '' ?>" data-toggle="tab" role="tab" href="#sources">
-				<?= I18N::translate('Sources') ?>
-				<?= Bootstrap4::badgeCount($sources) ?>
-			</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link<?= empty($notes) ? ' text-muted' : '' ?>" data-toggle="tab" role="tab" href="#notes">
-				<?= I18N::translate('Notes') ?>
-				<?= Bootstrap4::badgeCount($notes) ?>
-			</a>
-		</li>
-	</ul>
-
-	<div class="tab-content">
-		<div class="tab-pane active fade show" role="tabpanel" id="details">
-			<table class="table wt-facts-table">
-				<?php foreach ($facts as $fact): ?>
-					<?php FunctionsPrintFacts::printFact($fact, $controller->record) ?>
-				<?php endforeach ?>
-			</table>
-		</div>
-
-		<div class="tab-pane fade" role="tabpanel" id="individuals">
-			<?= FunctionsPrintLists::individualTable($individuals) ?>
-		</div>
-
-		<div class="tab-pane fade" role="tabpanel" id="families">
-			<?= FunctionsPrintLists::familyTable($families) ?>
-		</div>
-
-		<div class="tab-pane fade" role="tabpanel" id="media">
-			<?= FunctionsPrintLists::mediaTable($media_objects) ?>
-		</div>
-
-		<div class="tab-pane fade" role="tabpanel" id="sources">
-			<?= FunctionsPrintLists::sourceTable($sources) ?>
-		</div>
-
-		<div class="tab-pane fade" role="tabpanel" id="notes">
-			<?= FunctionsPrintLists::noteTable($notes) ?>
-		</div>
-	</div>
-</div>
+echo View::make('gedcom-record-page', [
+	'facts'         => $facts,
+	'families'      => $families,
+	'individuals'   => $individuals,
+	'media_objects' => $media_objects,
+	'notes'         => $notes,
+	'record'        => $controller->record,
+	'sources'       => $sources,
+]);
