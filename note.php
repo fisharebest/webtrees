d6b3677be38ceba2ff72e03efc8443158b20c9ce
Greg Roach <fisharebest@gmail.com>
2018-01-07 12:49:04 +0000

    Use view for note page

--- note.php
+++ note.php
@@ -74,94 +74,13 @@ foreach ($controller->record->getFacts() as $fact) {
 
 $text = Filter::formatText($controller->record->getNote(), $controller->record->getTree());
 
-?>
-<h2 class="wt-page-title">
-	<?= $controller->record->getFullName() ?>
-</h2>
-
-<div class="wt-page-content">
-	<ul class="nav nav-tabs" role="tablist">
-		<li class="nav-item">
-				<a class="nav-link active" data-toggle="tab" role="tab" href="#details">
-				<?= I18N::translate('Details') ?>
-			</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link<?= empty($individuals) ? ' text-muted' : '' ?>" data-toggle="tab" role="tab" href="#individuals">
-				<?= I18N::translate('Individuals') ?>
-				<?= Bootstrap4::badgeCount($individuals) ?>
-			</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link<?= empty($families) ? ' text-muted' : '' ?>" data-toggle="tab" role="tab" href="#families">
-				<?= I18N::translate('Families') ?>
-				<?= Bootstrap4::badgeCount($families) ?>
-			</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link<?= empty($media_objects) ? ' text-muted' : '' ?>" data-toggle="tab" role="tab" href="#media">
-				<?= I18N::translate('Media objects') ?>
-				<?= Bootstrap4::badgeCount($media_objects) ?>
-			</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link<?= empty($sources) ? ' text-muted' : '' ?>" data-toggle="tab" role="tab" href="#sources">
-				<?= I18N::translate('Sources') ?>
-				<?= Bootstrap4::badgeCount($sources) ?>
-			</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link<?= empty($notes) ? ' text-muted' : '' ?>" data-toggle="tab" role="tab" href="#notes">
-				<?= I18N::translate('Notes') ?>
-				<?= Bootstrap4::badgeCount($notes) ?>
-			</a>
-		</li>
-	</ul>
-
-	<div class="tab-content">
-		<div class="tab-pane active fade show" role="tabpanel" id="details">
-			<table class="table wt-facts-table">
-				<tr>
-					<td class="descriptionbox">
-						<?= I18N::translate('Shared note') ?>
-						<?php if (Auth::isEditor($controller->record->getTree())) { ?>
-							<div class="editfacts">
-								<?= FontAwesome::linkIcon('edit', I18N::translate('Edit'), ['class' => 'btn btn-link', 'href' => 'edit_interface.php?action=editnote&ged=' . $controller->record->getTree()->getNameUrl() . '&xref=' . $controller->record->getXref()]) ?>
-							</div>
-						<?php } ?>
-					</td>
-					<td class="optionbox wrap width80"><?= $text ?></td>
-				</tr>
-				<?php
-				foreach ($facts as $fact) {
-					FunctionsPrintFacts::printFact($fact, $controller->record);
-				}
-
-				if ($controller->record->canEdit()) {
-				FunctionsPrint::printAddNewFact($controller->record->getXref(), $facts, 'NOTE');
-				}
-				?>
-			</table>
-		</div>
-
-		<div class="tab-pane fade" role="tabpanel" id="individuals">
-			<?= FunctionsPrintLists::individualTable($individuals) ?>
-		</div>
-
-		<div class="tab-pane fade" role="tabpanel" id="families">
-			<?= FunctionsPrintLists::familyTable($families) ?>
-		</div>
-
-		<div class="tab-pane fade" role="tabpanel" id="media">
-			<?= FunctionsPrintLists::mediaTable($media_objects) ?>
-		</div>
-
-		<div class="tab-pane fade" role="tabpanel" id="sources">
-			<?= FunctionsPrintLists::sourceTable($sources) ?>
-		</div>
-
-		<div class="tab-pane fade" role="tabpanel" id="notes">
-			<?= FunctionsPrintLists::noteTable($notes) ?>
-		</div>
-	</div>
-</div>
+echo View::make('note-page', [
+	'note'          => $controller->record,
+	'families'      => $families,
+	'individuals'   => $individuals,
+	'sources'       => $sources,
+	'media_objects' => $media_objects,
+	'notes'         => $notes,
+	'facts'         => $facts,
+	'text'          => $text
+]);
