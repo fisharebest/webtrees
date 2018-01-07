305b26ebcfb24f30824626e05d97329a578f6fad
Greg Roach <fisharebest@gmail.com>
2018-01-07 12:49:04 +0000

    Use view for note page

---
+++ resources/views/note-page.php
@@ -0,0 +1,96 @@
+<?php use Fisharebest\Webtrees\Auth; ?>
+<?php use Fisharebest\Webtrees\Bootstrap4; ?>
+<?php use Fisharebest\Webtrees\FontAwesome; ?>
+<?php use Fisharebest\Webtrees\Functions\FunctionsPrint; ?>
+<?php use Fisharebest\Webtrees\Functions\FunctionsPrintFacts; ?>
+<?php use Fisharebest\Webtrees\Functions\FunctionsPrintLists; ?>
+<?php use Fisharebest\Webtrees\I18N; ?>
+
+<h2 class="wt-page-title">
+	<?= $note->getFullName() ?>
+</h2>
+
+<div class="wt-page-content">
+	<ul class="nav nav-tabs" role="tablist">
+		<li class="nav-item">
+			<a class="nav-link active" data-toggle="tab" role="tab" href="#details">
+				<?= I18N::translate('Details') ?>
+			</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link<?= empty($individuals) ? ' text-muted' : '' ?>" data-toggle="tab" role="tab" href="#individuals">
+				<?= I18N::translate('Individuals') ?>
+				<?= Bootstrap4::badgeCount($individuals) ?>
+			</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link<?= empty($families) ? ' text-muted' : '' ?>" data-toggle="tab" role="tab" href="#families">
+				<?= I18N::translate('Families') ?>
+				<?= Bootstrap4::badgeCount($families) ?>
+			</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link<?= empty($media_objects) ? ' text-muted' : '' ?>" data-toggle="tab" role="tab" href="#media">
+				<?= I18N::translate('Media objects') ?>
+				<?= Bootstrap4::badgeCount($media_objects) ?>
+			</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link<?= empty($sources) ? ' text-muted' : '' ?>" data-toggle="tab" role="tab" href="#sources">
+				<?= I18N::translate('Sources') ?>
+				<?= Bootstrap4::badgeCount($sources) ?>
+			</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link<?= empty($notes) ? ' text-muted' : '' ?>" data-toggle="tab" role="tab" href="#notes">
+				<?= I18N::translate('Notes') ?>
+				<?= Bootstrap4::badgeCount($notes) ?>
+			</a>
+		</li>
+	</ul>
+
+	<div class="tab-content">
+		<div class="tab-pane active fade show" role="tabpanel" id="details">
+			<table class="table wt-facts-table">
+				<tr>
+					<th scope="row">
+						<?= I18N::translate('Shared note') ?>
+						<?php if (Auth::isEditor($note->getTree())): ?>
+							<div class="editfacts">
+								<?= FontAwesome::linkIcon('edit', I18N::translate('Edit'), ['class' => 'btn btn-link', 'href' => 'edit_interface.php?action=editnote&ged=' . $note->getTree()->getNameUrl() . '&xref=' . $note->getXref()]) ?>
+							</div>
+						<?php endif ?>
+					</th>
+					<td><?= $text ?></td>
+				</tr>
+				<?php foreach ($facts as $fact): ?>
+					<?php FunctionsPrintFacts::printFact($fact, $note) ?>
+				<?php endforeach ?>
+
+				<?php if ($note->canEdit()): ?>
+					<?php FunctionsPrint::printAddNewFact($note->getXref(), $facts, 'NOTE') ?>
+				<?php endif ?>
+			</table>
+		</div>
+
+		<div class="tab-pane fade" role="tabpanel" id="individuals">
+			<?= FunctionsPrintLists::individualTable($individuals) ?>
+		</div>
+
+		<div class="tab-pane fade" role="tabpanel" id="families">
+			<?= FunctionsPrintLists::familyTable($families) ?>
+		</div>
+
+		<div class="tab-pane fade" role="tabpanel" id="media">
+			<?= FunctionsPrintLists::mediaTable($media_objects) ?>
+		</div>
+
+		<div class="tab-pane fade" role="tabpanel" id="sources">
+			<?= FunctionsPrintLists::sourceTable($sources) ?>
+		</div>
+
+		<div class="tab-pane fade" role="tabpanel" id="notes">
+			<?= FunctionsPrintLists::noteTable($notes) ?>
+		</div>
+	</div>
+</div>
