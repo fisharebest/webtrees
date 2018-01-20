607bcdb6231ed8885186ecf166a26b1af6869504
Greg Roach <fisharebest@gmail.com>
2018-01-20 15:40:31 +0000

    Update ramsey/uuid from 3.7.2 to 3.7.3

--- vendor/ramsey/uuid/src/Provider/Node/SystemNodeProvider.php
+++ vendor/ramsey/uuid/src/Provider/Node/SystemNodeProvider.php
@@ -39,7 +39,7 @@ class SystemNodeProvider implements NodeProviderInterface
         $matches = array();
 
         // first try a  linux specific way
-        $node = $this->getsysfs();
+        $node = $this->getSysfs();
 
         // Search the ifconfig output for all MAC addresses and return
         // the first one found
@@ -84,14 +84,20 @@ class SystemNodeProvider implements NodeProviderInterface
      *
      * @return string|bool
      */
-    protected function getsysfs()
+    protected function getSysfs()
     {
         $mac = false;
+
         if (strtoupper(php_uname('s')) === "LINUX") {
-            // get all the macadresses of all systems
+            $addressPaths = glob('/sys/class/net/*/address', GLOB_NOSORT);
+
+            if (empty($addressPaths)) {
+                return false;
+            }
+
             $macs = array_map(
                 'file_get_contents',
-                glob('/sys/class/net/*/address', GLOB_NOSORT)
+                $addressPaths
             );
 
             $macs = array_map('trim', $macs);
@@ -107,6 +113,7 @@ class SystemNodeProvider implements NodeProviderInterface
 
             $mac = reset($macs);
         }
+
         return $mac;
     }
 }
