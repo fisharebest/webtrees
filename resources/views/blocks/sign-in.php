b8f2863d24ba605eea21b854e8e848ae8fcc8f6c
Rico Sonntag <rico.sonntag@netresearch.de>
2018-03-13 09:55:37 +0100

    Updated log/sign in forms to responsive format

--- resources/views/blocks/sign-in.php
+++ resources/views/blocks/sign-in.php
@@ -1,55 +1,39 @@
 <?php use Fisharebest\Webtrees\I18N; ?>
 
-<form name="login-form" method="POST" action="<?= route('login', ['url' => route('user-page')]) ?>">
-	<?= csrf_field() ?>
+<div class="form-signin">
+	<div class="wt-page-options wt-page-options-login">
+		<form action="<?= route('login', ['url' => route('user-page')]) ?>" method="post">
+			<?= csrf_field() ?>
 
-	<div class="form-group">
-		<label for="username">
-			<?= I18N::translate('Username') ?>
-		</label>
-		<input type="text" id="username" name="username" class="form-control">
-	</div>
+			<div class="form-label-group mb-2">
+				<label for="username" class="col-form-label sr-only">
+					<?= I18N::translate('Username') ?>
+				</label>
+				<input type="text" id="username" name="username" class="form-control"
+					value="" placeholder="<?= I18N::translate('Username') ?>" required autofocus>
+			</div>
 
-	<div class="form-group">
-		<label for="password">
-			<?= I18N::translate('Password') ?>
-		</label>
-		<input type="password" id="password" name="password" class="form-control" autocomplete="current-password">
-	</div>
+			<div class="form-label-group mb-3">
+				<label for="password" class="col-form-label sr-only">
+					<?= I18N::translate('Password') ?>
+				</label>
+				<input autocomplete="current-password" type="password" id="password"
+					name="password" class="form-control" placeholder="<?= I18N::translate('Password') ?>" required>
+			</div>
 
-	<div>
-		<button type="submit" class="btn btn-primary">
-			<?= /* I18N: A button label. */ I18N::translate('sign in') ?>
-		</button>
-
-		<button type="button" class="btn btn-secondary" data-toggle="collapse" data-target="#forgot-password" aria-expanded="false" aria-controls="forgot-password">
-			<?= I18N::translate('Forgot password?') ?>
-		</button>
-	</div>
-</form>
-
-<?php if ($allow_register): ?>
-	<a class="btn btn-link" href="<?= e(route('register')) ?>">
-		<?= I18N::translate('Request a new user account') ?>
-	</a>
-<?php endif ?>
-
-<div class="collapse" id="forgot-password">
-	<form action="<?= e(route('login')) ?>" method="POST">
-		<input type="hidden" name="time" value="">
-		<input type="hidden" name="action" value="requestpw">
-		<?= I18N::translate('Request a new password') ?>
-		<div class="form-group">
-			<label for="new_passwd_username">
-				<?= I18N::translate('Username or email address') ?>
-				<input type="text" id="new_passwd_username" name="new_passwd_username" class="form-control">
-			</label>
-		</div>
-		<div>
-			<button type="submit" class="btn btn-primary">
-				<?= /* I18N: A button label. */ I18N::translate('continue') ?>
+			<button class="btn btn-lg btn-primary btn-block mb-2" type="submit">
+				<?= I18N::translate('sign in') ?>
 			</button>
-		</div>
-	</form>
-</div>
+		</form>
+	</div>
 
+	<a class="password-reset" href="<?= e(route('forgot-password')) ?>">
+		<?= I18N::translate('Forgot password?') ?>
+	</a>
+
+	<?php if ($allow_register): ?>
+		<a class="new-account" href="<?= e(route('register')) ?>">
+			<?= I18N::translate('Request a new user account') ?>
+		</a>
+	<?php endif; ?>
+</div>
--- resources/views/login-page.php
+++ resources/views/login-page.php
@@ -1,11 +1,5 @@
 <?php use Fisharebest\Webtrees\I18N; ?>
 
-<?php if ($error): ?>
-	<div class="alert alert-danger">
-		<?= $error ?>
-	</div>
-<?php endif ?>
-
 <h2 class="wt-page-title">
 	<?= I18N::translate('Welcome to this genealogy website') ?>
 </h2>
@@ -14,50 +8,53 @@
 	<?= nl2br($welcome, false) ?>
 </p>
 
-<form action="<?= e(route('login')) ?>" class="wt-page-options wt-page-options-login" method="post">
-	<?= csrf_field() ?>
-	<input type="hidden" name="url" value="<?= e($url) ?>">
-
-	<div class="form-group row">
-		<label class="col-sm-3 col-form-label wt-page-options-label" for="username">
-			<?= I18N::translate('Username') ?>
-		</label>
-		<div class="col-sm-9 wt-page-options-value">
-			<input class="form-control" type="text" id="username" name="username" required value="<?= e($username) ?>">
+<div class="form-login">
+	<?php if ($error): ?>
+		<div class="alert alert-danger">
+			<?= $error ?>
 		</div>
-	</div>
+	<?php endif; ?>
 
-	<div class="form-group row">
-		<label class="col-sm-3 col-form-label wt-page-options-label" for="password">
-			<?= I18N::translate('Password') ?>
-		</label>
-		<div class="col-sm-9 wt-page-options-value">
-			<input autocomplete="current-password" class="form-control" id="password" name="password" required type="password">
-		</div>
-	</div>
+	<div class="wt-page-options wt-page-options-login">
+		<form action="<?= e(route('login')) ?>" method="post">
+			<?= csrf_field() ?>
+			<input type="hidden" name="url" value="<?= e($url) ?>">
 
-	<div class="form-group row">
-		<div class="col-sm-3 col-form-label wt-page-options-label">
-		</div>
-		<div class="col-sm-9 wt-page-options-value">
-			<button class="btn btn-primary" type="submit">
-				<?= /* I18N: A button label. */ I18N::translate('sign in') ?>
+			<div class="form-label-group mb-2">
+				<label for="username" class="col-form-label sr-only">
+					<?= I18N::translate('Username') ?>
+				</label>
+				<input type="text" id="username" name="username" class="form-control"
+					value="<?= e($username) ?>" placeholder="<?= I18N::translate('Username') ?>" required autofocus>
+			</div>
+
+			<div class="form-label-group mb-3">
+				<label for="password" class="col-form-label sr-only">
+					<?= I18N::translate('Password') ?>
+				</label>
+				<input autocomplete="current-password" type="password" id="password"
+					name="password" class="form-control" placeholder="<?= I18N::translate('Password') ?>" required>
+			</div>
+
+			<button class="btn btn-lg btn-primary btn-block mb-2" type="submit">
+				<?= I18N::translate('sign in') ?>
 			</button>
-			<!--
-			Emails are sent from a TREE, not from a SITE. Therefore if there is no
-			tree available (initial setup or all trees private), then we can't send email.
-			-->
-			<?php if ($tree): ?>
-				<a class="btn btn-link" href="<?= e(route('forgot-password')) ?>">
-					<?= I18N::translate('Forgot password?') ?>
-				</a>
 
-				<?php if ($can_register): ?>
-					<a class="btn btn-link" href="<?= e(route('register')) ?>">
-						<?= I18N::translate('Request a new user account') ?>
+			<?php /* Emails are sent from a TREE, not from a SITE. Therefore if there is no
+					tree available (initial setup or all trees private), then we can't send email. */ ?>
+			<?php if ($tree): ?>
+				<div class="password-reset clearfix">
+					<a class="float-right" href="<?= e(route('forgot-password')) ?>">
+						<?= I18N::translate('Forgot password?') ?>
 					</a>
-				<?php endif ?>
-			<?php endif ?>
-		</div>
+				</div>
+			<?php endif; ?>
+		</form>
 	</div>
-</form>
+
+	<?php if ($tree && $can_register): ?>
+		<a class="text-center new-account mt-3" href="<?= e(route('register')) ?>">
+			<?= I18N::translate('Request a new user account') ?>
+		</a>
+	<?php endif; ?>
+</div>
