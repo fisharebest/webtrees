27c0f572b934f4face3bfa96657fa783691d714d
David Drury <david@drury.me.uk>
2018-05-05 11:59:13 +0100

    add back lost schema changes

---
+++ app/Module/OpenStreetMap/Schema/Migration2.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * webtrees: online genealogy
+ * Copyright (C) 2018 webtrees development team
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace Fisharebest\Webtrees\Module\OpenStreetMap\Schema;
+
+use Fisharebest\Webtrees\Database;
+use Fisharebest\Webtrees\Schema\MigrationInterface;
+
+/**
+ * Upgrade the database schema from version 2 to version 3.
+ */
+class Migration2 implements MigrationInterface {
+	/** {@inheritDoc} */
+	public function upgrade() {
+		// Convert flag icons from .gif to .png
+
+		Database::exec("UPDATE `##placelocation` SET pl_icon=REPLACE(pl_icon, '.gif', '.png')");
+	}
+}
---
+++ app/Module/OpenStreetMap/Schema/Migration3.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * webtrees: online genealogy
+ * Copyright (C) 2018 webtrees development team
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fisharebest\Webtrees\Module\OpenStreetMap\Schema;
+
+use Fisharebest\Webtrees\Database;
+use Fisharebest\Webtrees\DebugBar;
+use Fisharebest\Webtrees\Schema\MigrationInterface;
+use PDOException;
+
+/**
+ * Upgrade the database schema from version 3 to version 4.
+ */
+
+class Migration3 implements MigrationInterface {
+	/** {@inheritDoc} */
+
+	public function upgrade() {
+		$queries = [
+			// Remove streetview fields
+			"ALTER TABLE `##placelocation`" .
+			" DROP COLUMN pl_media," .
+			" DROP COLUMN sv_long, " .
+			" DROP COLUMN sv_lati, " .
+			" DROP COLUMN sv_bearing, " .
+			" DROP COLUMN sv_elevation, ".
+			" DROP COLUMN sv_zoom",
+			// Reset fields to default empty value
+			"UPDATE `##placelocation` SET " .
+			"pl_long = IF(pl_long IN ('', 'E0'), NULL, pl_long)," .
+			"pl_lati = IF(pl_lati IN ('', 'N0'), NULL, pl_lati)," .
+			"pl_zoom = IF(pl_zoom = '', NULL, pl_zoom)," .
+			"pl_icon = IF(pl_icon = '', NULL, pl_icon)",
+			// Clear out earlier versions of settings (if any)
+			"DELETE FROM `##module_setting` WHERE module_name='openstreetmap' AND setting_name=''"
+			];
+
+		foreach($queries as $query) {
+			try {
+				Database::exec($query);
+			} catch (PDOException $ex) {
+				DebugBar::addThrowable($ex);
+
+				// Already done this?
+			}
+		}
+	}
+}
