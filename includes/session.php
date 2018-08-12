d43d85779ecbca81a1daae7356c59ff690494822
Greg Roach <fisharebest@gmail.com>
2018-08-12 09:00:38 +0100

    1.7.10

--- includes/session.php
+++ includes/session.php
@@ -40,7 +40,7 @@ global $WT_TREE, $SEARCH_SPIDER;
 
 // Identify ourself
 define('WT_WEBTREES', 'webtrees');
-define('WT_VERSION', '1.7.10');
+define('WT_VERSION', '1.7.11-dev');
 
 // External URLs
 define('WT_WEBTREES_URL', 'https://www.webtrees.net/');
