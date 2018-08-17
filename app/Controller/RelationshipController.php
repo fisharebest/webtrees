23cbb7db2c340c86173433f532c198b47d63e6ab
Greg Roach <fisharebest@webtrees.net>
2018-08-17 18:07:37 +0100

    PHP7.2 compatibility

--- app/Controller/RelationshipController.php
+++ app/Controller/RelationshipController.php
@@ -74,7 +74,7 @@ class RelationshipController extends PageController {
 			// Insert the paths into the queue, with an exclusion list.
 			$queue[] = array('path' => $path, 'exclude' => array());
 			// While there are un-extended paths
-			while (list(, $next) = each($queue)) {
+            for ($next = current($queue); $next !== false; $next = next($queue)) {
 				// For each family on the path
 				for ($n = count($next['path']) - 2; $n >= 1; $n -= 2) {
 					$exclude = $next['exclude'];
