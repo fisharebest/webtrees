2e610ef35d7d4a98330b07e073f9da6fa032cb27
Greg Roach <fisharebest@webtrees.net>
2018-08-18 16:44:37 +0100

    Fix: #1825 - double escaping of block title

--- resources/views/modules/block-template.php
+++ resources/views/modules/block-template.php
@@ -3,10 +3,10 @@
 
 <div class="card mb-4 wt-block wt-block-<?= e($block) ?>-block" id="block-<?= e($id) ?>">
 	<div class="card-header wt-block-header wt-block-header-<?= e($block) ?>" dir="auto">
-		<?php if ($config_url): ?>
+		<?php if ($config_url !== ''): ?>
 			<?= FontAwesome::linkIcon('preferences', I18N::translate('Preferences'), ['class' => 'btn btn-link', 'href' => $config_url]) ?>
 		<?php endif ?>
-		<?= e($title) ?>
+		<?= $title ?>
 	</div>
 	<div class="card-body wt-block-content wt-block-content-<?= e($block) ?>">
 		<?= $content ?>
