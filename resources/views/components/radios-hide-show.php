8643019c40c45553e73458e34db89a4b2e124230
Greg Roach <fisharebest@webtrees.net>
2018-09-06 22:06:09 +0100

    Fix: #1890 - cannot choose 'false' option on checkboxes

--- resources/views/components/radios-hide-show.php
+++ resources/views/components/radios-hide-show.php
@@ -1,15 +1,15 @@
 <?php use Fisharebest\Webtrees\I18N; ?>
 
 <div class="form-check form-check-inline">
-	<input class="form-check-input" type="radio" name="<?= e($name) ?>" id="radio-<?= e($name) ?>-hide" <?= $value ? '' : 'checked' ?>>
-	<label class="form-check-label" for="radio-<?= e($name) ?>-hide">
+	<input class="form-check-input" type="radio" name="<?= e($name) ?>" id="radio-<?= e($name) ?>-false" value="" <?= $value ? '' : 'checked' ?>>
+	<label class="form-check-label" for="radio-<?= e($name) ?>-false">
     <?= I18N::translate('hide') ?>
 	</label>
 </div>
 
 <div class="form-check form-check-inline">
-	<input class="form-check-input" type="radio" name="<?= e($name) ?>" id="radio-<?= e($name) ?>-show" <?= $value ? 'checked' : '' ?>>
-	<label class="form-check-label" for="radio-<?= e($name) ?>-show">
+	<input class="form-check-input" type="radio" name="<?= e($name) ?>" id="radio-<?= e($name) ?>-true" value="1" <?= $value ? 'checked' : '' ?>>
+	<label class="form-check-label" for="radio-<?= e($name) ?>-true">
     <?= I18N::translate('show') ?>
 	</label>
 </div>
--- resources/views/components/radios-no-yes.php
+++ resources/views/components/radios-no-yes.php
@@ -1,15 +1,15 @@
 <?php use Fisharebest\Webtrees\I18N; ?>
 
 <div class="form-check form-check-inline">
-	<input class="form-check-input" type="radio" name="<?= e($name) ?>" id="radio-<?= e($name) ?>-no" <?= $value ? '' : 'checked' ?>>
-	<label class="form-check-label" for="radio-<?= e($name) ?>-no">
+	<input class="form-check-input" type="radio" name="<?= e($name) ?>" id="radio-<?= e($name) ?>-false" value="" <?= $value ? '' : 'checked' ?>>
+	<label class="form-check-label" for="radio-<?= e($name) ?>-false">
     <?= I18N::translate('no') ?>
 	</label>
 </div>
 
 <div class="form-check form-check-inline">
-	<input class="form-check-input" type="radio" name="<?= e($name) ?>" id="radio-<?= e($name) ?>-yes" <?= $value ? 'checked' : '' ?>>
-	<label class="form-check-label" for="radio-<?= e($name) ?>-yes">
+	<input class="form-check-input" type="radio" name="<?= e($name) ?>" id="radio-<?= e($name) ?>-true" value="1" <?= $value ? 'checked' : '' ?>>
+	<label class="form-check-label" for="radio-<?= e($name) ?>-true">
     <?= I18N::translate('yes') ?>
 	</label>
 </div>
