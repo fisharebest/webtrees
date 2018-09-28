15c2c78b408f65bb28627855ed0445e719f33580
Greg Roach <fisharebest@webtrees.net>
2018-09-28 18:36:01 +0100

    Fix: #1937 - composer autoload dev/no-dev (again)

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,6 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
-    'Mockery' => array($vendorDir . '/mockery/mockery/library'),
 );
