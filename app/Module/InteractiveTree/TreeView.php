1de810172b2fe9b7cd434f952ee021def759befe
Greg Roach <fisharebest@webtrees.net>
2018-10-08 13:06:42 +0100

    Code-style

--- app/Module/InteractiveTree/TreeView.php
+++ app/Module/InteractiveTree/TreeView.php
@@ -35,7 +35,7 @@ class TreeView
      *
      * @param string $name the name of the TreeView object’s instance
      */
-    public function __construct($name = 'tree')
+    public function __construct(string $name = 'tree')
     {
         $this->name = $name;
     }
@@ -49,11 +49,11 @@ class TreeView
      *
      * @return string[]  HTML and Javascript
      */
-    public function drawViewport(Individual $individual, $generations): array
+    public function drawViewport(Individual $individual, int $generations): array
     {
         $html = view('interactive-tree-chart', [
             'name'       => $this->name,
-            'individual' => $this->drawPerson($individual, $generations, 0, null, null, true),
+            'individual' => $this->drawPerson($individual, $generations, 0, null, '', true),
         ]);
 
         return [
@@ -70,31 +70,41 @@ class TreeView
      *
      * @return string
      */
-    public function getPersons(Tree $tree, $list): string
+    public function getPersons(Tree $tree, string $list): string
     {
         $list = explode(';', $list);
         $r    = [];
         foreach ($list as $jsonRequest) {
             $firstLetter = substr($jsonRequest, 0, 1);
             $jsonRequest = substr($jsonRequest, 1);
+
             switch ($firstLetter) {
                 case 'c':
-                    $fidlist = explode(',', $jsonRequest);
-                    $flist   = [];
-                    foreach ($fidlist as $fid) {
-                        $flist[] = Family::getInstance($fid, $tree);
+                    $xrefs    = explode(',', $jsonRequest);
+                    $families = [];
+
+                    foreach ($xrefs as $xref) {
+                        $family = Family::getInstance($xref, $tree);
+
+                        if ($family instanceof Family) {
+                            $families[] = $family;
+                        }
                     }
-                    $r[] = $this->drawChildren($flist, 1, true);
+                    $r[] = $this->drawChildren($families, 1, true);
                     break;
+
                 case 'p':
-                    $params = explode('@', $jsonRequest);
-                    $fid    = $params[0];
-                    $order  = $params[1];
-                    $f      = Family::getInstance($fid, $tree);
-                    if ($f->getHusband()) {
-                        $r[] = $this->drawPerson($f->getHusband(), 0, 1, $f, $order);
-                    } elseif ($f->getWife()) {
-                        $r[] = $this->drawPerson($f->getWife(), 0, 1, $f, $order);
+                    list($xref, $order) = explode('@', $jsonRequest);
+
+                    $family = Family::getInstance($xref, $tree);
+                    if ($family instanceof Family) {
+                        // Prefer the paternal line
+                        $parent = $family->getHusband() ?? $family->getWife();
+
+                        // The family may have no parents (just children).
+                        if ($parent instanceof Individual) {
+                            $r[] = $this->drawPerson($parent, 0, 1, $family, $order, false);
+                        }
                     }
                     break;
             }
@@ -166,16 +176,13 @@ class TreeView
      *
      * @return string
      */
-    private function drawChildren(array $familyList, $gen = 1, $ajax = false): string
+    private function drawChildren(array $familyList, int $gen = 1, bool $ajax = false): string
     {
         $html          = '';
         $children2draw = [];
         $f2load        = [];
 
         foreach ($familyList as $f) {
-            if (empty($f)) {
-                continue;
-            }
             $children = $f->getChildren();
             if ($children) {
                 $f2load[] = $f->getXref();
@@ -200,7 +207,7 @@ class TreeView
                 } else {
                     $co = 'h';
                 }
-                $html .= $this->drawPerson($child, $gen - 1, -1, null, $co);
+                $html .= $this->drawPerson($child, $gen - 1, -1, null, $co, false);
             }
             if (!$ajax) {
                 $html = '<td align="right"' . ($gen == 0 ? ' abbr="c' . $f2load . '"' : '') . '>' . $html . '</td>' . $this->drawHorizontalLine();
@@ -213,29 +220,27 @@ class TreeView
     /**
      * Draw a person in the tree
      *
-     * @param Individual $person The Person object to draw the box for
-     * @param int        $gen    The number of generations up or down to print
-     * @param int        $state  Whether we are going up or down the tree, -1 for descendents +1 for ancestors
-     * @param Family     $pfamily
-     * @param string     $order  first (1), last(2), unique(0), or empty. Required for drawing lines between boxes
-     * @param bool       $isRoot
+     * @param Individual  $person The Person object to draw the box for
+     * @param int         $gen    The number of generations up or down to print
+     * @param int         $state  Whether we are going up or down the tree, -1 for descendents +1 for ancestors
+     * @param Family|null $pfamily
+     * @param string      $line   b, c, h, t. Required for drawing lines between boxes
+     * @param bool        $isRoot
      *
      * @return string
-     *
-     * Notes : "spouse" means explicitely married partners. Thus, the word "partner"
-     * (for "life partner") here fits much better than "spouse" or "mate"
-     * to translate properly the modern french meaning of "conjoint"
      */
-    private function drawPerson(Individual $person, $gen, $state, Family $pfamily = null, $order = '', $isRoot = false): string
+    private function drawPerson(Individual $person, int $gen, int $state, Family $pfamily = null, string $line = '', $isRoot = false): string
     {
         if ($gen < 0) {
             return '';
         }
+
         if (!empty($pfamily)) {
             $partner = $pfamily->getSpouse($person);
         } else {
             $partner = $person->getCurrentSpouse();
         }
+
         if ($isRoot) {
             $html = '<table id="tvTreeBorder" class="tv_tree"><tbody><tr><td id="tv_tree_topleft"></td><td id="tv_tree_top"></td><td id="tv_tree_topright"></td></tr><tr><td id="tv_tree_left"></td><td>';
         } else {
@@ -249,73 +254,83 @@ class TreeView
             $html .= $this->drawChildren($person->getSpouseFamilies(), $gen);
         } else {
             // draw the parent’s lines
-            $html .= $this->drawVerticalLine($order) . $this->drawHorizontalLine();
+            $html .= $this->drawVerticalLine($line) . $this->drawHorizontalLine();
         }
 
         /* draw the person. Do NOT add person or family id as an id, since a same person could appear more than once in the tree !!! */
         // Fixing the width for td to the box initial width when the person is the root person fix a rare bug that happen when a person without child and without known parents is the root person : an unwanted white rectangle appear at the right of the person’s boxes, otherwise.
         $html .= '<td' . ($isRoot ? ' style="width:1px"' : '') . '><div class="tv_box' . ($isRoot ? ' rootPerson' : '') . '" dir="' . I18N::direction() . '" style="text-align: ' . (I18N::direction() === 'rtl' ? 'right' : 'left') . '; direction: ' . I18N::direction() . '" abbr="' . $person->getXref() . '" onclick="' . $this->name . 'Handler.expandBox(this, event);">';
-        $html .= $this->drawPersonName($person);
+        $html .= $this->drawPersonName($person, '');
         $fop = []; // $fop is fathers of partners
         if ($partner !== null) {
             $dashed = '';
             foreach ($person->getSpouseFamilies() as $family) {
                 $spouse = $family->getSpouse($person);
-                if ($spouse) {
+                if ($spouse instanceof Individual) {
                     $spouse_parents = $spouse->getPrimaryChildFamily();
-                    if ($spouse_parents && $spouse_parents->getHusband()) {
-                        $fop[] = [
-                            $spouse_parents->getHusband(),
-                            $spouse_parents,
-                        ];
-                    } elseif ($spouse_parents && $spouse_parents->getWife()) {
-                        $fop[] = [
-                            $spouse_parents->getWife(),
-                            $spouse_parents,
-                        ];
+                    if ($spouse_parents instanceof Family) {
+                        // Prefer the paternal line
+                        $parent = $spouse_parents->getHusband() ?? $spouse_parents->getWife();
+
+                        // The family may have no parents (just children).
+                        if ($parent instanceof Individual) {
+                            $fop[] = [
+                                $parent,
+                                $spouse_parents,
+                            ];
+                        }
+                        $html .= $this->drawPersonName($spouse, $dashed);
+                        $dashed = 'dashed';
                     }
-                    $html .= $this->drawPersonName($spouse, $dashed);
-                    $dashed = 'dashed';
                 }
             }
         }
         $html .= '</div></td>';
 
         $primaryChildFamily = $person->getPrimaryChildFamily();
-        if (!empty($primaryChildFamily)) {
-            $parent = $primaryChildFamily->getHusband();
-            if (empty($parent)) {
-                $parent = $primaryChildFamily->getWife();
-            }
+
+        if ($primaryChildFamily instanceof Family) {
+            // Prefer the paternal line
+            $parent = $primaryChildFamily->getHusband() ?? $primaryChildFamily->getWife();
+        } else {
+            $parent = null;
         }
-        if (!empty($parent) || count($fop) || ($state < 0)) {
+
+        // The family may have no parents (just children).
+        if ($parent instanceof Individual || !empty($fop) || $state < 0) {
             $html .= $this->drawHorizontalLine();
         }
+
         /* draw the parents */
-        if ($state >= 0 && (!empty($parent) || count($fop))) {
-            $unique = empty($parent) || count($fop) === 0;
+        if ($primaryChildFamily instanceof Family && $state >= 0 && ($parent instanceof Individual || !empty($fop))) {
+            $unique = $parent === null || empty($fop);
             $html .= '<td align="left"><table class="tv_tree"><tbody>';
+
             if (!empty($parent)) {
                 $u = $unique ? 'c' : 't';
                 $html .= '<tr><td ' . ($gen == 0 ? ' abbr="p' . $primaryChildFamily->getXref() . '@' . $u . '"' : '') . '>';
-                $html .= $this->drawPerson($parent, $gen - 1, 1, $primaryChildFamily, $u);
+                $html .= $this->drawPerson($parent, $gen - 1, 1, $primaryChildFamily, $u, false);
                 $html .= '</td></tr>';
             }
+
             if (count($fop)) {
                 $n  = 0;
                 $nb = count($fop);
                 foreach ($fop as $p) {
                     $n++;
                     $u = $unique ? 'c' : ($n == $nb || empty($p[1]) ? 'b' : 'h');
-                    $html .= '<tr><td ' . ($gen == 0 ? ' abbr="p' . $p[1]->getXref() . '@' . $u . '"' : '') . '>' . $this->drawPerson($p[0], $gen - 1, 1, $p[1], $u) . '</td></tr>';
+                    $html .= '<tr><td ' . ($gen == 0 ? ' abbr="p' . $p[1]->getXref() . '@' . $u . '"' : '') . '>' . $this->drawPerson($p[0], $gen - 1, 1, $p[1], $u, false) . '</td></tr>';
                 }
             }
             $html .= '</tbody></table></td>';
         }
+
         if ($state < 0) {
-            $html .= $this->drawVerticalLine($order);
+            $html .= $this->drawVerticalLine($line);
         }
+
         $html .= '</tr></tbody></table>';
+
         if ($isRoot) {
             $html .= '</td><td id="tv_tree_right"></td></tr><tr><td id="tv_tree_bottomleft"></td><td id="tv_tree_bottom"></td><td id="tv_tree_bottomright"></td></tr></tbody></table>';
         }
@@ -326,12 +341,12 @@ class TreeView
     /**
      * Draw a person name preceded by sex icon, with parents as tooltip
      *
-     * @param Individual $individual an individual
-     * @param string     $dashed     if = 'dashed' print dashed top border to separate multiple spuses
+     * @param Individual $individual The individual to draw
+     * @param string     $dashed     Either "dashed", to print dashed top border to separate multiple spouses, or ""
      *
      * @return string
      */
-    private function drawPersonName(Individual $individual, $dashed = ''): string
+    private function drawPersonName(Individual $individual, string $dashed): string
     {
         $family = $individual->getPrimaryChildFamily();
         if ($family) {
@@ -365,7 +380,7 @@ class TreeView
      *
      * @return string
      */
-    private function getThumbnail(Individual $individual)
+    private function getThumbnail(Individual $individual): string
     {
         if ($individual->getTree()->getPreference('SHOW_HIGHLIGHT_IMAGES')) {
             return $individual->displayImage(40, 50, 'crop', []);
@@ -377,7 +392,7 @@ class TreeView
     /**
      * Draw a vertical line
      *
-     * @param string $order A parameter that set how to draw this line with auto-redimensionning capabilities
+     * @param string $line A parameter that set how to draw this line with auto-redimensionning capabilities
      *
      * @return string
      * WARNING : some tricky hacks are required in CSS to ensure cross-browser compliance
@@ -385,9 +400,9 @@ class TreeView
      * and some other browsers (ex: firefox) shows a <div> tag, which have no size limit in height
      * Therefore, Firefox is a good choice to print very big trees.
      */
-    private function drawVerticalLine($order): string
+    private function drawVerticalLine(string $line): string
     {
-        return '<td class="tv_vline tv_vline_' . $order . '"><div class="tv_vline tv_vline_' . $order . '"></div></td>';
+        return '<td class="tv_vline tv_vline_' . $line . '"><div class="tv_vline tv_vline_' . $line . '"></div></td>';
     }
 
     /**
