783c6f8d516dad892d4bf912ace1d108fb2cce9b
Greg Roach <fisharebest@webtrees.net>
2018-11-02 11:08:45 +0000

    Age

--- app/Age.php
+++ app/Age.php
@@ -25,19 +25,38 @@ use function trim;
 
 /**
  * Representation of a GEDCOM age.
+ *
  * Ages may be a keyword (stillborn, infant, child) or a number of years,
  * months and/or days such as "6y 3m".
  */
 class Age
 {
     // GEDCOM keyword: died just prior, at, or near birth, 0 years
-    const KEYWORD_STILLBORN = 'stillborn';
+    private const KEYWORD_STILLBORN = 'stillborn';
 
     // GEDCOM keyword: age < 1 year
-    const KEYWORD_INFANT = 'infant';
+    private const KEYWORD_INFANT = 'infant';
 
     // GEDCOM keyword: age < 8 years
-    const KEYWORD_CHILD = 'child';
+    private const KEYWORD_CHILD = 'child';
+
+    // GEDCOM symbol: aged less than
+    private const SYMBOL_LESS_THAN = '<';
+
+    // GEDCOM symbol: aged more than
+    private const SYMBOL_MORE_THAN = '>';
+
+    // GEDCOM symbol: number of years
+    private const SYMBOL_YEARS = 'y';
+
+    // GEDCOM symbol: number of months
+    private const SYMBOL_MONTHS = 'm';
+
+    // GEDCOM symbol: number of weeks (this is a non-standard extension)
+    private const SYMBOL_WEEKS = 'w';
+
+    // GEDCOM symbol: number of days
+    private const SYMBOL_DAYS = 'd';
 
     /** @var string */
     private $keyword = '';
@@ -76,33 +95,15 @@ class Age
         // Qualifier
         $qualifier = substr($age, 0, 1);
 
-        if ($qualifier === '<' || $qualifier === '>') {
+        if ($qualifier === self::SYMBOL_LESS_THAN || $qualifier === self::SYMBOL_MORE_THAN) {
             $this->qualifier = $qualifier;
         }
 
         // Number of years, months, weeks and days.
-        // Weeks are a non-standard extension.
-        $this->years  = $this->extractNumber($age, 'y');
-        $this->months = $this->extractNumber($age, 'm');
-        $this->weeks  = $this->extractNumber($age, 'w');
-        $this->days   = $this->extractNumber($age, 'd');
-    }
-
-    /**
-     * Extract a number of days/weeks/months/years from the age string.
-     *
-     * @param string $age
-     * @param string $suffix
-     *
-     * @return int
-     */
-    private function extractNumber(string $age, string $suffix): int
-    {
-        if (preg_match('/(\d+)' . $suffix . '/', $age, $match)) {
-            return (int) $match[1];
-        }
-
-        return 0;
+        $this->years  = $this->extractNumber($age, self::SYMBOL_YEARS);
+        $this->months = $this->extractNumber($age, self::SYMBOL_MONTHS);
+        $this->weeks  = $this->extractNumber($age, self::SYMBOL_WEEKS);
+        $this->days   = $this->extractNumber($age, self::SYMBOL_DAYS);
     }
 
     /**
@@ -111,17 +112,17 @@ class Age
     public function asText(): string
     {
         if ($this->keyword === self::KEYWORD_STILLBORN) {
-            // I18N: Description of an individual’s age at an event. e.g. Died 14 Jan 1900 (stillborn)
+            // I18N: An individual’s age at an event. e.g. Died 14 Jan 1900 (stillborn)
             return I18N::translate('(stillborn)');
         }
 
         if ($this->keyword === self::KEYWORD_INFANT) {
-            // I18N: Description of an individual’s age at an event. e.g. Died 14 Jan 1900 (in infancy)
+            // I18N: An individual’s age at an event. e.g. Died 14 Jan 1900 (in infancy)
             return I18N::translate('(in infancy)');
         }
 
         if ($this->keyword === self::KEYWORD_CHILD) {
-            // I18N: Description of an individual’s age at an event. e.g. Died 14 Jan 1900 (in childhood)
+            // I18N: An individual’s age at an event. e.g. Died 14 Jan 1900 (in childhood)
             return I18N::translate('(in childhood)');
         }
 
@@ -155,17 +156,34 @@ class Age
 
         $age_string = implode(I18N::$list_separator, $age);
 
-        if ($this->qualifier === '<') {
+        if ($this->qualifier === self::SYMBOL_LESS_THAN) {
             // I18N: Description of an individual’s age at an event. For example, Died 14 Jan 1900 (aged less than 21 years)
             return I18N::translate('(aged less than %s)', $age_string);
         }
 
-        if ($this->qualifier === '>') {
+        if ($this->qualifier === self::SYMBOL_MORE_THAN) {
             // I18N: Description of an individual’s age at an event. For example, Died 14 Jan 1900 (aged more than 21 years)
             return I18N::translate('(aged more than %s)', $age_string);
         }
 
         // I18N: Description of an individual’s age at an event. For example, Died 14 Jan 1900 (aged 43 years)
         return I18N::translate('(aged %s)', $age_string);
+    }
+
+    /**
+     * Extract a number of days/weeks/months/years from the age string.
+     *
+     * @param string $age
+     * @param string $suffix
+     *
+     * @return int
+     */
+    private function extractNumber(string $age, string $suffix): int
+    {
+        if (preg_match('/(\d+) *' . $suffix . '/', $age, $match)) {
+            return (int) $match[1];
+        }
+
+        return 0;
     }
 }
