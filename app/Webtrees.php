8b2f02c531837b556b7711bcad1430bf8ea27626
Greg Roach <fisharebest@webtrees.net>
2018-12-08 09:36:42 +0000

    2.0.0-alpha.4

--- app/Webtrees.php
+++ app/Webtrees.php
@@ -43,7 +43,7 @@ class Webtrees
     const SCHEMA_VERSION = 40;
 
     // e.g. "dev", "alpha", "beta.3", etc.
-    const STABILITY = 'dev';
+    const STABILITY = 'alpha.4';
 
     // Project website.
     const URL = 'https://www.webtrees.net/';
