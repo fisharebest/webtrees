3eae54c8a20b98985facfcff8d58c982338bbf85
Greg Roach <fisharebest@webtrees.net>
2019-08-13 11:21:16 +0100

    Update example middleware

--- modules_v4/example-middleware.disable/module.php
+++ modules_v4/example-middleware.disable/module.php
@@ -11,12 +11,24 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use function in_array;
+use function preg_match;
 
 /**
  * An example module to demonstrate middleware.
  */
 return new class extends AbstractModule implements ModuleCustomInterface, MiddlewareInterface {
     use ModuleCustomTrait;
+
+    // Regular-expressions to match unwanted bots.
+    private const BAD_USER_AGENTS = [
+        '/AhrefsBot/',
+        '/MJ12bot/',
+        '/SeznamBot/',
+    ];
+
+    // List of unwanted IP addresses.
+    private const BAD_IP_ADDRESSES = [
+    ];
 
     /**
      * How should this module be identified in the control panel, etc.?
@@ -39,9 +51,8 @@ return new class extends AbstractModule implements ModuleCustomInterface, Middle
     }
 
     /**
-     * For a description of request and response objects, refer to the Symfony HttpFoundation documentation.
-     *
-     * @see https://symfony.com/doc/current/components/http_foundation.html
+     * Code here is executed before and after we process the request/response.
+     * We can block access by throwing an exception.
      *
      * @param ServerRequestInterface  $request
      * @param RequestHandlerInterface $handler
@@ -51,20 +62,23 @@ return new class extends AbstractModule implements ModuleCustomInterface, Middle
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         // Code here is executed before we process the request/response.
-        // We can prevent the request being executed by throwing an exception.
-
-        $blacklist = [];
         $ip_address = $request->getAttribute('client_ip');
-
-        if (in_array($ip_address, $blacklist, true)) {
+        if (in_array($ip_address, self::BAD_IP_ADDRESSES, true)) {
             throw new AccessDeniedHttpException();
         }
 
-        // Generate the response from the next middleware handler.
+        $user_agent = $request->getHeaderLine('HTTP_USER_AGENT');
+        foreach (self::BAD_USER_AGENTS as $bad_user_agent) {
+            if (preg_match($bad_user_agent, $user_agent)) {
+                throw new AccessDeniedHttpException();
+            }
+        }
+
+        // Generate the response.
         $response = $handler->handle($request);
 
         // Code here is executed after we process the request/response.
-        // We can modify the response.
+        // We can also modify the response.
         $response = $response->withHeader('X-Powered-By', 'Fish');
 
         return $response;
