01202f8037e516171a36f999e52383b38b2f3b10
Greg Roach <fisharebest@webtrees.net>
2019-10-07 22:30:53 +0100

    Replace files deleted in error

---
+++ app/Http/Middleware/AuthMember.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * webtrees: online genealogy
+ * Copyright (C) 2019 webtrees development team
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
+namespace Fisharebest\Webtrees\Http\Middleware;
+
+use Fig\Http\Message\RequestMethodInterface;
+use Fisharebest\Webtrees\Auth;
+use Fisharebest\Webtrees\Contracts\UserInterface;
+use Fisharebest\Webtrees\FlashMessages;
+use Fisharebest\Webtrees\Http\RequestHandlers\LoginPage;
+use Fisharebest\Webtrees\I18N;
+use Fisharebest\Webtrees\Session;
+use Fisharebest\Webtrees\Tree;
+use Fisharebest\Webtrees\User;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use function in_array;
+use function redirect;
+
+/**
+ * Middleware to restrict access to editors.
+ */
+class AuthMember implements MiddlewareInterface
+{
+    /**
+     * @param ServerRequestInterface  $request
+     * @param RequestHandlerInterface $handler
+     *
+     * @return ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $tree = $request->getAttribute('tree');
+        $user = $request->getAttribute('user');
+
+        // Logged in with the correct role?
+        if ($tree instanceof Tree && Auth::isMember($tree, $user)) {
+            return $handler->handle($request);
+        }
+
+        // Logged in, but without the correct role?
+        if ($user instanceof User) {
+            throw new AccessDeniedHttpException();
+        }
+
+        // Not logged in.
+        return redirect(route(LoginPage::class, ['url' => $request->getAttribute('request_uri')]));
+    }
+}
