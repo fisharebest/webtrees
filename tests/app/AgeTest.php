506a570fa9c3f6601c12f783d0393f4ccacc5844
Greg Roach <fisharebest@webtrees.net>
2019-10-23 17:45:29 +0100

    CodeStyle

--- tests/app/AgeTest.php
+++ tests/app/AgeTest.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * webtrees: online genealogy
  * Copyright (C) 2019 webtrees development team
@@ -13,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 declare(strict_types=1);
 
 namespace Fisharebest\Webtrees;
