915db8c48659f56625569bc7989799a928114d5a
Greg Roach <fisharebest@webtrees.net>
2019-11-21 23:03:56 +0000

    Update documentation for custom modules

--- modules_v4/example-server-configuration.disable/module.php
+++ modules_v4/example-server-configuration.disable/module.php
@@ -8,6 +8,12 @@ declare(strict_types=1);
 
 namespace MyCustomNamespace;
 
+// Unlike the other examples, this one has a separate file for the class definition.
+// This is because the constructor has some dependencies, so we must create it
+// with "app(CustomModule::class)" rather than "new CustomModule()".
+// This means we can't use an anonymous class, and our coding standards
+// mean that the class needs to go in its own file.
+// For simple modules, it might be easier to declare the class here.
 require __DIR__ . '/ExampleServerConfigurationModule.php';
 
 return app(ExampleServerConfigurationModule::class);
