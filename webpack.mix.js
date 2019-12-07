11709c808d4d38db0a8712313618ca8417ce1cef
David Drury <ddrury@users.noreply.github.com>
2019-12-07 21:07:01 +0000

    Replace Colorbox with Lightgallery Fix download URL Compile the scss file to get bespoke css fix problem with showing images from slide show block removed some duplicate code speed improvements fix fonts path Make javascript a reusable view formatting add parameter to display only clicked item or everything fixed problem with webpack compiling assets - see note in webpack.mix.js allow each instance to have own options track upstream - follow js semistandard where possible

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,76 +1,76 @@
 // https://laravel-mix.com
-const mix = require("laravel-mix");
+const mix = require('laravel-mix');
 
 // https://github.com/postcss/postcss-import
-const postcss_import = require("postcss-import")();
+const postcss_import = require('postcss-import')();
 
 // https://github.com/postcss/autoprefixer
-const postcss_autoprefixer = require("autoprefixer")();
+const postcss_autoprefixer = require('autoprefixer')();
 
 // https://github.com/jakob101/postcss-inline-rtl
-const postcss_rtl = require("postcss-rtl")();
+const postcss_rtl = require('postcss-rtl')();
 
 // https://github.com/bezoerb/postcss-image-inliner
-const postcss_image_inliner = require("postcss-image-inliner")({
-    assetPaths: ["resources/css"],
-    maxFileSize: 0,
+const postcss_image_inliner = require('postcss-image-inliner')({
+  assetPaths: ['resources/css'],
+  maxFileSize: 0
 });
 
 // https://github.com/postcss/postcss-custom-properties
 // Enable CSS variables in IE
-const postcss_custom_properties = require("postcss-custom-properties")();
+const postcss_custom_properties = require('postcss-custom-properties')();
 
- /** 
+/**
  *  When using the mix.styles[] lines in this file
- *  along with compiling the lightgallery scss files meant that the the 
+ *  along with compiling the lightgallery scss files meant that the the
  *  lightgallery.css file wasn't included in vendor.css as the sass compilation
  *  occurred after mix.styles ran.
- * 
+ *
  * See resources/sass/vendor.scss
  */
 
 mix
-    .sass('resources/sass/vendor.scss', "public/css/vendor.min.css")
-    .autoload({
-        jquery: ["$", "jQuery"],
-    })
-    .setPublicPath('./public')
-    .sourceMaps()
-    .js("resources/js/vendor.js", "public/js/vendor.min.js")
-    .babel(["resources/js/webtrees.js", "resources/js/statistics.js", "resources/js/treeview.js"], "public/js/webtrees.min.js")
-    .copy("node_modules/leaflet/dist/images/*", "public/css/images/")
-    .copy("node_modules/dejavu-fonts-ttf/ttf/DejaVuSans.ttf", "resources/fonts/")
-    .copy("node_modules/lightgallery/src/fonts/*", "resources/fonts/")
-    .options({
-            processCssUrls: false,
-            postCss: [
-                postcss_import,
-                postcss_rtl,
-                postcss_autoprefixer,
-                postcss_image_inliner,
-                postcss_custom_properties,
-            ]
-    })
-    .postCss("resources/css/administration.css", "public/css/administration.min.css")
-    .postCss("resources/css/clouds.css", "public/css/clouds.min.css")
-    .postCss("resources/css/colors.css", "public/css/colors.min.css")
-    .postCss("resources/css/colors/aquamarine.css", "public/css/colors/aquamarine.min.css")
-    .postCss("resources/css/colors/ash.css", "public/css/colors/ash.min.css")
-    .postCss("resources/css/colors/belgianchocolate.css", "public/css/colors/belgianchocolate.min.css")
-    .postCss("resources/css/colors/bluelagoon.css", "public/css/colors/bluelagoon.min.css")
-    .postCss("resources/css/colors/bluemarine.css", "public/css/colors/bluemarine.min.css")
-    .postCss("resources/css/colors/coffeeandcream.css", "public/css/colors/coffeeandcream.min.css")
-    .postCss("resources/css/colors/coldday.css", "public/css/colors/coldday.min.css")
-    .postCss("resources/css/colors/greenbeam.css", "public/css/colors/greenbeam.min.css")
-    .postCss("resources/css/colors/mediterranio.css", "public/css/colors/mediterranio.min.css")
-    .postCss("resources/css/colors/mercury.css", "public/css/colors/mercury.min.css")
-    .postCss("resources/css/colors/nocturnal.css", "public/css/colors/nocturnal.min.css")
-    .postCss("resources/css/colors/olivia.css", "public/css/colors/olivia.min.css")
-    .postCss("resources/css/colors/pinkplastic.css", "public/css/colors/pinkplastic.min.css")
-    .postCss("resources/css/colors/sage.css", "public/css/colors/sage.min.css")
-    .postCss("resources/css/colors/shinytomato.css", "public/css/colors/shinytomato.min.css")
-    .postCss("resources/css/colors/tealtop.css", "public/css/colors/tealtop.min.css")
-    .postCss("resources/css/fab.css", "public/css/fab.min.css")
-    .postCss("resources/css/minimal.css", "public/css/minimal.min.css")
-    .postCss("resources/css/webtrees.css", "public/css/webtrees.min.css")
-    .postCss("resources/css/xenea.css", "public/css/xenea.min.css");
+  .sass('resources/sass/vendor.scss', 'public/css/vendor.min.css')
+  .autoload({
+    jquery: ['$', 'jQuery']
+  })
+  .setPublicPath('./public')
+  .sourceMaps()
+  .js('resources/js/vendor.js', 'public/js/vendor.min.js')
+  .babel(['resources/js/webtrees.js', 'resources/js/statistics.js', 'resources/js/treeview.js'], 'public/js/webtrees.min.js')
+  .copy('node_modules/leaflet/dist/images/*', 'public/css/images/')
+  .copy('node_modules/dejavu-fonts-ttf/ttf/DejaVuSans.ttf', 'resources/fonts/')
+  .copy('node_modules/lightgallery/src/fonts/*', 'public/fonts/')
+  .options({
+    processCssUrls: false,
+    postCss: [
+      postcss_import,
+      postcss_rtl,
+      postcss_autoprefixer,
+      postcss_image_inliner,
+      postcss_custom_properties
+    ]
+  })
+  .postCss('resources/css/administration.css', 'public/css/administration.min.css')
+  .postCss('resources/css/clouds.css', 'public/css/clouds.min.css')
+  .postCss('resources/css/colors.css', 'public/css/colors.min.css')
+  .postCss('resources/css/colors/aquamarine.css', 'public/css/colors/aquamarine.min.css')
+  .postCss('resources/css/colors/ash.css', 'public/css/colors/ash.min.css')
+  .postCss('resources/css/colors/belgianchocolate.css', 'public/css/colors/belgianchocolate.min.css')
+  .postCss('resources/css/colors/bluelagoon.css', 'public/css/colors/bluelagoon.min.css')
+  .postCss('resources/css/colors/bluemarine.css', 'public/css/colors/bluemarine.min.css')
+  .postCss('resources/css/colors/coffeeandcream.css', 'public/css/colors/coffeeandcream.min.css')
+  .postCss('resources/css/colors/coldday.css', 'public/css/colors/coldday.min.css')
+  .postCss('resources/css/colors/greenbeam.css', 'public/css/colors/greenbeam.min.css')
+  .postCss('resources/css/colors/mediterranio.css', 'public/css/colors/mediterranio.min.css')
+  .postCss('resources/css/colors/mercury.css', 'public/css/colors/mercury.min.css')
+  .postCss('resources/css/colors/nocturnal.css', 'public/css/colors/nocturnal.min.css')
+  .postCss('resources/css/colors/olivia.css', 'public/css/colors/olivia.min.css')
+  .postCss('resources/css/colors/pinkplastic.css', 'public/css/colors/pinkplastic.min.css')
+  .postCss('resources/css/colors/sage.css', 'public/css/colors/sage.min.css')
+  .postCss('resources/css/colors/shinytomato.css', 'public/css/colors/shinytomato.min.css')
+  .postCss('resources/css/colors/tealtop.css', 'public/css/colors/tealtop.min.css')
+  .postCss('resources/css/fab.css', 'public/css/fab.min.css')
+  .postCss('resources/css/minimal.css', 'public/css/minimal.min.css')
+  .postCss('resources/css/webtrees.css', 'public/css/webtrees.min.css')
+  .postCss('resources/css/xenea.css', 'public/css/xenea.min.css');
