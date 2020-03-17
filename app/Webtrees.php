2d1617f460f9b0c28b498f800ba33a1e7012ece4
Greg Roach <greg@subaqua.co.uk>
2020-03-17 15:42:49 +0000

    2.0.3

--- app/Webtrees.php
+++ app/Webtrees.php
@@ -92,7 +92,7 @@ class Webtrees
     public const SCHEMA_VERSION = 44;
 
     // e.g. "dev", "alpha", "beta", etc.
-    public const STABILITY = 'dev';
+    public const STABILITY = '';
 
     // Version number
     public const VERSION = '2.0.3' . (self::STABILITY === '' ? '' : '-') . self::STABILITY;
