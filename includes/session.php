abcc7fb0f68c7dedd629d1b44da0cfec57688f26
Greg Roach <greg@subaqua.co.uk>
2020-05-11 13:24:43 +0100

    1.7.17

--- includes/session.php
+++ includes/session.php
@@ -41,7 +41,7 @@ global $WT_TREE, $SEARCH_SPIDER;
 
 // Identify ourself
 define('WT_WEBTREES', 'webtrees');
-define('WT_VERSION', '1.7.16');
+define('WT_VERSION', '1.7.17');
 
 // External URLs
 define('WT_WEBTREES_URL', 'https://www.webtrees.net/');
