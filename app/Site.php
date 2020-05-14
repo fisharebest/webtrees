743d1d6315ac635e54507834e798b8f85d4219cb
Greg Roach <greg@subaqua.co.uk>
2020-05-14 15:17:03 +0100

    Fix: #3242 - do not store sensitive data in the audit log

--- app/Site.php
+++ app/Site.php
@@ -21,6 +21,7 @@ namespace Fisharebest\Webtrees;
 
 use Illuminate\Database\Capsule\Manager as DB;
 
+use function in_array;
 use function mb_substr;
 
 /**
@@ -28,6 +29,11 @@ use function mb_substr;
  */
 class Site
 {
+    // The following preferences contain sensitive data, and should not be logged.
+    private const SENSITIVE_PREFERENCES = [
+        'SMTP_AUTH_PASS'
+    ];
+
     /**
      * Everything from the wt_site_setting table.
      *
@@ -77,6 +83,10 @@ class Site
             ]);
 
             self::$preferences[$setting_name] = $setting_value;
+
+            if (in_array($setting_name, self::SENSITIVE_PREFERENCES, true)) {
+                $setting_value = '********';
+            }
 
             Log::addConfigurationLog('Site preference "' . $setting_name . '" set to "' . $setting_value . '"', null);
         }
