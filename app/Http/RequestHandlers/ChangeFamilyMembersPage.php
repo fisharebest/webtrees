43322877c77e9a8a16cba087b3fe7e0af63a6800
Greg Roach <greg@subaqua.co.uk>
2020-06-14 07:10:26 +0100

    Factor controller actions into request handlers

---
+++ app/Http/RequestHandlers/ChangeFamilyMembersPage.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * webtrees: online genealogy
+ * Copyright (C) 2020 webtrees development team
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Fisharebest\Webtrees\Http\RequestHandlers;
+
+use Fisharebest\Webtrees\Auth;
+use Fisharebest\Webtrees\Factory;
+use Fisharebest\Webtrees\Http\ViewResponseTrait;
+use Fisharebest\Webtrees\I18N;
+use Fisharebest\Webtrees\Tree;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+use function assert;
+
+/**
+ * Change the members of a family.
+ */
+class ChangeFamilyMembersPage implements RequestHandlerInterface
+{
+    use ViewResponseTrait;
+
+    /**
+     * @param ServerRequestInterface $request
+     *
+     * @return ResponseInterface
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $tree = $request->getAttribute('tree');
+        assert($tree instanceof Tree);
+
+        $xref   = $request->getQueryParams()['xref'];
+        $family = Factory::family()->make($xref, $tree);
+        $family = Auth::checkFamilyAccess($family, true);
+
+        $title = I18N::translate('Change family members') . ' – ' . $family->fullName();
+
+        return $this->viewResponse('edit/change-family-members', [
+            'tree'     => $tree,
+            'title'    => $title,
+            'family'   => $family,
+            'father'   => $family->husband(),
+            'mother'   => $family->wife(),
+            'children' => $family->children(),
+        ]);
+    }
+}
