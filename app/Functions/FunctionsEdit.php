38c3d44034de91ee2ac726e639e16f9f28d2fbad
Greg Roach <greg@subaqua.co.uk>
2020-10-11 21:00:15 +0100

    Fix: NAME field not updated when editing GIVN field

--- app/Functions/FunctionsEdit.php
+++ app/Functions/FunctionsEdit.php
@@ -432,7 +432,7 @@ class FunctionsEdit
             $html .= '</div>';
         } elseif ($fact === 'GIVN') {
             $html .= '<div class="input-group">';
-            $html .= '<input class="form-control" type="text" id="' . $id . '" name="' . $name . '" value="' . e($value) . '" data-autocomplete-typeoninput="updatewholename()" autofocus>';
+            $html .= '<input class="form-control" type="text" id="' . $id . '" name="' . $name . '" value="' . e($value) . '" oninput="updatewholename()" autofocus>';
             $html .= view('edit/input-addon-keyboard', ['id' => $id]);
             $html .= '</div>';
         } elseif ($fact === 'SURN' || $fact === '_MARNM_SURN') {
