e4cf93e367ec5647c295019975cd39de03da8c74
Greg Roach <greg@subaqua.co.uk>
2020-12-16 10:57:30 +0000

    Always show death of parent in relative-facts even if was before the birth

--- app/Module/IndividualFactsTabModule.php
+++ app/Module/IndividualFactsTabModule.php
@@ -851,7 +851,8 @@ class IndividualFactsTabModule extends AbstractModule implements ModuleTabInterf
             foreach ($family->spouses() as $parent) {
                 if (str_contains($SHOW_RELATIVES_EVENTS, '_DEAT' . ($sosa === 1 ? '_PARE' : '_GPAR'))) {
                     foreach ($parent->facts(['DEAT', 'BURI', 'CREM']) as $fact) {
-                        if ($this->includeFact($fact, $min_date, $max_date)) {
+                        // Show death of parent when it happened prior to birth
+                        if ($sosa === 1 && Date::compare($fact->date(), $min_date) < 0 || $this->includeFact($fact, $min_date, $max_date)) {
                             switch ($sosa) {
                                 case 1:
                                     $facts[] = $this->convertEvent($fact, $death_of_a_parent[$fact->getTag()][$fact->record()->sex()]);
