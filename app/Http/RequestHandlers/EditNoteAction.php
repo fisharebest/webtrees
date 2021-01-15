9dce578d99445e6098ac1e8cf1f38367a902bfcd
Greg Roach <greg@subaqua.co.uk>
2021-01-15 10:38:26 +0000

    Refactor controller into request handlers

---
+++ app/Http/RequestHandlers/EditNoteAction.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * webtrees: online genealogy
+ * Copyright (C) 2020 webtrees development team
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Fisharebest\Webtrees\Http\RequestHandlers;
+
+use Fisharebest\Webtrees\Auth;
+use Fisharebest\Webtrees\Registry;
+use Fisharebest\Webtrees\Tree;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+use function assert;
+
+/**
+ * Edit note objects.
+ */
+class EditNoteAction implements RequestHandlerInterface
+{
+    /**
+     * Show a form to create a new note object.
+     *
+     * @param ServerRequestInterface $request
+     *
+     * @return ResponseInterface
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $tree = $request->getAttribute('tree');
+        assert($tree instanceof Tree);
+
+        $xref = $request->getAttribute('xref');
+
+        $note = Registry::noteFactory()->make($xref, $tree);
+        $note = Auth::checkNoteAccess($note, true);
+
+        $params = (array) $request->getParsedBody();
+
+        $NOTE = $params['NOTE'];
+
+        // Convert HTML line endings to GEDCOM continuations
+        $NOTE = strtr($NOTE, ["\r\n" => "\n1 CONT "]);
+
+        // "\" and "$" are significant in preg replacement strings, so escape them.
+        $NOTE = str_replace(['\\', '$'], ['\\\\', '\\$'], $NOTE);
+
+        $gedrec = preg_replace(
+            '/^0 @' . $note->xref() . '@ NOTE.*(\n1 CONT.*)*/',
+            '0 @' . $note->xref() . '@ NOTE ' . $NOTE,
+            $note->gedcom()
+        );
+
+        $note->updateRecord($gedrec, true);
+
+        return redirect($note->url());
+    }
+}
--- app/Http/RequestHandlers/EditNotePage.php
+++ app/Http/RequestHandlers/EditNotePage.php
@@ -17,22 +17,26 @@
 
 declare(strict_types=1);
 
-namespace Fisharebest\Webtrees\Http\Controllers;
+namespace Fisharebest\Webtrees\Http\RequestHandlers;
 
 use Fisharebest\Webtrees\Auth;
+use Fisharebest\Webtrees\Http\ViewResponseTrait;
 use Fisharebest\Webtrees\I18N;
 use Fisharebest\Webtrees\Registry;
 use Fisharebest\Webtrees\Tree;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 use function assert;
 
 /**
- * Controller for edit forms and responses.
+ * Edit note objects.
  */
-class EditNoteController extends AbstractBaseController
+class EditNotePage implements RequestHandlerInterface
 {
+    use ViewResponseTrait;
+
     /**
      * Show a form to create a new note object.
      *
@@ -40,7 +44,7 @@ class EditNoteController extends AbstractBaseController
      *
      * @return ResponseInterface
      */
-    public function editNoteObject(ServerRequestInterface $request): ResponseInterface
+    public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $tree = $request->getAttribute('tree');
         assert($tree instanceof Tree);
@@ -55,43 +59,5 @@ class EditNoteController extends AbstractBaseController
             'title' => I18N::translate('Edit the shared note'),
             'tree'  => $tree,
         ]);
-    }
-
-    /**
-     * Show a form to create a new note object.
-     *
-     * @param ServerRequestInterface $request
-     *
-     * @return ResponseInterface
-     */
-    public function updateNoteObject(ServerRequestInterface $request): ResponseInterface
-    {
-        $tree = $request->getAttribute('tree');
-        assert($tree instanceof Tree);
-
-        $xref = $request->getAttribute('xref');
-
-        $note = Registry::noteFactory()->make($xref, $tree);
-        $note = Auth::checkNoteAccess($note, true);
-
-        $params = (array) $request->getParsedBody();
-
-        $NOTE = $params['NOTE'];
-
-        // Convert HTML line endings to GEDCOM continuations
-        $NOTE = strtr($NOTE, ["\r\n" => "\n1 CONT "]);
-
-        // "\" and "$" are significant in preg replacement strings, so escape them.
-        $NOTE = str_replace(['\\', '$'], ['\\\\', '\\$'], $NOTE);
-
-        $gedrec = preg_replace(
-            '/^0 @' . $note->xref() . '@ NOTE.*(\n1 CONT.*)*/',
-            '0 @' . $note->xref() . '@ NOTE ' . $NOTE,
-            $note->gedcom()
-        );
-
-        $note->updateRecord($gedrec, true);
-
-        return redirect($note->url());
     }
 }
