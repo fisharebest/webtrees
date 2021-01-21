b598eac0d4271423963f305419d3af8c64ebc949
Greg Roach <greg@subaqua.co.uk>
2021-01-21 10:18:37 +0000

    Strict types

--- app/Site.php
+++ app/Site.php
@@ -2,7 +2,7 @@
 
 /**
  * webtrees: online genealogy
- * Copyright (C) 2019 webtrees development team
+ * Copyright (C) 2021 webtrees development team
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -93,7 +93,7 @@ class Site
                 $setting_value = '********';
             }
 
-            if (!in_array($setting_name, self::UNIMPORTANT_PREFERENCES)) {
+            if (!in_array($setting_name, self::UNIMPORTANT_PREFERENCES, true)) {
                 Log::addConfigurationLog('Site preference "' . $setting_name . '" set to "' . $setting_value . '"', null);
             }
         }
