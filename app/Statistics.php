2da2e0a6818e1203f492a103d86aebe95653ac7c
Greg Roach <greg@subaqua.co.uk>
2021-01-22 09:35:34 +0000

    Fix: #3664 - individual name statistics not working correctly

--- app/Statistics.php
+++ app/Statistics.php
@@ -2,7 +2,7 @@
 
 /**
  * webtrees: online genealogy
- * Copyright (C) 2020 webtrees development team
+ * Copyright (C) 2021 webtrees development team
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -2193,7 +2193,7 @@ class Statistics implements
     public function commonSurnamesTotals(
         string $threshold = '1',
         string $number_of_surnames = '10',
-        string $sorting = 'rcount'
+        string $sorting = 'count'
     ): string {
         return $this->individualRepository->commonSurnamesTotals((int) $threshold, (int) $number_of_surnames, $sorting);
     }
@@ -2223,7 +2223,7 @@ class Statistics implements
     public function commonSurnamesListTotals(
         string $threshold = '1',
         string $number_of_surnames = '10',
-        string $sorting = 'rcount'
+        string $sorting = 'count'
     ): string {
         return $this->individualRepository
             ->commonSurnamesListTotals((int) $threshold, (int) $number_of_surnames, $sorting);
