89f7189b61a494347591c99bdb92afb7d8b66e1b
Greg Roach <greg@subaqua.co.uk>
2021-01-25 15:52:32 +0000

    Use https link in copyright header

--- app/Module/FrenchHistory.php
+++ app/Module/FrenchHistory.php
@@ -2,7 +2,7 @@
 
 /**
  * webtrees: online genealogy
- * Copyright (C) 2020 webtrees development team
+ * Copyright (C) 2021 webtrees development team
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -12,7 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  *
  * Original source : http://www.lorand.org/spip.php?article194
  */
