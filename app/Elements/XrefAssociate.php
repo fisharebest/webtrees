7b54238961f347ceabed416b844859d4260b5348
Greg Roach <fisharebest@gmail.com>
2021-05-08 14:34:30 +0100

    Gedcom tags

---
+++ app/Elements/XrefAssociate.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * webtrees: online genealogy
+ * Copyright (C) 2021 webtrees development team
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Fisharebest\Webtrees\Elements;
+
+use Fisharebest\Webtrees\Registry;
+use Fisharebest\Webtrees\Tree;
+
+use function view;
+
+/**
+ * XREF:INDI := {Size=1:22}
+ * A pointer to, or a cross-reference identifier of, an individual record.
+ */
+class XrefAssociate extends XrefIndividual
+{
+    protected const SUBTAGS = [
+        'RELA' => '0:1',
+        'NOTE' => '0:M',
+        'SOUR' => '0:M',
+    ];
+}
