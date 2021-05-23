d36fce8c366ca445c8c1cf949463b24454867d72
Greg Roach <greg@subaqua.co.uk>
2021-05-23 21:16:55 +0100

    Fix favicons

--- app/Http/Routes/WebRoutes.php
+++ app/Http/Routes/WebRoutes.php
@@ -53,6 +53,7 @@ use Fisharebest\Webtrees\Http\RequestHandlers\AutoCompletePlace;
 use Fisharebest\Webtrees\Http\RequestHandlers\AutoCompleteSurname;
 use Fisharebest\Webtrees\Http\RequestHandlers\BroadcastAction;
 use Fisharebest\Webtrees\Http\RequestHandlers\BroadcastPage;
+use Fisharebest\Webtrees\Http\RequestHandlers\BrowserconfigXml;
 use Fisharebest\Webtrees\Http\RequestHandlers\CalendarAction;
 use Fisharebest\Webtrees\Http\RequestHandlers\CalendarEvents;
 use Fisharebest\Webtrees\Http\RequestHandlers\CalendarPage;
@@ -707,8 +708,9 @@ class WebRoutes
             $router->post(SelectTheme::class, '/theme/{theme}');
             $router->get(HomePage::class, '/');
 
-            // Some URL rewrite configurations will pass everything not in /public to index.php
+            // Special files
             $router->get(AppleTouchIconPng::class, '/apple-touch-icon.png');
+            $router->get(BrowserconfigXml::class, '/browserconfig.xml');
             $router->get(FaviconIco::class, '/favicon.ico');
             $router->get(WebmanifestJson::class, '/webmanifest.json');
         });
