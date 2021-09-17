4461ee8de656dd02a874bbf1e72003239df9fa2f
Greg Roach <greg@subaqua.co.uk>
2021-09-17 10:19:50 +0100

    Remove unused import

--- app/Elements/WhereWithinSource.php
+++ app/Elements/WhereWithinSource.php
@@ -20,7 +20,6 @@ declare(strict_types=1);
 namespace Fisharebest\Webtrees\Elements;
 
 use Fisharebest\Webtrees\Http\RequestHandlers\AutoCompleteCitation;
-use Fisharebest\Webtrees\Registry;
 use Fisharebest\Webtrees\Tree;
 
 use function e;
