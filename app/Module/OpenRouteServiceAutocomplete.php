cada4c7ceb964a4e903e8e52010c0547a732cfeb
Greg Roach <greg@subaqua.co.uk>
2021-10-21 19:53:14 +0100

    Fix: #4082 open-route-search gives results with no country

--- app/Module/OpenRouteServiceAutocomplete.php
+++ app/Module/OpenRouteServiceAutocomplete.php
@@ -137,17 +137,23 @@ class OpenRouteServiceAutocomplete extends AbstractModule implements ModuleConfi
         $results = json_decode($body, false, 512, JSON_THROW_ON_ERROR);
 
         foreach ($results->features as $result) {
+            $result->properties->name ??= null;
+            $result->properties->county ??= null;
+            $result->properties->region ??= null;
+            $result->properties->macroregion ??= null;
+            $result->properties->country ??= null;
+
             if ($result->properties->country === 'United Kingdom') {
                 // macroregion will contain England, Scotland, etc.
                 $result->properties->country = null;
             }
 
             $parts = [
-                $result->properties->name ?? null,
-                $result->properties->county ?? null,
-                $result->properties->region ?? null,
-                $result->properties->macroregion ?? null,
-                $result->properties->country ?? null,
+                $result->properties->name,
+                $result->properties->county,
+                $result->properties->region,
+                $result->properties->macroregion,
+                $result->properties->country,
             ];
 
             $places[] = implode(Gedcom::PLACE_SEPARATOR, array_filter($parts)) ?: $result->properties->label;
