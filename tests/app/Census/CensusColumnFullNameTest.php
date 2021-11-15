ede12dd76e86462107502e1d66b4be09c45656d0
Greg Roach <greg@subaqua.co.uk>
2021-11-15 20:11:55 +0000

    CodeStyle

--- tests/app/Census/CensusColumnFullNameTest.php
+++ tests/app/Census/CensusColumnFullNameTest.php
@@ -39,11 +39,11 @@ class CensusColumnFullNameTest extends TestCase
      */
     public function xxxtestFullName(): void
     {
-        $individual = self::createStub(Individual::class);
+        $individual = $this->createStub(Individual::class);
         $individual->method('getAllNames')->willReturn([['full' => 'Joe Bloggs']]);
         $individual->method('spouseFamilies')->willReturn(new Collection());
 
-        $census = self::createStub(CensusInterface::class);
+        $census = $this->createStub(CensusInterface::class);
         $census->method('censusDate')->willReturn('');
 
         $column = new CensusColumnFullName($census, '', '');
@@ -70,22 +70,22 @@ class CensusColumnFullNameTest extends TestCase
 
         $marriage_date = new Date('02 DATE 2019');
 
-        $marriage = self::createStub(Fact::class);
+        $marriage = $this->createStub(Fact::class);
         $marriage->method('date')->willReturn($marriage_date);
 
-        $spouse = self::createStub(Individual::class);
+        $spouse = $this->createStub(Individual::class);
         $spouse->method('getAllNames')->willReturn($husband_names);
 
-        $family = self::createStub(Family::class);
+        $family = $this->createStub(Family::class);
         $family->method('facts')->willReturn(new Collection([$marriage]));
         $family->method('getMarriageDate')->willReturn($marriage_date);
         $family->method('spouse')->willReturn($spouse);
 
-        $individual = self::createStub(Individual::class);
+        $individual = $this->createStub(Individual::class);
         $individual->method('getAllNames')->willReturn($wife_names);
         $individual->method('spouseFamilies')->willReturn(new Collection([$family]));
 
-        $census = self::createStub(CensusInterface::class);
+        $census = $this->createStub(CensusInterface::class);
         $census->method('censusDate')->willReturn('01 JAN 2020');
 
         $column = new CensusColumnFullName($census, '', '');
