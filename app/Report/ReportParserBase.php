9fbee35a0eb24037416f1174f538080a0486fee1
Greg Roach <greg@subaqua.co.uk>
2021-12-03 12:02:06 +0000

    Fix: reports not found - relative/absolute path error

--- app/Report/ReportParserBase.php
+++ app/Report/ReportParserBase.php
@@ -82,7 +82,7 @@ class ReportParserBase
             }
         );
 
-        $fp = Registry::filesystem()->root()->readStream($report);
+        $fp = fopen($report, 'rb');
 
         while ($data = fread($fp, 4096)) {
             if (!xml_parse($this->xml_parser, $data, feof($fp))) {
