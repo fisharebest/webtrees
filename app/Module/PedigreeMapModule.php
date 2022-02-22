149f27379c9c160647f3c76ffda497f3cf2ba26c
David Drury <david@drury.me.uk>
2022-02-22 10:31:29 +0000

    Add parent identifier & thumbnail image to Pedigree map sidebar

--- app/Module/PedigreeMapModule.php
+++ app/Module/PedigreeMapModule.php
@@ -66,8 +66,8 @@ class PedigreeMapModule extends AbstractModule implements ModuleChartInterface, 
     // Limits
     public const MAXIMUM_GENERATIONS = 10;
 
-    // CSS colors defined for each generation
-    private const GENERATION_COLORS = 12;
+    // CSS colors for each generation
+    private const COUNT_CSS_COLORS = 12;
 
     private ChartService $chart_service;
 
@@ -261,7 +261,7 @@ class PedigreeMapModule extends AbstractModule implements ModuleChartInterface, 
                 $polyline           = null;
                 $sosa_points[$sosa] = [$latitude, $longitude];
                 $sosa_child         = intdiv($sosa, 2);
-                $color              = 'var(--wt-pedigree-map-gen-' . $sosa_child % self::GENERATION_COLORS . ')';
+                $color              = 'var(--wt-pedigree-map-gen-' . $sosa_child % self::COUNT_CSS_COLORS . ')';
 
                 if (array_key_exists($sosa_child, $sosa_points)) {
                     // Would like to use a GeometryCollection to hold LineStrings
@@ -286,11 +286,13 @@ class PedigreeMapModule extends AbstractModule implements ModuleChartInterface, 
                     ],
                     'properties' => [
                         'iconcolor' => $color,
-                        'tooltip'   => $fact->place()->gedcomName(),
+                        'tooltip'   => ucfirst($this->getSosaName($sosa)) . "\n" . strip_tags($fact->record()->fullName()),
                         'summary'   => view('modules/pedigree-map/events', [
                             'fact'         => $fact,
                             'relationship' => ucfirst($this->getSosaName($sosa)),
                             'sosa'         => $sosa,
+                            'colorclass'   => 'wt-pedigree-map-gen-' . $sosa_child % self::COUNT_CSS_COLORS,
+                            'tree'         => $tree,
                         ]),
                     ],
                 ];
