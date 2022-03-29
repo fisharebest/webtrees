1763aecae76691021ba10c281e767c91c92330f4
Greg Roach <greg@subaqua.co.uk>
2022-03-29 09:09:56 +0100

    Add DataForSEO to the list of unwanted robots

--- app/Http/Middleware/BadBotBlocker.php
+++ app/Http/Middleware/BadBotBlocker.php
@@ -63,6 +63,7 @@ class BadBotBlocker implements MiddlewareInterface
         'AspiegelBot',
         'Barkrowler',
         'BLEXBot',
+        'DataForSEO',
         'DotBot',
         'Grapeshot',
         'ia_archiver',
