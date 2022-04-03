7e2dedbb6a7f6b85c05aa517a7b19b737bafbb7b
Greg Roach <greg@subaqua.co.uk>
2022-04-03 14:39:05 +0100

    2.1.0-beta.2

--- app/Webtrees.php
+++ app/Webtrees.php
@@ -135,7 +135,7 @@ class Webtrees
     public const SCHEMA_VERSION = 45;
 
     // e.g. "-dev", "-alpha", "-beta", etc.
-    public const STABILITY = '-beta.1';
+    public const STABILITY = '-beta.2';
 
     // Version number
     public const VERSION = '2.1.0' . self::STABILITY;
