07e9e923ccb8f5be1becc5cdcf342b430cc3ba70
Greg Roach <greg@subaqua.co.uk>
2022-04-23 11:14:31 +0100

    Fix: #4334 - cannot delete user messages

--- app/Module/UserMessagesModule.php
+++ app/Module/UserMessagesModule.php
@@ -88,7 +88,7 @@ class UserMessagesModule extends AbstractModule implements ModuleBlockInterface
     {
         $tree        = Validator::attributes($request)->tree();
         $context     = Validator::queryParams($request)->string('context');
-        $message_ids = Validator::parsedBody($request)->array('message_ids');
+        $message_ids = Validator::parsedBody($request)->array('message_id');
 
         DB::table('message')
             ->where('user_id', '=', Auth::id())
