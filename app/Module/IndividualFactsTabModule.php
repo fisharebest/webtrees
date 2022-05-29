c67afd10bd35cc25acdb4f8f6ae2e71e13d72eae
Greg Roach <greg@subaqua.co.uk>
2022-05-29 21:20:27 +0100

    Fix: #4430 - family sources shown on individual facts tab

--- app/Module/IndividualFactsTabModule.php
+++ app/Module/IndividualFactsTabModule.php
@@ -125,11 +125,10 @@ class IndividualFactsTabModule extends AbstractModule implements ModuleTabInterf
             ->map(fn (ModuleTabInterface $tab): Collection => $tab->supportedFacts())
             ->flatten();
 
-        $indi_exclude_facts = $sidebar_facts->merge($tab_facts);
-        $fam_exclude_facts  = new Collection(['FAM:CHAN', 'FAM:_UID', 'FAM:HUSB', 'FAM:WIFE', 'FAM:CHIL']);
+        $exclude_facts = $sidebar_facts->merge($tab_facts);
 
-        $individual_facts = $this->individual_facts_service->individualFacts($individual, $indi_exclude_facts);
-        $family_facts     = $this->individual_facts_service->familyFacts($individual, $fam_exclude_facts);
+        $individual_facts = $this->individual_facts_service->individualFacts($individual, $exclude_facts);
+        $family_facts     = $this->individual_facts_service->familyFacts($individual, $exclude_facts);
         $relative_facts   = $this->individual_facts_service->relativeFacts($individual);
         $associate_facts  = $this->individual_facts_service->associateFacts($individual);
         $historic_facts   = $this->individual_facts_service->historicFacts($individual);
