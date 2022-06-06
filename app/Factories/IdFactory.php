5ce3c032897be77f5fc605d6055d1e9f1f0e8d95
Greg Roach <greg@subaqua.co.uk>
2022-06-06 10:14:39 +0100

    CodeStyle

--- app/Factories/IdFactory.php
+++ app/Factories/IdFactory.php
@@ -20,7 +20,6 @@ declare(strict_types=1);
 namespace Fisharebest\Webtrees\Factories;
 
 use Fisharebest\Webtrees\Contracts\IdFactoryInterface;
-
 use Ramsey\Uuid\Exception\RandomSourceException;
 use Ramsey\Uuid\Uuid;
 
