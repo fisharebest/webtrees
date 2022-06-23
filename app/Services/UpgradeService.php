e489b3bfe553cb0df016ee587dda91353a01a41a
Greg Roach <greg@subaqua.co.uk>
2022-06-23 20:54:59 +0100

    Fix: #4488 - log errors when unable to fetch latest version

--- app/Services/UpgradeService.php
+++ app/Services/UpgradeService.php
@@ -22,6 +22,7 @@ namespace Fisharebest\Webtrees\Services;
 use Fig\Http\Message\StatusCodeInterface;
 use Fisharebest\Webtrees\Http\Exceptions\HttpServerErrorException;
 use Fisharebest\Webtrees\I18N;
+use Fisharebest\Webtrees\Log;
 use Fisharebest\Webtrees\Registry;
 use Fisharebest\Webtrees\Site;
 use Fisharebest\Webtrees\Webtrees;
@@ -335,6 +336,7 @@ class UpgradeService
             } catch (GuzzleException $ex) {
                 // Can't connect to the server?
                 // Use the existing information about latest versions.
+                Log::addErrorLog('Cannot fetch latest webtrees version. ' . $ex->getMessage());
             }
         }
 
