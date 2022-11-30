f8024573d843adccf86153e56d5537e4026d6e87
Greg Roach <greg@subaqua.co.uk>
2022-11-30 19:35:06 +0000

    CodeStyle

--- app/Http/RequestHandlers/UploadMediaAction.php
+++ app/Http/RequestHandlers/UploadMediaAction.php
@@ -115,7 +115,7 @@ class UploadMediaAction implements RequestHandlerInterface
 
             try {
                 $file_exists = $data_filesystem->fileExists($path);
-            } catch (FilesystemException | UnableToCheckFileExistence ) {
+            } catch (FilesystemException | UnableToCheckFileExistence) {
                 $file_exists = false;
             }
 
