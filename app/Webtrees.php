d33f9d2063a5e1aac52eadf3f6f96792a367dc0f
Greg Roach <greg@subaqua.co.uk>
2023-07-04 12:44:52 +0100

    2.1.17

--- app/Webtrees.php
+++ app/Webtrees.php
@@ -143,7 +143,7 @@ class Webtrees
     public const STABILITY = '';
 
     // Version number.
-    public const VERSION = '2.1.16' . self::STABILITY;
+    public const VERSION = '2.1.17' . self::STABILITY;
 
     // Project website.
     public const URL = 'https://webtrees.net/';
