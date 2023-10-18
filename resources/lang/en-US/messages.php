7fcbe9f7a12caad418dc2d23a2235b82fae891f7
Greg Roach <greg@subaqua.co.uk>
2023-10-18 20:18:14 +0100

    Update US country names

--- resources/lang/en-US/messages.php
+++ resources/lang/en-US/messages.php
@@ -1,8 +1,19 @@
 <?php
 
-declare(strict_types=1);
-
 return array (
   '%H:%i:%s' => '%g:%i:%s %a',
   '%j %F %Y' => '%F %j, %Y',
+  'Asunción, Paraguay' => 'Asuncion, Paraguay',
+  'Bogotá, Colombia' => 'Bogota, Colombia',
+  'Ciudad Juárez, Mexico' => 'Ciudad Juarez, Mexico',
+  'Colonia Juárez, Mexico' => 'Colonia Juarez, Mexico',
+  'Córdoba, Argentina' => 'Cordoba, Argentina',
+  'Curaçao' => 'Curacao',
+  'Côte d’Ivoire' => 'Cote d’Ivoire',
+  'Réunion' => 'Reunion',
+  'Saint Barthélemy' => 'Saint Barthelemy',
+  'San José, Costa Rica' => 'San Jose, Costa Rica',
+  'São Paulo, Brazil' => 'Sao Paulo, Brazil',
+  'Tuxtla Gutiérrez, Mexico' => 'Tuxtla Gutierrez, Mexico',
+  'Åland Islands' => 'Aland Islands',
 );
