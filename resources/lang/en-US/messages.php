7404c52d5eef602e5ed3943f2556f39fdff2ff1a
Greg Roach <greg@subaqua.co.uk>
2024-04-14 14:45:30 +0100

    en-US langauge file updates

--- resources/lang/en-US/messages.php
+++ resources/lang/en-US/messages.php
@@ -5,4 +5,17 @@ declare(strict_types=1);
 return array (
   '%H:%i:%s' => '%g:%i:%s %a',
   '%j %F %Y' => '%F %j, %Y',
+  'Asunción, Paraguay' => 'Asuncion, Paraguay',
+  'Bogotá, Colombia' => 'Bogota, Colombia',
+  'Ciudad Juárez, Mexico' => 'Ciudad Juarez, Mexico',
+  'Colonia Juárez, Mexico' => 'Colonia Juarez, Mexico',
+  'Curaçao' => 'Curacao',
+  'Córdoba, Argentina' => 'Cordoba, Argentina',
+  'Côte d’Ivoire' => 'Cote d’Ivoire',
+  'Réunion' => 'Reunion',
+  'Saint Barthélemy' => 'Saint Barthelemy',
+  'San José, Costa Rica' => 'San Jose, Costa Rica',
+  'São Paulo, Brazil' => 'Sao Paulo, Brazil',
+  'Tuxtla Gutiérrez, Mexico' => 'Tuxtla Gutierrez, Mexico',
+  'Åland Islands' => 'Aland Islands',
 );
