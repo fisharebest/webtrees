618e8a4dd78a32eb60741ab466dc97d885f4bf2c
Greg Roach <greg@subaqua.co.uk>
2024-09-21 22:24:51 +0100

    #5003 - add MARR:TYPE COMMON LAW

--- app/Elements/MarriageType.php
+++ app/Elements/MarriageType.php
@@ -28,24 +28,21 @@ use function strtoupper;
  */
 class MarriageType extends AbstractElement
 {
-    public const VALUE_CIVIL     = 'CIVIL';
-    public const VALUE_PARTNERS  = 'PARTNERS';
-    public const VALUE_RELIGIOUS = 'RELIGIOUS';
+    public const VALUE_CIVIL      = 'CIVIL';
+    public const VALUE_COMMON_LAW = 'COMMON LAW';
+    public const VALUE_PARTNERS   = 'PARTNERS';
+    public const VALUE_RELIGIOUS  = 'RELIGIOUS';
 
     /**
      * Convert a value to a canonical form.
-     * GEDCOM 5.5EL uses 'RELI' and 'CIVIL'
-     *
-     * @param string $value
-     *
-     * @return string
+     * GEDCOM 5.5EL uses 'RELI' for 'Religious marriage' (RELIGIOUS)
      */
     public function canonical(string $value): string
     {
         $value = strtoupper(parent::canonical($value));
 
         $canonical = [
-            'RELI' => 'RELIGIOUS'
+            'RELI' => self::VALUE_RELIGIOUS,
         ];
 
         return $canonical[$value] ?? $value;
@@ -59,11 +56,11 @@ class MarriageType extends AbstractElement
     public function values(): array
     {
         return [
-            ''                    => '',
-            self::VALUE_CIVIL     => I18N::translate('Civil marriage'),
-            self::VALUE_PARTNERS  => I18N::translate('Registered partnership'),
-            self::VALUE_RELIGIOUS => I18N::translate('Religious marriage'),
-
+            ''                     => '',
+            self::VALUE_CIVIL      => I18N::translate('Civil marriage'),
+            self::VALUE_COMMON_LAW => I18N::translate('Common-law marriage'),
+            self::VALUE_PARTNERS   => I18N::translate('Registered partnership'),
+            self::VALUE_RELIGIOUS  => I18N::translate('Religious marriage'),
         ];
     }
 }
