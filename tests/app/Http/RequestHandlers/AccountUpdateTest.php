c98deec003399c8fa0767bb553d2e32e527ed675
Bernard <kbwhizz1@gmail.com>
2024-10-31 15:42:55 +0000

    fixing phpunit error

--- tests/app/Http/RequestHandlers/AccountUpdateTest.php
+++ tests/app/Http/RequestHandlers/AccountUpdateTest.php
@@ -64,6 +64,8 @@ class AccountUpdateTest extends TestCase
                 'timezone'       => 'g',
                 'user_name'      => 'h',
                 'visible-online' => 'i',
+		'secret'         => 'j',
+		'status-mfa'     => 'k',
             ]);
         $response = $handler->handle($request);
 
