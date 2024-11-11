e896a2ce86e9b1993960d024f2d984ae11098ee7
Bernard <kbwhizz1@gmail.com>
2024-11-11 19:40:11 +0000

    fix 2 continuous-integration issues

--- app/Http/RequestHandlers/LoginAction.php
+++ app/Http/RequestHandlers/LoginAction.php
@@ -138,7 +138,7 @@ class LoginAction implements RequestHandlerInterface
                     throw new Exception(I18N::translate('2FA code does not match. Please try again.'));
                 }
             } else {
-                    throw new Exception(I18N::translate('2FA code must be entered as you have 2FA authentication enabled. Please try again.'));
+                throw new Exception(I18N::translate('2FA code must be entered as you have 2FA authentication enabled. Please try again.'));
             }
         }
         Auth::login($user);
