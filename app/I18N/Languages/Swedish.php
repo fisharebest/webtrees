37b0db9075c6eacfc78addbe9be1be19919e9ec4
Fredrik Ekdahl <fekdahl@gmail.com>
2026-07-26 18:16:38 +0200

    Use narrow no break space as thousands separator in swedish

--- app/I18N/Languages/Swedish.php
+++ app/I18N/Languages/Swedish.php
@@ -32,7 +32,7 @@ final readonly class Swedish extends AbstractLanguage
     protected const PaperSize PAPER_SIZE         = PaperSize::A4;
     protected const string    LANGUAGE_TAG       = 'sv';
     protected const string    LOCALE_CODE        = 'sv_SE@collation=phonebook';
-    protected const string    DIGITS_SEPARATOR   = UTF8::NO_BREAK_SPACE;
+    protected const string    DIGITS_SEPARATOR   = UTF8::NARROW_NO_BREAK_SPACE;
     protected const string    NEGATIVE_SYMBOL    = UTF8::MINUS_SIGN;
     protected const string    DECIMAL_SYMBOL     = ',';
     protected const string    DATE_ABOUT         = 'ungefär %s';
