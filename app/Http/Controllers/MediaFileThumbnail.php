b3fa522c636f5647c13c58fa46976d799624a4ba
Greg Roach <greg@subaqua.co.uk>
2026-08-18 14:51:48 +0100

    Fix: #5455 missing files for thumbnail return 500 instead of 404

--- app/Http/Controllers/MediaFileThumbnail.php
+++ app/Http/Controllers/MediaFileThumbnail.php
@@ -89,17 +89,15 @@ final class MediaFileThumbnail
                 $height        = (int) $params['h'];
                 $add_watermark = Auth::needsWatermark($media_file->media()->tree(), $user);
                 $path          = $media_file->filename();
-                $filename      = basename($path);
                 $filesystem    = $media_file->media()->tree()->mediaFilesystem();
 
                 try {
                     $last_modified = $filesystem->lastModified(path: $path);
-                } catch (FilesystemException $exception) {
-                    throw new ImageException(
-                        status_code: HttpStatusCode::InternalServerError,
-                        filename: $filename,
-                        error: 'File is not readable',
-                    );
+                } catch (FilesystemException|UnableToRetrieveMetadata) {
+                    // If we can't get a timestamp, it might be a limitation of the filesystem,
+                    // rather than a fatal error.  This is only used for cache-busting.
+                    // If we can't actually read the file's contents, then we will throw a 404 later.
+                    $last_modified = 0;
                 }
 
                 $cache_key = implode(separator: ':', array: [
